e1905cddae89105d75fc4943ab122827b29ccf41
Lindsay Salisbury <lsalis@fb.com>
2019-12-03 18:04:12 -0800

    Add pre-built tarball for generator validation tests
    Summary:
    This adds a pre-built tarball needed to validate the generator
    feature of `image.source(...)` across varied linux distros/versions.  Details
    on the issue are in the comments.
    
    Differential Revision: D18776727
    
    fbshipit-source-id: 7ff3902471648cc6af76464d21d1488267305b98

--- fs_image/compiler/tests/hello_world_tar_generator.sh
+++ fs_image/compiler/tests/hello_world_tar_generator.sh
@@ -1,19 +1,10 @@
 #!/bin/bash -uex
 set -o pipefail
-test $# -eq 2
+test $# -eq 3
 tarball_name=$1  # Exercises `generator_args`
-out_dir=$2  # Provided per the tarball generator contract
-tmp_dir=$(mktemp -d)
-trap 'rm -rf \"$tmp_dir\"' EXIT
+prebuilt_tarball=$2  # Compensates for non-stable hashes of generated tar
+out_dir=$3  # Provided per the tarball generator contract
 
-# Make an inner "$tmp_dir/d" so we don't have to change the permissions on
-# the outer "$tmp_dir" (`mktemp` makes those restrictive by default.)
-mkdir "$tmp_dir"/d/
-touch "$tmp_dir"/d/hello_world
-
-# Use deterministic options to create this tarball, as per
-# reproducible-builds.org.  This ensures its hash is stable.
-tar --sort=name --mtime=2018-01-01 --owner=0 --group=0 --numeric-owner \
-  -C "$tmp_dir/d" -cf "$out_dir/$tarball_name" .
+cp -a "$prebuilt_tarball" "$out_dir/$tarball_name"
 
 echo "$tarball_name"  # Required by the tarball generator contract
