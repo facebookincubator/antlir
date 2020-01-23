80d987705e1a0df2d5066c1a2026b98c64c75a59
Lindsay Salisbury <lsalis@fb.com>
2020-01-23 10:24:19 -0800

    Pure python implementation of rpm version comparison (#8)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/fs_image/pull/8
    
    This provides a pure python implemenation for comparing rpm versions.  The primary purpose of this is to remove the external, third-party dependency on the `python-rpm` module that requires/relies on compiled shared objects. More description about this is in the comments.
    
    Reviewed By: snarkmaster
    
    Differential Revision: D19519858
    
    fbshipit-source-id: b08d68e218123ed5233bd5f633af1c51b5665cfb

--- fs_image/rpm/rpm_metadata.py
+++ fs_image/rpm/rpm_metadata.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import os
-import rpm
+import re
 import subprocess
 
 from .common import Path, get_file_logger
@@ -70,19 +70,162 @@ class RpmMetadata(NamedTuple):
         return RpmMetadata(name=n, epoch=int(e), version=v, release=r)
 
 
-# Returns  1 if the version of a is newer than b
-# Returns  0 if the versions match
-# Returns -1 if the version of a is older than b
+# This comprises a pure python implementation of rpm version comparison. The
+# purpose for this is so that the fs_image library does not have a dependency
+# on a C library that is (for the most part) only distributed as part of rpm
+# based distros. Depending on a C library complicates dependency management
+# significantly in the OSS space due to the complexity of handling 3rd party C
+# libraries with buck. Having this pure python implementation also eases future
+# rpm usage/handling for both non-rpm based distros and different arch types.
 #
-# Referenced from:
-# github.com/rpm-software-management/yum/blob/master/rpmUtils/miscutils.py
+# This implementation is adapted from both this blog post:
+#  https://blog.jasonantman.com/2014/07/how-yum-and-rpm-compare-versions/
+# and this Apache 2 licensed implementation:
+#   https://github.com/sassoftware/python-rpm-vercmp/blob/master/rpm_vercmp/vercmp.py
+#
+# There are extensive test cases in the `test_rpm_metadata.py` test case that
+# cover a wide variety of normal and weird version comparsions.
 def compare_rpm_versions(a: RpmMetadata, b: RpmMetadata) -> int:
+    """
+        Returns:
+            1 if the version of a is newer than b
+            0 if the versions match
+            -1 if the version of a is older than b
+    """
+
     # This is not a rule, but it makes sense that our libs don't want to
     # compare versions of different RPMs
     if a.name != b.name:
         raise ValueError(f"Cannot compare RPM versions when names do not match")
 
-    return rpm.labelCompare(
-        (str(a.epoch), a.version, a.release),
-        (str(b.epoch), b.version, b.release)
-    )
+    # First compare the epoch, if set.  If the epoch's are not the same, then
+    # the higher one wins no matter what the rest of the EVR is.
+    if a.epoch != b.epoch:
+        if a.epoch > b.epoch:
+            return 1  # a > b
+        else:
+            return -1  # a < b
+
+    # Epoch is the same, if version + release are the same we have a match
+    if (a.version == b.version) and (a.release == b.release):
+        return 0  # a == b
+
+    # Compare version first, if version is equal then compare release
+    compare_res = _compare_values(a.version, b.version)
+    if compare_res != 0:  # a > b || a < b
+        return compare_res
+    else:
+        return _compare_values(a.release, b.release)
+
+
+R_NON_ALPHA_NUM_TILDE_CARET = re.compile(br"^([^a-zA-Z0-9~\^]*)(.*)$")
+R_NUM = re.compile(br"^([\d]+)(.*)$")
+R_ALPHA = re.compile(br"^([a-zA-Z]+)(.*)$")
+
+
+def _compare_values(left: str, right: str) -> int:
+    # Rpm versions can only be ascii, anything else is just
+    # ignored
+    left = left.encode("ascii", "ignore")
+    right = right.encode("ascii", "ignore")
+
+    if left == right:
+        return 0
+
+    while left or right:
+        match_left = R_NON_ALPHA_NUM_TILDE_CARET.match(left)
+        match_right = R_NON_ALPHA_NUM_TILDE_CARET.match(right)
+        left_head, left = match_left.group(1), match_left.group(2)
+        right_head, right = match_right.group(1), match_right.group(2)
+
+        # Ignore anything at the start we don't want
+        if left_head or right_head:
+            continue
+
+        # Look at tilde first, it takes precedent over everything else
+        if left.startswith(b'~'):
+            if not right.startswith(b'~'):
+                return -1  # left < right
+
+            # Strip the tilde and start again
+            left, right = left[1:], right[1:]
+            continue
+
+        # Tilde always means the version is less
+        if right.startswith(b'~'):
+            return 1  # left > right
+
+        # Now look at the caret, which is like the tilde but pointier.
+        if left.startswith(b'^'):
+            # left has a caret but right has ended
+            if not right:
+                return 1  # left > right
+
+            # left has a caret but right continues on
+            elif not right.startswith(b'^'):
+                return -1  # left < right
+
+            # strip the ^ and start again
+            left, right = left[1:], right[1:]
+            continue
+
+        # Caret means the version is less... Unless the other version
+        # has ended, then do the exact opposite.
+        if right.startswith(b'^'):
+            return -1 if not left else 1
+
+        # We've run out of characters to compare.
+        # Note: we have to do this after we compare the ~ and ^ madness
+        # because ~'s and ^'s take precedance.
+        if not left or not right:
+            break
+
+        # Lets see if we've got numbers
+        match_left = R_NUM.match(left)
+        if match_left:
+            match_right = R_NUM.match(right)
+            if not match_right:  # right is not a num and nums > alphas
+                return 1  # left > right
+            isnum = True
+        else:  # match is alpha
+            match_left = R_ALPHA.match(left)
+            match_right = R_ALPHA.match(right)
+            if not match_right:  # right is not an alpha and nums > alphas
+                return -1  # left < right
+            isnum = False
+
+        # strip off the leading numeric or alpha chars
+        left_head, left = match_left.group(1), match_left.group(2)
+        right_head, right = match_right.group(1), match_right.group(2)
+
+        if isnum:
+            left_head = left_head.lstrip(b'0')
+            right_head = right_head.lstrip(b'0')
+
+            # Length of contiguous numbers matters
+            left_head_len = len(left_head)
+            right_head_len = len(right_head)
+            if left_head_len < right_head_len:
+                return -1  # left < right
+            if left_head_len > right_head_len:
+                return 1  # left > right
+
+        # Either a number with the same number of chars or
+        # the leading chars are alpha so lets do a standard compare
+        if left_head < right_head:
+            return -1  # left < right
+        if left_head > right_head:
+            return 1  # left > right
+
+        # Both header segments are of equal length, keep going with the new
+        continue  # pragma: no cover
+
+    # if both are now zero length they must be equal
+    if len(left) == len(right) == 0:
+        return 0  # left == right
+
+    # Longer string is > than shorter string
+    if len(left) != 0:
+        return 1  # left > right
+
+    return -1  # left < right
--- fs_image/rpm/tests/test_rpm_metadata.py
+++ fs_image/rpm/tests/test_rpm_metadata.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
+import importlib.resources
 import os
+import re
 import shutil
 import unittest
 
 from .temp_repos import temp_repos_steps, Repo, Rpm
 from ..common import temp_dir
-from ..rpm_metadata import compare_rpm_versions, RpmMetadata
+from ..rpm_metadata import compare_rpm_versions, RpmMetadata, _compare_values
 from find_built_subvol import find_built_subvol
 
 
 class RpmMetadataTestCase(unittest.TestCase):
+
+    def _load_canonical_tests(self):
+        STMT = re.compile(
+            r"(.*)RPMVERCMP\(([^, ]*) *, *([^, ]*) *, *([^\)]*)\).*")
+
+        for line in importlib.resources.open_text(
+                'rpm', 'version-compare-tests').readlines():
+            m = STMT.match(line)
+            if m:
+                yield m.group(2), m.group(3), int(m.group(4))
+
     def test_rpm_metadata_from_subvol(self):
         layer_path = os.path.join(os.path.dirname(__file__), 'child-layer')
         child_subvol = find_built_subvol(layer_path)
@@ -69,7 +82,7 @@ class RpmMetadataTestCase(unittest.TestCase):
         #
         # This number of test cases is excessive but does show how interesting
         # RPM version comparisons can be.
-        test_data = [
+        test_evr_data = [
             # Non-alphanumeric (except ~) are ignored for equality
             ((1, '2', '3'), (1, '2', '3'), 0),      # 1:2-3 == 1:2-3
             ((1, ':2>', '3'), (1, '-2-', '3'), 0),  # 1::2>-3 == 1:-2--3
@@ -85,12 +98,22 @@ class RpmMetadataTestCase(unittest.TestCase):
             ((0, '^1', '3'), (0, '^', '3'), 1),        # 0:^1-3 > 0:^-3
             ((0, '^', '3'), (0, '^1', '3'), -1),       # 0:^-3 < 0:^1-3
             ((0, '0333', 'b'), (0, '0033', 'b'), 1),   # 0:0333-b > 0:0033-b
+            ((0, '0033', 'b'), (0, '0333', 'b'), -1),  # 0:0033-b < 0:0333-b
             ((0, '3', '~~'), (0, '3', '~~~'), 1),      # 0:3-~~ > 0:3-~~~
+            ((0, '3', '~~~'), (0, '3', '~~'), -1),     # 0:3-~~~ < 0:3-~~
+            ((0, '3', '~~~'), (0, '3', '~~~'), 0),     # 0:3-~~~ == 0:3-~~~
             ((0, 'a2aa', 'b'), (0, 'a2a', 'b'), 1),    # 0:a2aa-b > 0:a2a-b
             ((0, '33', 'b'), (0, 'aaa', 'b'), 1),      # 0:33-b > 0:aaa-b
         ]
 
-        for evr1, evr2, expected in test_data:
+        for evr1, evr2, expected in test_evr_data:
             a = RpmMetadata('test-name', *evr1)
             b = RpmMetadata('test-name', *evr2)
-            self.assertEqual(compare_rpm_versions(a, b), expected)
+            self.assertEqual(compare_rpm_versions(a, b),
+                    expected, f'failed: {evr1}, {evr2}, {expected}')
+
+        # Test against some more canonical tests.  These are derived from
+        # actual tests used for rpm itself.
+        for ver1, ver2, expected in self._load_canonical_tests():
+            self.assertEqual(_compare_values(ver1, ver2),
+                    expected, f'failed: {ver1}, {ver2}, {expected}')
