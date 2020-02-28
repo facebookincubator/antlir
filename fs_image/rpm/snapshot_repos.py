0a96626c68219e8395c56b33bec0b578cc13a994
Justin Trudell <jtru@fb.com>
2020-02-27 20:03:27 -0800

    Add exclude support to snapshot_repos
    Summary: Adds support to exclude specific repos from `snapshot_repos`.
    
    Reviewed By: snarkmaster
    
    Differential Revision: D20133350
    
    fbshipit-source-id: dc240e57aeb308ec44c4361632f2d085f7cab3e5

--- fs_image/rpm/snapshot_repos.py
+++ fs_image/rpm/snapshot_repos.py
@@ -27,8 +27,9 @@ import json
 import os
 import sys
 
+from configparser import ConfigParser
 from io import StringIO
-from typing import Callable, Dict, Iterable, List
+from typing import Callable, Dict, FrozenSet, Iterable, List
 
 from fs_image.common import get_file_logger
 
@@ -48,17 +49,32 @@ log = get_file_logger(__file__)
 
 
 def _write_confs_get_repos(
-    dest: Path, yum_conf_content: str, dnf_conf_content: str,
+    dest: Path,
+    yum_conf_content: str,
+    dnf_conf_content: str,
+    *,
+    exclude_repos: FrozenSet[str],
 ) -> Iterable[YumDnfConfRepo]:
+    assert not (exclude_repos & {'main', 'DEFAULT'}), exclude_repos
     yum_dnf_repos = []
     for out_name, content in [
         ('yum.conf', yum_conf_content), ('dnf.conf', dnf_conf_content),
     ]:
         if content is not None:
-            with create_ro(dest / out_name, 'w') as out:
+            # Save the original, unmodified config in case of an error
+            with create_ro(dest / (out_name + '.orig'), 'w') as out:
                 out.write(content)
+            # Remove the excluded repos
+            cp = ConfigParser()
+            cp.read_string(content)
+            for excluded in exclude_repos:
+                cp.remove_section(excluded)
+            with create_ro(dest / out_name, 'w+') as out:
+                cp.write(out)
+                out.seek(0)
+                new_content = out.read()
             yum_dnf_repos.append(set(
-                YumDnfConfParser(YumDnf.dnf, StringIO(content)).gen_repos()
+                YumDnfConfParser(YumDnf.dnf, StringIO(new_content)).gen_repos()
             ))
     yum_repos, dnf_repos = yum_dnf_repos
     diff_repos = yum_repos.symmetric_difference(dnf_repos)
@@ -88,16 +104,20 @@ def snapshot_repos(
     storage_cfg: Dict[str, str],
     rpm_shard: RpmShard,
     gpg_key_whitelist_dir: str,
+    exclude: FrozenSet[str],
 ):
     declared_sizer = RepoSizer()
     saved_sizer = RepoSizer()
-    repos = _write_confs_get_repos(dest, yum_conf_content, dnf_conf_content)
+    repos = _write_confs_get_repos(
+        dest, yum_conf_content, dnf_conf_content, exclude_repos=exclude
+    )
     os.mkdir(dest / 'repos')
     repos_and_universes = [
         # Evaluated eagerly for `all_snapshot_universes`.  Bonus: this also
         # fails fast if some repos cannot be resolved.
         (repo, validate_universe_name(repo_to_universe(repo)))
             for repo in repos
+            if repo.name not in exclude
     ]
     with RepoSnapshot.add_sqlite_to_storage(
         Storage.from_json(storage_cfg), dest
@@ -145,6 +165,10 @@ def snapshot_repos_from_args(argv: List[str]):
         '--yum-conf', type=Path.from_argparse,
         help='Snapshot this `yum.conf`; see help for `--dnf-conf`',
     )
+    parser.add_argument(
+        '--exclude', action='append', default=[],
+        help='Repos to be excluded in the snapshot.',
+    )
 
     universe_warning = (
         'Described in the `repo_db.py` docblock. In production, it is '
@@ -189,6 +213,7 @@ def snapshot_repos_from_args(argv: List[str]):
             storage_cfg=args.storage,
             rpm_shard=args.rpm_shard,
             gpg_key_whitelist_dir=args.gpg_key_whitelist_dir,
+            exclude=frozenset(args.exclude),
         )
 
 
