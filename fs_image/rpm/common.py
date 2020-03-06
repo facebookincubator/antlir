81975185e55f8e01d67b76a1b77d98761b86d89d
Nathan Jiang <natjiang@fb.com>
2020-03-05 16:58:28 -0800

    Eliminate dependency on `"//libfb/py/asyncio:decorators`
    Summary:
    Needed to eliminate dependency on `libfb` in order to move `kernel/vm` to OSS; therefore...
    - Added an `async_retryable` decorator to `//fs_image/rpm:common`
    - Replaced dependency on `//libfb/py/asyncio:decorators` and use new `async_retryable`.
    - Added unittests for the `async_retryable` decorator in `test_common.py`
    
    Reviewed By: justintrudell
    
    Differential Revision: D20230233
    
    fbshipit-source-id: 75e34d08b4cdcf1c9d6876d00c6d2323ef32e57c

--- fs_image/rpm/common.py
+++ fs_image/rpm/common.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import hashlib
 import inspect
+import logging
 import os
 import shutil
 import struct
@@ -31,12 +32,14 @@ def retry_fn(
     *,
     delays: Optional[Iterable[float]] = None,
     what: str,
+    log_exception: bool = True,
 ) -> T:
     '''Allows functions to be retried `len(delays)` times, with each iteration
     sleeping for its respective index into `delays`. `is_exception_retryable`
     is an optional function that takes the raised exception and potentially
     evaluate to False, at which case no retry will occur and the exception will
-    be re-raised.
+    be re-raised. If the exception is not re-raised, the retry message will be
+    logged to either DEBUG or ERROR depending whether `log_exception` is True.
 
     Delays are in seconds.
     '''
@@ -48,9 +51,11 @@ def retry_fn(
         except Exception as e:
             if is_exception_retryable and not is_exception_retryable(e):
                 raise
-            log.exception(
+            log.log(
+                logging.ERROR if log_exception else logging.DEBUG,
                 f'\n\n[Retry {i + 1} of {len(delays)}] {what} -- waiting '
-                f'{delay} seconds.\n\n'
+                f'{delay} seconds.\n\n',
+                exc_info=log_exception,
             )
             time.sleep(delay)
     return retryable_fn()  # With 0 retries, we should still run the function.
@@ -61,11 +66,13 @@ def retryable(
     delays: Iterable[float],
     *,
     is_exception_retryable: Optional[Callable[[Exception], bool]] = None,
+    log_exception: bool = True,
 ):
     '''Decorator used to retry a function if exceptions are thrown. `format_msg`
     should be a format string that can access any args provided to the
     decorated function. `delays` are the delays between retries, in seconds.
-    `is_exception_retryable` is forwarded to `retry_fn`, see its docblock.
+    `is_exception_retryable` and `log_exception` are forwarded to `retry_fn`,
+    see its docblock.
     '''
     def wrapper(fn):
         @wraps(fn)
@@ -75,7 +82,65 @@ def retryable(
                 lambda: fn(*args, **kwargs),
                 is_exception_retryable,
                 delays=delays,
-                what=format_msg.format(**fn_args)
+                what=format_msg.format(**fn_args),
+                log_exception=log_exception
+            )
+        return decorated
+    return wrapper
+
+
+async def async_retry_fn(
+    retryable_fn: Callable[[], T],
+    is_exception_retryable: Optional[Callable[[Exception], bool]] = None,
+    *,
+    delays: Optional[Iterable[float]] = None,
+    what: str,
+    log_exception: bool = True,
+) -> T:
+    ''' Similar to retry_fn except the function is executed asynchronously.
+    See retry_fn docblock for details.
+    '''
+    if delays is None:
+        delays = []
+    for i, delay in enumerate(delays):
+        try:
+            return await retryable_fn()
+        except Exception as e:
+            if is_exception_retryable and not is_exception_retryable(e):
+                raise
+            log.log(
+                logging.ERROR if log_exception else logging.DEBUG,
+                f'\n\n[Retry {i + 1} of {len(delays)}] {what} -- waiting '
+                f'{delay} seconds.\n\n',
+                exc_info=log_exception,
+            )
+            time.sleep(delay)
+    return await retryable_fn()  # With 0 retries, we should still run the function.
+
+
+def async_retryable(
+    format_msg: str,
+    delays: Iterable[float],
+    *,
+    is_exception_retryable: Optional[Callable[[Exception], bool]] = None,
+    log_exception: bool = True,
+):
+    '''Decorator used to retry an asynchronous function if exceptions are
+    thrown. `format_msg` should be a format string that can access any args
+    provided to the decorated function. `delays` are the delays between
+    retries, in seconds. `is_exception_retryable` and `log_exception` are
+    forwarded to `async_retry_fn`, see its docblock.
+    '''
+    def wrapper(fn):
+        @wraps(fn)
+        async def decorated(*args, **kwargs):
+            fn_args = inspect.getcallargs(fn, *args, **kwargs)
+            return await async_retry_fn(
+                lambda: fn(*args, **kwargs),
+                is_exception_retryable,
+                delays=delays,
+                what=format_msg.format(**fn_args),
+                log_exception=log_exception
             )
         return decorated
     return wrapper
