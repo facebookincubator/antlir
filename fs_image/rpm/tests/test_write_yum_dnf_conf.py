6968515c89d7aa55cc1c964f47bef9cb529c6768
Alexey Spiridonov <lesha@fb.com>
2020-03-20 15:57:18 -0700

    Generate yum.conf as part of the snapshot
    Summary:
    This collects the config-generation logic that's currently duplicated between `rpm/yum_dnf_from_snapshot.py` and `nspawn_in_subvol/repo_server.py` and puts it in `write_yum_dnf_conf.py`.
    
    We then use the binary to make the rewritten configs part of the `rpm_repo_snapshot` build artifact.
    
    This is great for two reasons:
     - We can delete the duplicated logic (in a stacked diff).
     - It is now clear that the config we use is fully deterministic, and easy to inspect via `buck-out`. The "deterministic" part is key for making our handling of `yum` / `dnf` caches fully robust.
    
    Reviewed By: filbranden
    
    Differential Revision: D20457517
    
    fbshipit-source-id: 0e4eddc3384c15eaab7d0637fc9461537a72a202

---
+++ fs_image/rpm/tests/test_write_yum_dnf_conf.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import textwrap
+import unittest
+
+from fs_image.fs_utils import create_ro, temp_dir
+from rpm.yum_dnf_conf import YumDnf
+
+from .. import write_yum_dnf_conf as wydc
+
+_CONF_IN = '''\
+# Unfortunately, comments are discarded by ConfigParser, but I don't want to
+# depend on `ConfigObj` or `iniparse` for this.
+[main]
+debuglevel=2
+gpgcheck=1
+
+[potato]
+baseurl=https://one.example.com/something-or-other
+enabled=1
+
+[oleander]
+baseurl=http://example.com/this-is-a-flowering-bush
+gpgkey=https://example.com/zupa
+\thttps://example.com/super/safe
+enabled=1
+'''
+
+_CONF_OUT = '''\
+[main]
+debuglevel = 2
+gpgcheck = 1
+cachedir = /var/cache/{prog_name}
+persistdir = /var/lib/{prog_name}
+reposdir = /dev/null
+logfile = /var/log/{prog_name}.log
+config_file_path = /INSTALL/DIR/{prog_name}/{prog_name}.conf
+timeout = 60
+plugins = 1
+pluginpath = /INSTALL/DIR/{prog_name}/plugins
+pluginconfpath = /INSTALL/DIR/{prog_name}/plugins
+varsdir = /dev/null
+usercache = 0
+syslog_device =\x20
+bugtracker_url =\x20
+fssnap_devices = !*
+{extra_directives}\
+
+[potato]
+baseurl = http://localhost:1234/potato
+\thttp://localhost:5678/potato
+enabled = 1
+gpgkey =''' + ' ' + '''
+
+[oleander]
+baseurl = http://localhost:1234/oleander
+\thttp://localhost:5678/oleander
+gpgkey = http://localhost:1234/oleander/zupa
+\thttp://localhost:1234/oleander/safe
+enabled = 1
+
+'''
+
+
+# This is the base class for two test classes at the bottom of the file.
+class WriteYumDnfConfTestImpl:
+
+    def test_conf(self):
+        install_dir = '/INSTALL/DIR'
+        prog_name = self._YUM_DNF.value
+        expected_out = _CONF_OUT.format(
+            prog_name=prog_name,
+            extra_directives=textwrap.dedent('''\
+                skip_missing_names_on_install = 0
+                skip_missing_names_on_update = 0
+            ''') if self._YUM_DNF == YumDnf.yum else '',
+        )
+        with temp_dir() as td:
+            with create_ro(td / 'in', 'w') as outf:
+                outf.write(_CONF_IN)
+            wydc.main([
+                f'--output-dir={td / "out"}',
+                f'--install-dir={install_dir}',
+                *('--write-conf', self._YUM_DNF.value, td / 'in', '1234 5678'),
+            ])
+            with open(td / f'out/{prog_name}/{prog_name}.conf') as infile:
+                self.assertEqual(expected_out, infile.read())
+
+
+class WriteDnfConfTest(WriteYumDnfConfTestImpl, unittest.TestCase):
+    _YUM_DNF = YumDnf.dnf
+
+
+class WriteYumConfTest(WriteYumDnfConfTestImpl, unittest.TestCase):
+    _YUM_DNF = YumDnf.yum
