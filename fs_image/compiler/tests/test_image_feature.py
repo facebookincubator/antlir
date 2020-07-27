5920235575aa666da3b2af1ea3cede1d3f8907b0
Maxim Patlasov <mpatlasov@fb.com>
2020-07-27 10:47:48 -0700

    fix test_install_order -- "foo/bar" can be the second
    Summary: The diff fixes a bug introduced in D22451214. Now, when we have three mounts (dev_null, host_etc, meownt), The index of foo/bar can be the second, the test must support it.
    
    Reviewed By: justintrudell
    
    Differential Revision: D22757157
    
    fbshipit-source-id: 764845d40e41c4bac93f83b2d1f4a263129b4771

--- fs_image/compiler/tests/test_image_feature.py
+++ fs_image/compiler/tests/test_image_feature.py
@@ -130,7 +130,7 @@ class ImageFeatureTestCase(unittest.TestCase):
             if v not in phase_items
         }
         # The 3 mounts are not ordered in any way with respect to the
-        # `foo/bar` tree, so any of these 3 can be the first.
+        # `foo/bar` tree, so any of these 4 can be the first.
         mount_idxs = sorted(
             [id_to_idx["dev_null"], id_to_idx["host_etc"], id_to_idx["meownt"]]
         )
@@ -138,6 +138,8 @@ class ImageFeatureTestCase(unittest.TestCase):
             self.assertEqual(3, id_to_idx["foo/bar"])
         elif 0 in mount_idxs and 1 in mount_idxs:
             self.assertEqual(2, id_to_idx["foo/bar"])
+        elif 0 in mount_idxs:
+            self.assertEqual(1, id_to_idx["foo/bar"])
         else:
             self.assertEqual(0, id_to_idx["foo/bar"])
         self.assertLess(
