1651f8afda01503df3055e642d7d05c2726ed311
Vinnie Magro <vmagro@fb.com>
2020-09-22 11:42:53 -0700

    antlir: fix docs build
    Summary: This was broken due to a deleted page being referenced in the sidebar
    
    Reviewed By: justintrudell
    
    Differential Revision: D23820710
    
    fbshipit-source-id: 95258ca8a93e64269fd9fb3ec230a388fb7068e2

--- website/sidebars.js
+++ website/sidebars.js
@@ -19,37 +19,15 @@ module.exports = {
       'coding-conventions/pyre',
       'coding-conventions/python',
     ],
-    TODOs: [
-      'todo/overview',
-      'todo/btrfs_diff',
-      'todo/compiler',
-    ],
-    Tutorials: [
-      ...FBInternalOnly([
-        'tutorials/fb/contributing-docs',
-        'tutorials/fb/bumping-the-systemd-version-in-base-images',
-      ]),
-    ],
+    TODOs: ['todo/overview', 'todo/btrfs_diff', 'todo/compiler'],
+    Tutorials: [...FBInternalOnly(['tutorials/fb/contributing-docs'])],
     RPMs: [
       ...FBInternalOnly([
         'rpms/fb/overview',
-        'rpms/fb/debugging-stale-snapshots',
         'rpms/fb/version-selection-in-buck-built-images',
-        'rpms/fb/mutable-rpm-investigation',
       ]),
     ],
     ...FBInternalOnly({
-      Infrastructure: [
-        'fb/infra/non-buck-target-determinators',
-        'fb/infra/chronos-based-automation',
-        'fb/infra/hiding-internal-edges-in-targets',
-        'fb/infra/making-sure-older-builds-work',
-      ],
-      'Automatic Repo Updates': [
-        'fb/automatic-repo-updates/overview',
-        'fb/automatic-repo-updates/fbpkg-auto-preservation',
-        'fb/automatic-repo-updates/handling-manual-rpm-snapshots',
-      ],
       Fbpkg: [
         'fb/fbpkg/overview',
         {
