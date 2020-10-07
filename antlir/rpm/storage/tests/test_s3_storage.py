9e4b6c745f9348e010cc6d5be56cd62da0e59bb5
Vinnie Magro <vmagro@fb.com>
2020-10-06 19:04:30 -0700

    antlir: rewrite s3 storage to use boto3
    Summary:
    Previously this was shelling out to the AWS cli, boto3 is much faster and more
    reliable.
    
    Reviewed By: zeroxoneb
    
    Differential Revision: D24147568
    
    fbshipit-source-id: 70fcc556232d78eab42345bf63c341a273f643ec

--- antlir/rpm/storage/tests/test_s3_storage.py
+++ antlir/rpm/storage/tests/test_s3_storage.py
@@ -4,103 +4,91 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import logging
-import os
-import unittest.mock
-from functools import wraps
+import io
+from contextlib import contextmanager
+from unittest.mock import ANY, MagicMock, patch
 
-from antlir.fs_utils import Path, temp_dir
-
-from ..s3_storage import S3Storage
-from .cli_object_storage_base_test import CLIObjectStorageBaseTestCase
-from .storage_base_test import Storage
+from .. import s3_storage
+from .storage_base_test import Storage, StorageBaseTestCase
 
 
-def mock_s3_cli(fn):
-    @wraps(fn)
-    def mock(*args, **kwargs):
-        with temp_dir() as td, Path.resource(
-            __package__, "mock-s3-cli", exe=True
-        ) as mock_s3_cli_path:
-            # We mock `_path_for_storage_id` such that the base dir
-            # is always going to be the TempDir we created
-            def _mock_path_for_storage_id(sid):
-                return (td / sid).decode()
+class S3StorageTestCase(StorageBaseTestCase):
+    bucket = "antlir-test"
+    region = "test-region"
+    prefix = "test/prefix"
 
-            # Instead of calls to `aws s3`, we want to call `mock-s3-cli`
-            with unittest.mock.patch.object(
-                S3Storage, "_base_cmd", return_value=[mock_s3_cli_path]
-            ), unittest.mock.patch.object(
-                S3Storage,
-                "_path_for_storage_id",
-                side_effect=_mock_path_for_storage_id,
-            ):
-                return fn(*args, **kwargs)
+    # some methods to mock an in-memory s3 bucket
+    # this matches the read-after-write consistency guarantee from the real s3
+    def _mock_upload_fileobj(self, contents, bucket: str, key: str):
+        assert bucket == self.bucket
+        self.objects[key] = contents.getbuffer()
 
-    return mock
+    def _mock_delete_object(self, bucket: str, key: str):
+        assert bucket == self.bucket
+        del self.objects[key]
 
+    @contextmanager
+    def _mock_open_url(self, url: str):
+        urlprefix = f"https://{self.bucket}.s3-{self.region}.amazonaws.com/"
+        assert url.startswith(urlprefix)
+        key = url[len(urlprefix) :]
+        yield io.BytesIO(self.objects[key])
 
-class S3StorageTestCase(CLIObjectStorageBaseTestCase):
     def setUp(self):
-        logging.getLogger().setLevel(logging.DEBUG)
-        self.storage = Storage.make(key="test", kind="s3", timeout_seconds=3)
-
-    def _decorate_id(self, sid: str) -> str:
-        # In the case of S3, we are using our `mock-s3-cli`
-        # which is automatically configured to write to a tmp
-        # directory; therefore, there is no need to decorate
-        # `sid`s to force the storage to use a test directory
-        return sid
-
-    @mock_s3_cli
-    def test_write_and_read_back(self):
-        self._test_write_and_read_back(S3Storage)
-
-    @mock_s3_cli
-    def test_uncommitted(self):
-        proc = self._test_uncommited(S3Storage)
-        self.assertFalse(proc.stdout)
-
-    @mock_s3_cli
-    def test_error_cleanup(self):
-        # Re-patch the _path_for_storage_id method to always
-        # return a path that doesn't exist.
-        def _mock_path_for_storage_id(sid):
-            return os.path.join("/not/a/valid/path", sid)
-
-        with unittest.mock.patch.object(
-            S3Storage,
-            "_path_for_storage_id",
-            side_effect=_mock_path_for_storage_id,
-        ):
-            self._test_error_cleanup("s3")
-
-    def test_base_cmd(self):
-        self.assertEqual(
-            Storage.make(key="test", kind="s3", timeout_seconds=123)._base_cmd(
-                "--cat", "meow"
-            ),
-            [
-                "aws",
-                "s3",
-                "--cli-read-timeout",
-                "123",
-                "--cli-connect-timeout",
-                "123",
-                "--cat",
-                "meow",
-            ],
+        self.objects = {}
+        self.s3 = MagicMock()
+        self.s3.upload_fileobj.side_effect = self._mock_upload_fileobj
+        self.s3.delete_object.side_effect = self._mock_delete_object
+        s3_storage.boto3 = MagicMock()
+        s3_storage.boto3.client.return_value = self.s3
+        self.storage = Storage.make(
+            key="test",
+            kind="s3",
+            bucket=self.bucket,
+            prefix=self.prefix,
+            region=self.region,
+            timeout_seconds=3,
         )
+        s3_storage.boto3.client.assert_called_with("s3")
+        self.assertEqual(self.storage.s3, self.s3)
+        s3_storage.open_url = self._mock_open_url
 
-    def test_path_for_storage_id(self):
-        s3_bucket_name = "s3://cats-go-meow"
-        blob_sid = "123"
-        with unittest.mock.patch.object(
-            S3Storage, "AWS_S3_BUCKET", s3_bucket_name
-        ):
-            self.assertEqual(
-                Storage.make(key="test", kind="s3")._path_for_storage_id(
-                    blob_sid
-                ),
-                os.path.join(s3_bucket_name, "flat", blob_sid),
+    def test_write_and_read_back(self):
+        # Do a bunch of mock writes and ensure that the s3 client was called
+        # with each storage id s3 key.
+        # The s3 client does not do any partial writes or buffering, so as long
+        # as it is called with the correct contents, that is enough to pass.
+        for contents, sid in self.check_storage_impl(self.storage):
+            self.s3.upload_fileobj.assert_any_call(
+                ANY, "antlir-test", self.storage._object_key(sid)
             )
+            # ensure that the written data was sent to s3 correctly, as one
+            # blob regardless of how many chunks it was input as
+            with self.storage.reader(sid) as f:
+                actual = f.read()
+                self.assertEqual(actual, b"".join(contents))
+
+    def test_delete(self):
+        with self.storage.writer() as out:
+            out.write(b"Hello world!")
+            sid = out.commit()
+        key = self.storage._object_key(sid)
+        self.assertIn(key, self.objects)
+        self.storage.remove(sid)
+        self.assertNotIn(key, self.objects)
+        self.s3.delete_object.called_with(self.bucket, key)
+
+    def test_reader_url(self):
+        with patch("antlir.rpm.storage.s3_storage.open_url") as open_url:
+            with self.storage.reader("1234") as _:
+                pass
+            open_url.assert_called_with(
+                "https://antlir-test.s3-test-region.amazonaws.com/"
+                "test/prefix/1234"
+            )
+
+    def test_object_key(self):
+        blob_sid = "123"
+        self.assertEqual(
+            self.storage._object_key(blob_sid), f"test/prefix/{blob_sid}"
+        )
