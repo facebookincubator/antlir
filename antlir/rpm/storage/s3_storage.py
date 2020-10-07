2d25e1a2fc17385bc920ff8a7a7db552f4552d0d
vmagro <>
2020-10-06 18:19:40 -0700

    antlir: rewrite s3 storage to use boto3
    Summary:
    Previously this was shelling out to the AWS cli, boto3 is much faster and more
    reliable.
    
    Differential Revision: D24147568
    
    fbshipit-source-id: 8ae94219bc308e871f913c0f25c6758db3d028ef

--- antlir/rpm/storage/s3_storage.py
+++ antlir/rpm/storage/s3_storage.py
@@ -4,51 +4,85 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import os
-from typing import List
+import io
+import logging
+import uuid
+from contextlib import contextmanager
+from typing import ContextManager
 
-from .cli_object_storage import CLIObjectStorage
+import boto3
+from antlir.common import get_file_logger
+from antlir.rpm.storage.storage import _CommitCallback
+
+from ..open_url import open_url
+from ..storage import Storage, StorageInput, StorageOutput
 
 
-class S3Storage(CLIObjectStorage, plugin_kind="s3"):
+log = get_file_logger(__file__)
 
-    AWS_S3_BUCKET = "s3://AWS_S3_BUCKET"
-    AWS_ACCESS_KEY_ID = "AWS_ACCESS_KEY_ID"
-    AWS_SECRET_ACCESS_KEY = "AWS_SECRET_ACCESS_KEY"
+boto3.set_stream_logger("", logging.WARNING)
 
-    def __init__(self, *, key: str, timeout_seconds: float = 400):
+
+class S3Storage(Storage, plugin_kind="s3"):
+    def __init__(
+        self,
+        *,
+        key: str,
+        bucket: str,
+        prefix: str,
+        region: str,
+        timeout_seconds: float = 400,
+    ):
         self.key = key
+        self.bucket = bucket
+        self.prefix = prefix
+        self.region = region
         self.timeout_seconds = timeout_seconds
+        # Reads require no credentials, writes go through AWS authentication
+        # and will fail if the required environment variables are not set
+        # Full details here, but in practice it's ok to ignore the intricacies.
+        # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/credentials.html
+        self.s3 = boto3.client("s3")
 
-    def _path_for_storage_id(self, sid: str) -> str:
-        return os.path.join(self.AWS_S3_BUCKET, "flat", sid)
+    @classmethod
+    def _make_storage_id(cls) -> str:
+        return str(uuid.uuid4()).replace("-", "")
 
-    def _base_cmd(self, *args) -> List[str]:
-        return [
-            "aws",
-            "s3",
-            "--cli-read-timeout",
-            str(self.timeout_seconds),
-            "--cli-connect-timeout",
-            str(self.timeout_seconds),
-            *args,
-        ]
+    def _object_key(self, sid: str) -> str:
+        key = sid
+        if ":" in key:
+            key = self.strip_key(sid)
+        return f"{self.prefix}/{key}"
 
-    def _read_cmd(self, *args, path: str) -> List[str]:
-        return self._base_cmd(*args) + ["cp", path, "-"]
+    @contextmanager
+    def reader(self, sid: str) -> ContextManager[StorageInput]:
+        url = (
+            f"https://{self.bucket}.s3-{self.region}.amazonaws.com/"
+            + self._object_key(sid)
+        )
+        with open_url(url) as f:
+            yield StorageInput(input=f)
 
-    def _write_cmd(self, *args, path: str) -> List[str]:
-        return self._base_cmd(*args) + ["cp", "-", path]
+    @contextmanager
+    def writer(self) -> ContextManager[StorageOutput]:
+        sid = self._make_storage_id()
+        key = self._object_key(sid)
+        log_prefix = f"{self.__class__.__name__}"
+        log.debug(f"{log_prefix} - Writing to {key}")
 
-    def _remove_cmd(self, *args, path: str) -> List[str]:
-        return self._base_cmd(*args) + ["rm", path]
+        buf = io.BytesIO()
 
-    def _exists_cmd(self, *args, path: str) -> List[str]:
-        return self._base_cmd(*args) + ["ls", path]
+        @contextmanager
+        def get_id_and_release_resources():
+            buf.seek(0)
+            self.s3.upload_fileobj(buf, self.bucket, key)
+            # S3 does not do partial puts, if the request returned 200, then
+            # there is read-after-write guarantees
+            yield sid
 
-    def _configured_env(self):
-        # Configure env with AWS credentials
-        env = os.environ.copy()
-        env["AWS_ACCESS_KEY_ID"] = self.AWS_ACCESS_KEY_ID
-        env["AWS_SECRET_ACCESS_KEY"] = self.AWS_SECRET_ACCESS_KEY
-        return env
+        with _CommitCallback(self, get_id_and_release_resources) as commit:
+            yield StorageOutput(output=buf, commit_callback=commit)
+
+    def remove(self, sid: str) -> None:
+        key = self._object_key(sid)
+        self.s3.delete_object(self.bucket, key)
