cfe6231c251ae459ad7cac53631aab0a699125e7
Justin Trudell <jtru@fb.com>
2020-10-15 10:32:39 -0700

    Move retryable to common
    Summary:
    I plan to use this in upcoming diff from outside of `rpm/` so am generalizing it here.
    
    Also, this suite of functions is not RPM specific and potentially useful in other scenarios, so I think it makes sense to open it up.
    
    Reviewed By: vmagro
    
    Differential Revision: D24289506
    
    fbshipit-source-id: c88ff88cb6cdcabbd773234115768adc48606a40

--- antlir/rpm/common.py
+++ antlir/rpm/common.py
@@ -5,24 +5,12 @@
 # LICENSE file in the root directory of this source tree.
 
 import hashlib
-import inspect
-import logging
 import os
 import shutil
 import struct
-import time
 from contextlib import AbstractContextManager
-from functools import wraps
 from io import BytesIO
-from typing import (
-    Callable,
-    ContextManager,
-    Generic,
-    Iterable,
-    NamedTuple,
-    Optional,
-    TypeVar,
-)
+from typing import ContextManager, Generic, Iterable, NamedTuple, TypeVar
 
 from antlir.common import byteme, get_file_logger
 
@@ -30,7 +18,6 @@ from antlir.common import byteme, get_file_logger
 log = get_file_logger(__file__)
 _UINT64_STRUCT = struct.Struct("=Q")
 T = TypeVar("T")
-_mockable_retry_fn_sleep = time.sleep
 
 
 class DecorateContextEntry(Generic[T], AbstractContextManager):
@@ -45,130 +32,6 @@ class DecorateContextEntry(Generic[T], AbstractContextManager):
 
     def __exit__(self, *args, **kwargs):
         return self._ctx_mgr.__exit__(*args, **kwargs)
-
-
-def retry_fn(
-    retryable_fn: Callable[[], T],
-    is_exception_retryable: Optional[Callable[[Exception], bool]] = None,
-    *,
-    delays: Iterable[float],
-    what: str,
-    log_exception: bool = True,
-) -> T:
-    """Allows functions to be retried `len(delays)` times, with each iteration
-    sleeping for its respective index into `delays`. `is_exception_retryable`
-    is an optional function that takes the raised exception and potentially
-    evaluate to False, at which case no retry will occur and the exception will
-    be re-raised. If the exception is not re-raised, the retry message will be
-    logged to either DEBUG or ERROR depending whether `log_exception` is True.
-
-    Delays are in seconds.
-    """
-    for i, delay in enumerate(delays):
-        try:
-            return retryable_fn()
-        except Exception as e:
-            if is_exception_retryable and not is_exception_retryable(e):
-                raise
-            log.log(
-                logging.ERROR if log_exception else logging.DEBUG,
-                f"\n\n[Retry {i + 1} of {len(delays)}] {what} -- waiting "
-                f"{delay} seconds.\n\n",
-                exc_info=log_exception,
-            )
-            _mockable_retry_fn_sleep(delay)
-    return retryable_fn()  # With 0 retries, we should still run the function.
-
-
-def retryable(
-    format_msg: str,
-    delays: Iterable[float],
-    *,
-    is_exception_retryable: Optional[Callable[[Exception], bool]] = None,
-    log_exception: bool = True,
-):
-    """Decorator used to retry a function if exceptions are thrown. `format_msg`
-    should be a format string that can access any args provided to the
-    decorated function. `delays` are the delays between retries, in seconds.
-    `is_exception_retryable` and `log_exception` are forwarded to `retry_fn`,
-    see its docblock.
-    """
-
-    def wrapper(fn):
-        @wraps(fn)
-        def decorated(*args, **kwargs):
-            fn_args = inspect.getcallargs(fn, *args, **kwargs)
-            return retry_fn(
-                lambda: fn(*args, **kwargs),
-                is_exception_retryable,
-                delays=delays,
-                what=format_msg.format(**fn_args),
-                log_exception=log_exception,
-            )
-
-        return decorated
-
-    return wrapper
-
-
-async def async_retry_fn(
-    retryable_fn: Callable[[], T],
-    is_exception_retryable: Optional[Callable[[Exception], bool]] = None,
-    *,
-    delays: Iterable[float],
-    what: str,
-    log_exception: bool = True,
-) -> T:
-    """Similar to retry_fn except the function is executed asynchronously.
-    See retry_fn docblock for details.
-    """
-    for i, delay in enumerate(delays):
-        try:
-            return await retryable_fn()
-        except Exception as e:
-            if is_exception_retryable and not is_exception_retryable(e):
-                raise
-            log.log(
-                logging.ERROR if log_exception else logging.DEBUG,
-                f"\n\n[Retry {i + 1} of {len(delays)}] {what} -- waiting "
-                f"{delay} seconds.\n\n",
-                exc_info=log_exception,
-            )
-            time.sleep(delay)
-    return (
-        await retryable_fn()
-    )  # With 0 retries, we should still run the function.
-
-
-def async_retryable(
-    format_msg: str,
-    delays: Iterable[float],
-    *,
-    is_exception_retryable: Optional[Callable[[Exception], bool]] = None,
-    log_exception: bool = True,
-):
-    """Decorator used to retry an asynchronous function if exceptions are
-    thrown. `format_msg` should be a format string that can access any args
-    provided to the decorated function. `delays` are the delays between
-    retries, in seconds. `is_exception_retryable` and `log_exception` are
-    forwarded to `async_retry_fn`, see its docblock.
-    """
-
-    def wrapper(fn):
-        @wraps(fn)
-        async def decorated(*args, **kwargs):
-            fn_args = inspect.getcallargs(fn, *args, **kwargs)
-            return await async_retry_fn(
-                lambda: fn(*args, **kwargs),
-                is_exception_retryable,
-                delays=delays,
-                what=format_msg.format(**fn_args),
-                log_exception=log_exception,
-            )
-
-        return decorated
-
-    return wrapper
 
 
 class RpmShard(NamedTuple):
