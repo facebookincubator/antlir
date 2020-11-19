a42f0b22f07a5dd20243483e64488a9e10edda34
Vinnie Magro <vmagro@fb.com>
2020-11-19 10:44:49 -0800

    bzldoc: docusaurus plugin
    Summary:
    Tiny plugin that regenerates the doc markdown when any bzl file changes during
    `yarn start` or during normal operation of `yarn build`.
    
    This is slightly overbuilding, but is necessary to ensure that docs files exist
    on disk before the docs plugin tries to read them (avoids a race condition with
    this bzldoc plugin at the cost of extracting docs once potentially unnecessarily)
    
    Reviewed By: zeroxoneb
    
    Differential Revision: D25072749
    
    fbshipit-source-id: 129984e0c684a765fa2d427175dd7a9225004a28

---
+++ website/gen/index.js
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+// Tiny docusaurus plugin to rerun bzldoc to regenerate the .md from .bzl
+// docstrings on file changes during `yarn start`, or on `yarn build`
+
+const path = require('path');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+// eslint-disable-next-line no-unused-vars
+module.exports = (context, options) => {
+  const bzlDir = path.resolve(context.siteDir, '../bzl');
+  const bzlGlobs = [`${bzlDir}/*.bzl`, `${bzlDir}/**/*.bzl`];
+  return {
+    name: 'bzldoc',
+    async loadContent() {
+      const out = `${context.siteDir}/docs/api/`;
+      await exec(
+        `buck run //antlir/website/gen:bzldoc -- ${bzlGlobs.join(' ')} ${out}`,
+      );
+      return null;
+    },
+    getPathsToWatch() {
+      return bzlGlobs;
+    },
+  };
+};
