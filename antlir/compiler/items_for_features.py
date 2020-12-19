af3075a1ce50166baec425cb3f253a92556c21f6
Justin Trudell <jtru@fb.com>
2020-12-18 17:00:35 -0800

    Don't raise on missing feature targets, yield single config
    Reviewed By: snarkmaster
    
    Differential Revision: D25541235
    
    fbshipit-source-id: 0cbee5f7e9ceaf86075a80c7bfc3f29a51363c9d

--- antlir/compiler/items_for_features.py
+++ antlir/compiler/items_for_features.py
@@ -6,7 +6,7 @@
 
 "Makes Items from the JSON that was produced by the Buck target image_feature"
 import json
-from typing import Iterable, Union, Mapping, Optional
+from typing import Any, Iterable, Union, Mapping, Optional, NamedTuple
 
 from antlir.compiler.items.clone import CloneItem
 from antlir.compiler.items.common import LayerOpts, image_source_item
@@ -25,11 +25,18 @@ from antlir.compiler.items.tarball import TarballItem
 from antlir.find_built_subvol import find_built_subvol
 
 
-def replace_targets_by_paths(
-    x,
-    target_to_path: Mapping[str, str],
-    subvolumes_dir: Optional[str],
-):
+class GenFeaturesContext(NamedTuple):
+    target_to_path: Mapping[str, str]
+    subvolumes_dir: Optional[str]
+    ignore_missing_paths: bool
+
+
+# Used alongside `ignore_missing_paths`
+class UnknownTarget(NamedTuple):
+    target: str
+
+
+def replace_targets_by_paths(x: Any, ctx: GenFeaturesContext):
     """
     Converts target_tagger.bzl sigils to buck-out paths or Subvol objects.
 
@@ -41,32 +48,31 @@ def replace_targets_by_paths(
     `--child-dependencies`, and performs the substitution in any image
     feature JSON it consumes.
 
-    Note: If `subvolumes_dir` is None, layer targets will not be replaced by
+    If `ctx.subvolumes_dir` is None, layer targets will not be replaced by
     their corresponding subvolumes, and will instead be left as-is.
+
+    If `ctx.ignore_missing_paths` is True, the target will simply be
+    returned if it is not found in `target_to_path`.
     """
     if type(x) is dict:
         if "__BUCK_TARGET" in x or "__BUCK_LAYER_TARGET" in x:
             assert len(x) == 1, x
             ((sigil, target),) = x.items()
-            if sigil == "__BUCK_LAYER_TARGET" and subvolumes_dir is None:
+            if sigil == "__BUCK_LAYER_TARGET" and ctx.subvolumes_dir is None:
                 return target  # pragma: no cover
-            path = target_to_path.get(target)
+            path = ctx.target_to_path.get(target)
             if not path:
-                raise RuntimeError(f"{target} not in {target_to_path}")
+                if ctx.ignore_missing_paths:  # pragma: no cover
+                    return UnknownTarget(target)
+                raise RuntimeError(f"{target} not in {ctx.target_to_path}")
             return (
                 path
                 if sigil == "__BUCK_TARGET"
-                else find_built_subvol(path, subvolumes_dir=subvolumes_dir)
+                else find_built_subvol(path, subvolumes_dir=ctx.subvolumes_dir)
             )
-        return {
-            k: replace_targets_by_paths(v, target_to_path, subvolumes_dir)
-            for k, v in x.items()
-        }
+        return {k: replace_targets_by_paths(v, ctx) for k, v in x.items()}
     elif type(x) is list:
-        return [
-            replace_targets_by_paths(v, target_to_path, subvolumes_dir)
-            for v in x
-        ]
+        return [replace_targets_by_paths(v, ctx) for v in x]
     elif type(x) in [int, float, str, bool, type(None)]:
         return x
     raise AssertionError(f"Unknown {type(x)} for {x}")  # pragma: no cover
@@ -74,17 +80,12 @@ def replace_targets_by_paths(
 
 def gen_included_features(
     features_or_paths: Iterable[Union[str, dict]],
-    target_to_path: Mapping[str, str],
-    subvolumes_dir: Optional[str],
+    features_ctx: GenFeaturesContext,
 ):
     for feature_or_path in features_or_paths:
         if isinstance(feature_or_path, str):
             with open(feature_or_path) as f:
-                items = replace_targets_by_paths(
-                    json.load(f),
-                    target_to_path,
-                    subvolumes_dir,
-                )
+                items = replace_targets_by_paths(json.load(f), features_ctx)
         else:
             # Any inline feature would have had its target paths unwrapped by
             # the outer feature that contains it.  That's always true because
@@ -94,13 +95,13 @@ def gen_included_features(
 
         yield from gen_included_features(
             features_or_paths=items.pop("features", []),
-            target_to_path=target_to_path,
-            subvolumes_dir=subvolumes_dir,
+            features_ctx=features_ctx,
         )
 
         target = items.pop("target")
         for feature_key, configs in items.items():
-            yield (feature_key, target, configs)
+            for cfg in configs:
+                yield (feature_key, target, cfg)
 
 
 def gen_items_for_features(
@@ -129,14 +130,15 @@ def gen_items_for_features(
         "foreign_layer": ForeignLayerItem,
     }
 
-    for (feature_key, target, configs) in gen_included_features(
+    for (feature_key, target, config) in gen_included_features(
         features_or_paths=features_or_paths,
-        target_to_path=layer_opts.target_to_path,
-        subvolumes_dir=layer_opts.subvolumes_dir,
+        features_ctx=GenFeaturesContext(
+            target_to_path=layer_opts.target_to_path,
+            subvolumes_dir=layer_opts.subvolumes_dir,
+            ignore_missing_paths=False,
+        ),
     ):
         assert (
             feature_key in key_to_item_factory
         ), f"Unsupported item: {feature_key}"
-        item_factory = key_to_item_factory[feature_key]
-        for config in configs:
-            yield item_factory(from_target=target, **config)
+        yield key_to_item_factory[feature_key](from_target=target, **config)
