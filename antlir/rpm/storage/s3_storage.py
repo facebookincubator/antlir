dde498e263a790d8a4d8f30950fe085ae44d832f
Vinnie Magro <vmagro@fb.com>
2020-12-28 10:07:00 -0800

    antlir oss: rework snapshots and s3 storage
    Summary:
    I noticed this when attempting to move the snapshots db to rds, the
    storage id stored in the db is only part of the key, and it is then
    possible for the rest of the key information to be lost if the storage
    config changes (as I did to add a fedora33 snapshot with a different
    prefix).
    
    This diff also rolls together a fedora 33 snapshot (deleting the old
    snapshot - bits are still on s3 so old revs will still work) with this
    new storage key mechanism and using universes slightly more sanely, as
    well as a new stable build appliance revision based on fedora 33.
    
    allow-large-files
    
    Reviewed By: snarkmaster
    
    Differential Revision: D25336713
    
    fbshipit-source-id: 4ef77b4d442e364cd848d3a435e5a3f04ac60998

--- antlir/rpm/storage/s3_storage.py
+++ antlir/rpm/storage/s3_storage.py
@@ -6,7 +6,9 @@
 
 import io
 import logging
+import os.path
 import uuid
+import warnings
 from contextlib import contextmanager
 from typing import ContextManager
 
@@ -36,22 +38,16 @@ class S3Storage(Storage, plugin_kind="s3"):
         self.region = region
         self.timeout_seconds = timeout_seconds
 
-    @classmethod
-    def _make_storage_id(cls) -> str:
-        return str(uuid.uuid4()).replace("-", "")
-
     def _object_key(self, sid: str) -> str:
         key = sid
         if ":" in key:
             key = self.strip_key(sid)
-        return f"{self.prefix}/{key}"
+        return key
 
     @contextmanager
     def reader(self, sid: str) -> ContextManager[StorageInput]:
-        url = (
-            f"https://{self.bucket}.s3-{self.region}.amazonaws.com/"
-            + self._object_key(sid)
-        )
+        key = self._object_key(sid)
+        url = f"https://{self.bucket}.s3-{self.region}.amazonaws.com/{key}"
         with open_url(url) as f:
             yield StorageInput(input=f)
 
@@ -63,16 +59,19 @@ class S3Storage(Storage, plugin_kind="s3"):
         import boto3
 
         boto3.set_stream_logger("", logging.WARNING)
+        # botocore is not threadsafe, each thread needs its own session.
+        session = boto3.session.Session()
         # Reads require no credentials, writes go through AWS authentication
         # and will fail if the required environment variables are not set
         # Full details here, but in practice it's ok to ignore the intricacies.
         # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/credentials.html
-        return boto3.client("s3")
+        s3 = session.resource("s3")
+        return s3.Bucket(self.bucket)
 
     @contextmanager
     def writer(self) -> ContextManager[StorageOutput]:
-        sid = self._make_storage_id()
-        key = self._object_key(sid)
+        sid = str(uuid.uuid4()).replace("-", "")
+        key = os.path.join(self.prefix, sid)
         log_prefix = f"{self.__class__.__name__}"
         log.debug(f"{log_prefix} - Writing to {key}")
 
@@ -81,14 +80,18 @@ class S3Storage(Storage, plugin_kind="s3"):
         @contextmanager
         def get_id_and_release_resources():
             buf.seek(0)
-            self.s3.upload_fileobj(buf, self.bucket, key)
+            # boto3 spews unclosed resource warnings everywhere, without a
+            # possible fix client side :(
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore", ResourceWarning)
+                self.s3.upload_fileobj(buf, key)
             # S3 does not do partial puts, if the request returned 200, then
             # there is read-after-write guarantees
-            yield sid
+            yield key
 
         with _CommitCallback(self, get_id_and_release_resources) as commit:
             yield StorageOutput(output=buf, commit_callback=commit)
 
     def remove(self, sid: str) -> None:
         key = self._object_key(sid)
-        self.s3.delete_object(self.bucket, key)
+        self.s3.delete_objects(Delete={"Objects": [{"Key": key}]})
