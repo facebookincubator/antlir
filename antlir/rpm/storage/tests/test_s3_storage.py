38480cfd96463fec62e1d398c6130ffb8616ef6c
Vinnie Magro <vmagro@fb.com>
2020-12-28 10:07:00 -0800

    antlir oss: rework snapshots and s3 storage
    Summary:
    I noticed this when attempting to move the snapshots db to rds, the
    storage id stored in the db is only part of the key, and it is then
    possible for the rest of the key information to be lost if the storage
    config changes (as I did to add a fedora33 snapshot with a different
    prefix).
    
    This diff also rolls together a fedora 33 snapshot (deleting the old
    snapshot - bits are still on s3 so old revs will still work) with this
    new storage key mechanism and using universes slightly more sanely, as
    well as a new stable build appliance revision based on fedora 33.
    
    allow-large-files
    
    Reviewed By: snarkmaster
    
    Differential Revision: D25336713
    
    fbshipit-source-id: 4ef77b4d442e364cd848d3a435e5a3f04ac60998

--- antlir/rpm/storage/tests/test_s3_storage.py
+++ antlir/rpm/storage/tests/test_s3_storage.py
@@ -19,13 +19,12 @@ class S3StorageTestCase(StorageBaseTestCase):
 
     # some methods to mock an in-memory s3 bucket
     # this matches the read-after-write consistency guarantee from the real s3
-    def _mock_upload_fileobj(self, contents, bucket: str, key: str):
-        assert bucket == self.bucket
+    def _mock_upload_fileobj(self, contents, key: str):
         self.objects[key] = contents.getbuffer()
 
-    def _mock_delete_object(self, bucket: str, key: str):
-        assert bucket == self.bucket
-        del self.objects[key]
+    def _mock_delete_objects(self, Delete):
+        for key in [o["Key"] for o in Delete["Objects"]]:
+            del self.objects[key]
 
     @contextmanager
     def _mock_open_url(self, url: str):
@@ -38,7 +37,7 @@ class S3StorageTestCase(StorageBaseTestCase):
         self.objects = {}
         self.s3 = MagicMock()
         self.s3.upload_fileobj.side_effect = self._mock_upload_fileobj
-        self.s3.delete_object.side_effect = self._mock_delete_object
+        self.s3.delete_objects.side_effect = self._mock_delete_objects
         self.storage = Storage.make(
             key="test",
             kind="s3",
@@ -47,14 +46,21 @@ class S3StorageTestCase(StorageBaseTestCase):
             region=self.region,
             timeout_seconds=3,
         )
-        self.boto3_client_patch = patch("boto3.client")
-        boto3_client = self.boto3_client_patch.start()
-        boto3_client.return_value = self.s3
+        self.boto3_session_patch = patch("boto3.session.Session")
+        mock_session = MagicMock()
+        session = self.boto3_session_patch.start()
+        session.return_value = mock_session
+        mock_resource = MagicMock()
+        mock_session.resource.return_value = mock_resource
+        mock_resource.Bucket.return_value = self.s3
+        # basic checks to make sure the mocks are setup correctly
         self.assertEqual(self.storage.s3, self.s3)
+        mock_session.resource.assert_called_with("s3")
+        mock_resource.Bucket.assert_called_with(self.bucket)
         s3_storage.open_url = self._mock_open_url
 
     def tearDown(self):
-        self.boto3_client_patch.stop()
+        self.boto3_session_patch.stop()
 
     def test_write_and_read_back(self):
         # Do a bunch of mock writes and ensure that the s3 client was called
@@ -63,13 +69,21 @@ class S3StorageTestCase(StorageBaseTestCase):
         # as it is called with the correct contents, that is enough to pass.
         for contents, sid in self.check_storage_impl(self.storage):
             self.s3.upload_fileobj.assert_any_call(
-                ANY, "antlir-test", self.storage._object_key(sid)
+                ANY, self.storage._object_key(sid)
             )
             # ensure that the written data was sent to s3 correctly, as one
             # blob regardless of how many chunks it was input as
             with self.storage.reader(sid) as f:
                 actual = f.read()
                 self.assertEqual(actual, b"".join(contents))
+
+    def test_writer_prefix(self):
+        # Reads are expected to already have the prefix, but writes should
+        # create new blobs under the set prefix
+        with self.storage.writer() as out:
+            out.write(b"Hello world!")
+            sid = out.commit()
+        self.assertTrue(sid.startswith(f"test:{self.prefix}/"), sid)
 
     def test_delete(self):
         with self.storage.writer() as out:
@@ -79,11 +93,11 @@ class S3StorageTestCase(StorageBaseTestCase):
         self.assertIn(key, self.objects)
         self.storage.remove(sid)
         self.assertNotIn(key, self.objects)
-        self.s3.delete_object.called_with(self.bucket, key)
+        self.s3.delete_objects.called_with(self.bucket, key)
 
     def test_reader_url(self):
         with patch("antlir.rpm.storage.s3_storage.open_url") as open_url:
-            with self.storage.reader("1234") as _:
+            with self.storage.reader("test/prefix/1234") as _:
                 pass
             open_url.assert_called_with(
                 "https://antlir-test.s3-test-region.amazonaws.com/"
@@ -91,7 +105,8 @@ class S3StorageTestCase(StorageBaseTestCase):
             )
 
     def test_object_key(self):
-        blob_sid = "123"
+        blob_sid = "test/prefix/123"
         self.assertEqual(
-            self.storage._object_key(blob_sid), f"test/prefix/{blob_sid}"
+            self.storage._object_key(blob_sid),
+            "test/prefix/123",
         )
