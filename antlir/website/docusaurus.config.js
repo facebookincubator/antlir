1b82f3e8910425faec3c9cdd43c6154dc24ac5fe
Lindsay Salisbury <lsalis@fb.com>
2021-01-13 16:29:20 -0800

    Resolve node-forge version issue + update all other deps
    Summary: Upgraded with `yarn upgrade`.  Just did the whole thing cause I don't really know how all of this works.  I also had to fix a deprecated element in the config.
    
    Reviewed By: snarkmaster
    
    Differential Revision: D25870689
    
    fbshipit-source-id: 7450e5cba8ff9b1550cbdd3c6ecba959baea26e8

--- antlir/website/docusaurus.config.js
+++ antlir/website/docusaurus.config.js
@@ -34,7 +34,7 @@ module.exports = {
         alt: 'My Facebook Project Logo',
         src: 'img/logo.svg',
       },
-      links: [
+      items: [
         {
           to: 'docs/',
           activeBasePath: 'docs',
