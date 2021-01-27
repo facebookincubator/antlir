aeb55ca430bf3bb6b9dba8e602abf1d834ff53e5
Lindsay Salisbury <lsalis@fb.com>
2021-01-27 14:08:16 -0800

    Fix load_repo_config and repo_root for mode/opt when no repo root exists
    Summary: There is one case where we might not have a repository to find when we do `load_repo_config()`. This case is legit, so this fixes it.  It also adds more tests to verify the different cases.
    
    Reviewed By: justintrudell
    
    Differential Revision: D26053153
    
    fbshipit-source-id: afb7cc00b1a265f48cdbf8070305395bfc278ab4

--- antlir/config.py
+++ antlir/config.py
@@ -5,19 +5,31 @@
 
 import importlib
 import json
+from typing import Optional
 
 from antlir.artifacts_dir import find_repo_root
+from antlir.fs_utils import Path
 from antlir.repo_config_t import repo_config_t as base_repo_config_t
+
+_read_text = importlib.resources.read_text
 
 
 def load_repo_config(path_in_repo=None):
-    with importlib.resources.open_text(__package__, "config.json") as r:
-        data = json.load(r)
+    data = json.loads(_read_text(__package__, "config.json"))
 
-    return repo_config_t(
-        repo_root=find_repo_root(path_in_repo=path_in_repo), **data
-    )
+    # If we don't need the artifacts, then it's reasonable that we might
+    # not find a repo root.  We can safely ignore the error and not have
+    # a repo_root in that case.  But if we *do* need the artifacts, we
+    # should fail loudly here because things will likely be broke.
+    repo_root = None
+    try:
+        repo_root = find_repo_root(path_in_repo=path_in_repo)
+    except RuntimeError as re:
+        if data.get("artifacts_require_repo"):
+            raise re
+
+    return repo_config_t(repo_root=repo_root, **data)
 
 
 class repo_config_t(base_repo_config_t):
-    repo_root: str
+    repo_root: Optional[Path] = None
--- antlir/tests/test_config.py
+++ antlir/tests/test_config.py
@@ -4,12 +4,33 @@
 # LICENSE file in the root directory of this source tree.
 
 import unittest
+import unittest.mock
 
 from antlir.artifacts_dir import find_repo_root
 from antlir.config import load_repo_config, repo_config_t
+from antlir.fs_utils import Path
 
 
 class RepoConfigTestCase(unittest.TestCase):
+    def _test_repo_config_json(self, **kwargs) -> str:
+        # These are just hand constructed values that have no
+        # real meaning outside of the context of this test case.
+        defaults = {
+            "artifacts_require_repo": True,
+            "build_appliance_default": "//build/appliance:default",
+            "rpm_installer_default": "yum",
+            "rpm_installers_supported": [
+                "yum",
+                "dnf",
+            ],
+            "version_set_to_path": {
+                "all_jobs": "//all/jobs/version/set:path",
+            },
+            "version_set_default": "all_jobs",
+        }
+        defaults.update(kwargs)
+        return repo_config_t(**defaults).json(exclude={"repo_root": ...})
+
     def test_repo_config(self):
         config = load_repo_config()
 
@@ -25,4 +46,39 @@ class RepoConfigTestCase(unittest.TestCase):
         self.assertIsNotNone(config.build_appliance_default)
         self.assertNotEqual(config.build_appliance_default, "")
 
-        self.assertEqual(config.repo_root, str(find_repo_root()))
+        self.assertEqual(config.repo_root, find_repo_root())
+
+    @unittest.mock.patch("antlir.config._read_text")
+    def test_repo_config_artifacts_require_repo_false(self, _read_text):
+        # Test case for loading the config and finding the repo root
+        # where the artifacts don't require the repo.  This is a possible
+        # case if the binaries are built standalone (mode/opt internally).
+        # It is reasonable to assume that we may not have a code repository
+        # on disk in this case.
+
+        # Generate data to be loaded by the load_repo_config method
+        _read_text.return_value = self._test_repo_config_json(
+            artifacts_require_repo=False,
+        )
+
+        # To force the lack of a repo, we need to set the `path_in_repo` to /
+        # so we are ensured to never find a repo.
+        config = load_repo_config(path_in_repo=Path("/"))
+        self.assertIsInstance(config, repo_config_t)
+
+        # We shouldn't have a repository root
+        self.assertIsNone(config.repo_root)
+
+    @unittest.mock.patch("antlir.config._read_text")
+    def test_repo_config_fail_artifacts_require_repo_true(self, _read_text):
+        # Test the case where the artifacts require the repo, but we can't
+        # find it.
+
+        # Force the value of `artifacts_require_repo` to True so we can force
+        # the error we are testing for
+        _read_text.return_value = self._test_repo_config_json(
+            artifacts_require_repo=True,
+        )
+
+        with self.assertRaises(RuntimeError):
+            load_repo_config(path_in_repo=Path("/"))
