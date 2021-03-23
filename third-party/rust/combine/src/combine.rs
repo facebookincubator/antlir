4e97e1a8b25fec9098b1e8cb2a2b6cf9d5c51aa7
Vinnie Magro <v@vinn.ie>
2021-03-23 10:49:27 -0700

    parse [path=] attr

--- third-party/rust/combine/src/combine.rs
+++ third-party/rust/combine/src/combine.rs
@@ -7,8 +7,8 @@ use anyhow::{Context, Result};
 use cargo_toml::Manifest;
 use quote::ToTokens;
 use syn::token::Brace;
-use syn::visit_mut::{self, VisitMut};
-use syn::ItemMod;
+use syn::visit_mut::VisitMut;
+use syn::{ItemMod, Lit, Meta};
 
 struct ModVisitor {
     root_dir: PathBuf,
@@ -19,7 +19,26 @@ impl VisitMut for ModVisitor {
         if node.content.is_none() {
             // TODO: this only handles one level of nesting, but I think that is
             // good enough
-            let path = self.root_dir.join(format!("{}.rs", node.ident));
+            let mut path = self.root_dir.join(format!("{}.rs", node.ident));
+            if let Some(path_attr) = node
+                .attrs
+                .iter()
+                .find(|a| a.path.get_ident().map(|i| i.to_string()) == Some("path".to_string()))
+            {
+                match path_attr.parse_meta().expect(&format!(
+                    "path attribute '{}' not understood",
+                    path_attr.tokens
+                )) {
+                    Meta::NameValue(nv) => match nv.lit {
+                        Lit::Str(s) => {
+                            path = self.root_dir.join(s.value());
+                        }
+                        _ => panic!("path attr value must be string"),
+                    },
+                    _ => panic!("path attr must be name-value"),
+                }
+            }
+
             let mut include =
                 File::open(&path).expect(&format!("could not open module file '{:?}'", path));
             let mut content = String::new();
@@ -31,7 +50,7 @@ impl VisitMut for ModVisitor {
         }
 
         // Delegate to the default impl to visit any nested modules.
-        visit_mut::visit_item_mod_mut(self, node);
+        // visit_mut::visit_item_mod_mut(self, node);
     }
 }
 
@@ -39,8 +58,8 @@ fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
 
     let crate_dir = Path::new(&args[1]);
-    let manifest =
-        Manifest::from_path(crate_dir.join("Cargo.toml")).context("failed to load manifest")?;
+    let manifest = Manifest::from_path(crate_dir.join("Cargo.toml"))
+        .with_context(|| format!("failed to load manifest from {:?}", crate_dir))?;
     let out_file = Path::new(&args[2]);
 
     let lib = manifest
@@ -64,7 +83,7 @@ fn main() -> Result<()> {
     let ts = syntax.to_token_stream();
 
     let mut out = File::create(out_file)?;
-    write!(out, "{}\n", ts.to_string())?;
+    write!(out, "{}\n", ts)?;
 
     Ok(())
 }
