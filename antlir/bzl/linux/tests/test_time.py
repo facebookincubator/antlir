a6a9d2989ddf503becccc7e13281cce10d225fc5
Lindsay Salisbury <lsalis@fb.com>
2021-03-29 11:29:09 -0700

    Add a feature for configuring the timezone.
    Summary: This provides features that will configure the timezone.  It implies that the layer being operated on has the `tzdata` installed.
    
    Reviewed By: snarkmaster
    
    Differential Revision: D27136058
    
    fbshipit-source-id: 573f2cf2226727ec70300e687b45d14919eec400

---
+++ antlir/bzl/linux/tests/test_time.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import os
+import subprocess
+import unittest
+
+
+class TimeTest(unittest.TestCase):
+    def test_timezome(self) -> None:
+        """ Verify that the timezone is set properly. """
+        out = subprocess.run(
+            ["/usr/bin/date", "+%Z"], check=True, stdout=subprocess.PIPE
+        )
+        self.assertIn(
+            out.stdout.decode().strip(),
+            os.environ["ANTLIR_TEST_EXPECTED_TIMEZONES"].split(),
+        )
