516f451ffd41fe0f0453a41dfe47cdf123a85d27
Vinnie Magro <vmagro@fb.com>
2021-04-30 17:26:05 -0700

    antlir linux: systemd generator scaffolding
    Summary:
    Introduce a parser for kernel cmdline that is then used by a systemd generator.
    This generator currently parses the cmdline for `antlir.control_os_uri` and
    instantiates a template unit `antlir-fetch-control-os@.service` that will be
    added in the next diff.
    When `antlirctl` grows support for RuntimeConfigCompiler or disk partitioning,
    this generator will likely also expand.
    
    Reviewed By: epilatow
    
    Differential Revision: D26834846
    
    fbshipit-source-id: 65cf51798dc52d8ef9f444ecba53676b5ba09fda

---
+++ antlir/linux/antlirctl/src/generator.rs
@@ -0,0 +1,141 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+use std::fs;
+use std::io::BufWriter;
+use std::os::unix::fs::symlink;
+use std::path::PathBuf;
+
+use anyhow::{Context, Result};
+use slog::{error, info, o, Drain, Logger};
+use slog_glog_fmt::kv_categorizer::ErrorCategorizer;
+use structopt::StructOpt;
+
+use crate::kernel_cmdline::AntlirCmdline;
+use crate::systemd::{self, PROVIDER_ROOT};
+
+#[derive(StructOpt)]
+pub struct Opts {
+    normal_dir: PathBuf,
+    #[allow(unused)]
+    early_dir: PathBuf,
+    #[allow(unused)]
+    late_dir: PathBuf,
+}
+
+// This helper intentionally not implemented in systemd.rs. The generator has to
+// operate on files since systemd might not be ready while the generator is
+// running. Post-generator interactions with systemd should happen over dbus,
+// not filesystem mangling.
+fn instantiate_and_enable_template<U: AsRef<str>, I: AsRef<str>, W: AsRef<str>>(
+    normal_dir: PathBuf,
+    unit: U,
+    instance: I,
+    wanted_by: W,
+) -> Result<()> {
+    let instance_unit = systemd::template_unit_name(&unit, instance)?;
+    let instance_file = normal_dir.join(&instance_unit);
+    let template_src = PathBuf::from(PROVIDER_ROOT).join(unit.as_ref());
+    symlink(&template_src, &instance_file).with_context(|| {
+        format!(
+            "failed to symlink {:?} -> {:?}",
+            instance_file, template_src
+        )
+    })?;
+    let wants_dir = normal_dir.join(format!("{}.wants", wanted_by.as_ref()));
+    fs::create_dir_all(&wants_dir).with_context(|| format!("failed to create {:?}", wants_dir))?;
+    symlink(&instance_file, wants_dir.join(&instance_unit)).with_context(|| {
+        format!(
+            "failed to symlink {:?} -> {:?}",
+            wants_dir.join(&instance_unit),
+            instance_file,
+        )
+    })?;
+    Ok(())
+}
+
+fn generator_maybe_err(log: Logger, opts: Opts) -> Result<()> {
+    let cmdline =
+        AntlirCmdline::from_kernel().context("invalid kernel cmdline options for Antlir")?;
+
+    if let Some(control_os_uri) = cmdline.control_os_uri() {
+        info!(
+            log,
+            "Instantiating antlir-fetch-image@.service";
+        );
+        instantiate_and_enable_template(
+            opts.normal_dir,
+            "antlir-fetch-image@.service",
+            control_os_uri,
+            "initrd.target",
+        )?;
+    }
+    Ok(())
+}
+
+pub fn generator(log: Logger, opts: Opts) -> Result<()> {
+    // antlir-generator has an additional logging drain setup that is not as
+    // pretty looking as other slog drain formats, but is usable with /dev/kmsg.
+    // Otherwise, the regular drain that logs to stderr silently disappears when
+    // systemd runs the generator.
+    let kmsg = fs::OpenOptions::new()
+        .write(true)
+        .open("/dev/kmsg")
+        .context("failed to open /dev/kmsg for logging")?;
+    let kmsg = BufWriter::new(kmsg);
+
+    let decorator = slog_term::PlainDecorator::new(kmsg);
+    let drain = slog_glog_fmt::GlogFormat::new(decorator, ErrorCategorizer).fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+
+    let log = slog::Logger::root(slog::Duplicate::new(log, drain).fuse(), o!());
+
+    info!(log, "antlir-generator starting");
+
+    let sublog = log.new(o!());
+
+    match generator_maybe_err(sublog, opts) {
+        Ok(()) => Ok(()),
+        Err(e) => {
+            error!(log, "{}", e.to_string());
+            Err(e)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::systemd::PROVIDER_ROOT;
+    use std::path::Path;
+    use std::time::SystemTime;
+
+    #[test]
+    fn instantiate_example_template() -> Result<()> {
+        let ts = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?;
+        let tmpdir = std::env::temp_dir().join(format!("instantiate_template_{:?}", ts));
+        std::fs::create_dir(&tmpdir)?;
+        instantiate_and_enable_template(
+            tmpdir.clone(),
+            "hello@.service",
+            "world",
+            "multi-user.target",
+        )?;
+        assert_eq!(
+            tmpdir.join("hello@world.service").read_link()?,
+            Path::new(PROVIDER_ROOT).join("hello@.service"),
+        );
+        assert_eq!(
+            tmpdir
+                .join("multi-user.target.wants/hello@world.service")
+                .read_link()?,
+            tmpdir.join("hello@world.service"),
+        );
+        std::fs::remove_dir_all(&tmpdir)?;
+        Ok(())
+    }
+}
---
+++ antlir/linux/antlirctl/src/kernel_cmdline.rs
@@ -0,0 +1,159 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+use std::fs;
+
+use anyhow::{Context, Error, Result};
+use structopt::clap::AppSettings;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt, PartialEq, Default)]
+#[structopt(name = "kernel-cmdline", setting(AppSettings::NoBinaryName))]
+pub struct AntlirCmdline {
+    #[structopt(parse(from_str = parse_opt))]
+    opts: Vec<KernelCmdlineOpt>,
+}
+
+impl AntlirCmdline {
+    pub fn from_kernel() -> Result<AntlirCmdline> {
+        fs::read_to_string("/proc/cmdline")
+            .context("failed to read /proc/cmdline")?
+            .parse()
+    }
+
+    fn arg(&self, key: &str) -> Option<&KernelCmdlineOpt> {
+        self.opts.iter().find(|o| match o {
+            KernelCmdlineOpt::OnOff(k, _) => k == key,
+            KernelCmdlineOpt::Kv(k, _) => k == key,
+        })
+    }
+
+    pub fn control_os_uri(&self) -> Option<&str> {
+        self.arg("antlir.control_os_uri").and_then(|opt| match opt {
+            KernelCmdlineOpt::Kv(_, uri) => Some(uri.as_str()),
+            _ => None,
+        })
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum KernelCmdlineOpt {
+    OnOff(String, bool),
+    Kv(String, String),
+}
+
+fn parse_opt(src: &str) -> KernelCmdlineOpt {
+    match src.split_once("=") {
+        Some((key, val)) => {
+            // normalize dashes in keys to underscores
+            let key = key.replace("-", "_");
+            match val {
+                "0" | "false" | "no" => KernelCmdlineOpt::OnOff(key, false),
+                "1" | "true" | "yes" => KernelCmdlineOpt::OnOff(key, true),
+                _ => KernelCmdlineOpt::Kv(key, val.to_owned()),
+            }
+        }
+        None => KernelCmdlineOpt::OnOff(src.replace("-", "_"), true),
+    }
+}
+
+#[derive(PartialEq)]
+enum ParserState {
+    Push,
+    Quoted,
+}
+
+impl std::str::FromStr for AntlirCmdline {
+    type Err = Error;
+
+    /// Parse /proc/cmdline to get values from the booted kernel cmdline. Some
+    /// selected options are available when they have significance for Antlir
+    /// code, for example 'antlir.control_os_uri'.
+    fn from_str(s: &str) -> Result<Self> {
+        // strip leading and trailing whitespace
+        let s = s.trim();
+        let mut state = ParserState::Push;
+        let mut args = vec![];
+        let mut current = String::new();
+        for ch in s.chars() {
+            if ch.is_whitespace() {
+                match state {
+                    ParserState::Push => {
+                        args.push(current);
+                        current = String::new();
+                    }
+                    ParserState::Quoted => current.push(ch),
+                };
+            } else if ch == '"' {
+                state = match state {
+                    ParserState::Push => ParserState::Quoted,
+                    ParserState::Quoted => ParserState::Push,
+                }
+            } else {
+                current.push(ch);
+            }
+        }
+        // last arg does not have the trailing whitespace to trigger the push in
+        // the above loop
+        args.push(current);
+        eprintln!("{:?}", args);
+        AntlirCmdline::from_iter_safe(args).context("failed to parse")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::AntlirCmdline;
+    use anyhow::Result;
+
+    #[test]
+    fn basic_cmdlines() -> Result<()> {
+        for cmdline in &[
+            "rd.systemd.debug_shell=1 quiet antlir.control_os_uri=https://some/url",
+            "rd.systemd.debug_shell=1 quiet antlir.control-os-uri=https://some/url",
+            "rd.systemd.debug_shell=1 quiet antlir.control_os_uri=\"https://some/url\"",
+        ] {
+            let cmdline: AntlirCmdline = cmdline.parse()?;
+            eprintln!("{:?}", cmdline);
+            assert_eq!(Some("https://some/url"), cmdline.control_os_uri(),);
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn real_life_cmdline() -> Result<()> {
+        let cmdline = "BOOT_IMAGE=(hd0,msdos1)/vmlinuz-5.2.9-229_fbk15_hardened_4185_g357f49b36602 ro root=LABEL=/ biosdevname=0 net.ifnames=0 fsck.repair=yes systemd.gpt_auto=0 ipv6.autoconf=0 erst_disable cgroup_no_v1=all nox2apic crashkernel=128M hugetlb_cma=6G console=tty0 console=ttyS0,115200".parse()?;
+        let kv = |key: &str, val: &str| super::KernelCmdlineOpt::Kv(key.to_owned(), val.to_owned());
+        let on_off = |key: &str, val: bool| super::KernelCmdlineOpt::OnOff(key.to_owned(), val);
+        assert_eq!(
+            AntlirCmdline {
+                opts: vec![
+                    kv(
+                        "BOOT_IMAGE",
+                        "(hd0,msdos1)/vmlinuz-5.2.9-229_fbk15_hardened_4185_g357f49b36602"
+                    ),
+                    on_off("ro", true),
+                    kv("root", "LABEL=/"),
+                    on_off("biosdevname", false),
+                    on_off("net.ifnames", false),
+                    on_off("fsck.repair", true),
+                    on_off("systemd.gpt_auto", false),
+                    on_off("ipv6.autoconf", false),
+                    on_off("erst_disable", true),
+                    kv("cgroup_no_v1", "all"),
+                    on_off("nox2apic", true),
+                    kv("crashkernel", "128M"),
+                    kv("hugetlb_cma", "6G"),
+                    kv("console", "tty0"),
+                    kv("console", "ttyS0,115200")
+                ]
+            },
+            cmdline
+        );
+        Ok(())
+    }
+}
