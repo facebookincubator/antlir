f339e86eafd7214aab819ca35baac779942b43a3
Vinnie Magro <vmagro@fb.com>
2021-04-30 17:26:05 -0700

    antlirctl: add rudimentary support for labels in mount
    Summary:
    Support the syntax `mount LABEL=$somelabel /target -t fstype`
    For booting pre-provisioned systems, we can just piggyback on the existing disk
    with `LABEL=/` to avoid having to care about partitioning schemes and device
    types.
    
    This still makes more sense to implement in antlirctl than re-introducing
    busybox, because we need access to the same information to generate the
    `sysroot.mount` unit in the next diff (systemd is unable to determine device
    dependencies when the source is passed through unmodified as something like
    `LABEL=/`)
    
    Reviewed By: epilatow
    
    Differential Revision: D27873756
    
    fbshipit-source-id: a0d8419ffa84f8bf8ba8c55314691e1fc6b8f6bd

--- antlir/linux/antlirctl/src/mount.rs
+++ antlir/linux/antlirctl/src/mount.rs
@@ -24,7 +24,7 @@ use structopt::StructOpt;
 
 #[derive(StructOpt)]
 pub struct Opts {
-    source: PathBuf,
+    source: String,
     target: PathBuf,
     #[structopt(short = "t")]
     fstype: String,
@@ -49,8 +49,9 @@ fn parse_options(options: Vec<String>) -> (Vec<String>, MsFlags) {
 
 pub fn mount(opts: Opts) -> Result<()> {
     let (data, flags) = parse_options(opts.options);
+    let source = source_to_device_path(opts.source)?;
     nix::mount::mount(
-        Some(&opts.source),
+        Some(&source),
         opts.target.as_path(),
         Some(opts.fstype.as_str()),
         flags,
@@ -59,10 +60,46 @@ pub fn mount(opts: Opts) -> Result<()> {
     .context("mount failed")
 }
 
+/// Parse the source argument into a path where the source device should be
+/// mounted. This handles things like LABELs as well as full paths.
+pub fn source_to_device_path<S: AsRef<str>>(src: S) -> Result<PathBuf> {
+    if let Some(label) = src.as_ref().strip_prefix("LABEL=") {
+        // This is a very rudimentary level of support for disk labels.
+        // It should probably be using libblkid proper, but this works for the
+        // current system setups at least.
+        eprintln!("label = {:?}", label);
+        return Ok(format!("/dev/disk/by-label/{}", blkid_encode_string(label)).into());
+    }
+    // Assume that src is some opaque value that the kernel will know what to do
+    // with.
+    Ok(src.as_ref().into())
+}
+
+// blkid encodes certain tags in a safe encoding that hex-escapes "unsafe"
+// characters. This is not a complete implementation and panics when given
+// non-ASCII input. See note above: this could be replaced with libblkid
+// directly if the need arises.
+fn blkid_encode_string<S: AsRef<str>>(s: S) -> String {
+    if s.as_ref().chars().any(|ch| !ch.is_ascii()) {
+        unimplemented!("this version of blkid_encode_string only supports ASCII");
+    }
+    let mut encoded = String::with_capacity(s.as_ref().len());
+    for ch in s.as_ref().chars() {
+        if ch.is_ascii_alphanumeric() {
+            encoded.push(ch);
+            continue;
+        }
+        encoded.push_str(format!("\\x{:x}", ch as u8).as_str());
+    }
+    encoded
+}
+
 #[cfg(test)]
 mod tests {
-    use super::parse_options;
+    use super::{blkid_encode_string, parse_options, source_to_device_path};
+    use anyhow::Result;
     use nix::mount::MsFlags;
+    use std::path::Path;
 
     #[test]
     fn parse_options_data_only() {
@@ -93,5 +130,23 @@ mod tests {
                 MsFlags::MS_RDONLY | MsFlags::MS_REMOUNT
             )
         );
+    }
+
+    #[test]
+    fn blkid_encode_string_samples() {
+        assert_eq!("\\x2f", blkid_encode_string("/"));
+        assert_eq!("\\x2fboot", blkid_encode_string("/boot"));
+        assert_eq!("test\\x20string", blkid_encode_string("test string"));
+    }
+
+    #[test]
+    fn sources() -> Result<()> {
+        assert_eq!(Path::new("/dev/sda"), source_to_device_path("/dev/sda")?);
+        assert_eq!(Path::new("sda"), source_to_device_path("sda")?);
+        assert_eq!(
+            Path::new("/dev/disk/by-label/\\x2f"),
+            source_to_device_path("LABEL=/")?
+        );
+        Ok(())
     }
 }
