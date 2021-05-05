69ddf8683aad4afeb1d866b095b32d8cea490cd6
Naveed Golafshani <naveedgol@fb.com>
2021-05-04 18:22:12 -0700

    Add failure prompt when using deprecated helpers like -container, -boot
    Summary:
    - To help ease the migration to the new `=helper` format, introduce a new binary that is run when users mistakenly use old helpers that prompts users that their helper has been deprecated in favour of the current naming scheme
    - New binary simply logs a message as provided via an argument
    
    Reviewed By: zeroxoneb
    
    Differential Revision: D28080150
    
    fbshipit-source-id: e95141e72550b9a70f60649e033022736fa6b029

---
+++ antlir/bzl/tests/test_fail_with_message.py
@@ -0,0 +1,31 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import subprocess
+import unittest
+
+from antlir.fs_utils import Path
+
+from ..fail_with_message import log_failure_message, log
+
+
+class FailWithMessageTestCase(unittest.TestCase):
+    def test_fail_with_message_logs_error(self):
+        msg_to_log = "TEST FAILURE MSG"
+        with self.assertLogs(log, level="ERROR") as log_ctx:
+            log_failure_message(msg_to_log)
+            self.assertIn(msg_to_log, str(log_ctx.output))
+
+    def test_fail_with_message_e2e(self):
+        msg_to_log = "TEST FAILURE MSG"
+        with Path.resource(
+            __package__, "fail-with-message", exe=True
+        ) as binary:
+            res = subprocess.run(
+                [binary, "--message", msg_to_log],
+                check=True,
+                capture_output=True,
+            )
+            self.assertIn(msg_to_log, res.stderr.decode())
