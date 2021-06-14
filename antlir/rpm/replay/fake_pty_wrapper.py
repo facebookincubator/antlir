b487f49b6f9f25f85fd53c2117f41bc6c2947a80
Alexey Spiridonov <lesha@fb.com>
2021-06-14 11:31:00 -0700

    Back out "Don't use `/usr/libexec/platform-python`, it is not available on all distros"
    Summary:
    ... but do so without breaking tests on Github, hopefully :D
    
    The first `test-fake-pty` did not work work on Ubuntu (which runs our OSS tests on Github) or Fedora since both lack `platform-python`. So, the revertee D29040759 (https://github.com/facebookincubator/antlir/commit/9a056dec9a7a550ff2062bcea093679d388b7348) switched to `python3` to fix the tests.
    
    But, all CentOS flavors use `platform-python` and do NOT provide a system Python by default.
    
    To paper over these distro differences, I add `fake_pty_wrapper.py` that picks "whatever Python it can get".
    
    Another option would be to rewrite this in C, in the style of `clonecaps.c` or `rename_shadowed.c`. I didn't do this primarily because this would negatively impact build speed. But that's the right backstop if we need this to work on Python-free OSes.
    
    Reviewed By: zeroxoneb
    
    Differential Revision: D29088667
    
    fbshipit-source-id: 78cc060342f1f7ba8681ee2770aa26ea10ff95d0

---
+++ antlir/rpm/replay/fake_pty_wrapper.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import os
+from typing import ContextManager, List
+
+from antlir.fs_utils import MehStr, Path
+
+
+def fake_pty_resource() -> ContextManager[Path]:
+    return Path.resource(__package__, "fake_pty_real.py", exe=False)
+
+
+def fake_pty_cmd(os_root: MehStr, fake_pty_path: MehStr) -> List[MehStr]:
+    # Try to find a usable Python in the container that will run fake-pty
+    os_root = Path(os_root)
+    for python in [
+        "usr/bin/python3",
+        "usr/libexec/platform-python",
+        "usr/bin/python2",
+    ]:
+        if os.access(os_root / python, os.X_OK):
+            break
+    else:  # pragma: no cover
+        raise RuntimeError(f"Could not find Python in {os_root}")
+    return ["/" + python, fake_pty_path]
