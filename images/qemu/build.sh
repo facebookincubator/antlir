c4a1bcc3476065eef3da9dc108c4b036cbc58060
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2021-06-29 19:55:55 +0200

    Build QEmu with static libs
    This required some trial & error to find out which static packages are required.
    Building pixman from source was also needed, since there's no pixman-static pkg.
    NB: the final build artifacts are installed in /output/qemu

--- images/qemu/build.sh
+++ images/qemu/build.sh
@@ -1,9 +1,26 @@
-#!/usr/bin/env
+#!/usr/bin/env bash
 
-# usage: build.sh path/to/qemu-sources.tar.xz version-number
+# usage: build.sh qemu-version pixman-version
+set -e
 
-tar -xvJf $1
-cd qemu-$2
-./configure # --target-list=x86_64-softmmu --disable-slirp --static --enable-kvm
+# pixman
+tar -xzf pixman.tar.gz
+cd pixman-pixman-$2
+./autogen.sh
+export CFLAGS="$CFLAGS -fPIE"
+mkdir /_temp_qemu/pixman
+./configure --enable-static=yes --prefix=/_temp_qemu/pixman
 make
-# make install
+make install
+cd ..
+
+# qemu
+tar -xJf source.tar.xz
+cd qemu-$1
+export LDFLAGS="$LDFLAGS -L/_temp_qemu/pixman/lib/"
+export CFLAGS="$CFLAGS -I/_temp_qemu/pixman/include/"
+mkdir /output/qemu
+./configure --target-list=x86_64-softmmu --disable-slirp --static --prefix=/output/qemu
+make
+make install
+cd ..
