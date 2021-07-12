be995b78561e99c0e4f54b253316d83b9bebc684
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2021-07-12 16:41:49 -0300

    Clean unneeded RPMs and rename image layers

--- third-party/fedora33/qemu/build.sh
+++ third-party/fedora33/qemu/build.sh
@@ -2,14 +2,14 @@
 
 # usage: build.sh qemu-version pixman-version licap_ng-version
 set -e
+pushd /_temp_qemu
 
 # pixman
 tar -xzf pixman.tar.gz --skip-old-files
 cd pixman-pixman-$2
 ./autogen.sh
-mkdir -p /_temp_qemu/pixman
 export CFLAGS="-fPIE"
-./configure --enable-static=yes --prefix=/_temp_qemu/pixman
+./configure --enable-static=yes --prefix=/output
 make
 make install
 cd ..
@@ -18,8 +18,7 @@ cd ..
 tar -xzf libcap-ng.tar.gz --skip-old-files
 cd libcap-ng-$3
 ./autogen.sh
-mkdir -p /_temp_qemu/libcap-ng
-./configure --enable-static=yes --prefix=/_temp_qemu/libcap-ng
+./configure --enable-static=yes --prefix=/output
 make
 make install
 cd ..
@@ -27,12 +26,12 @@ cd ..
 # qemu
 tar -xJf source.tar.xz --skip-old-files
 cd qemu-$1
-export LDFLAGS="-L/_temp_qemu/pixman/lib/ -L/_temp_qemu/libcap-ng/lib/"
-export CFLAGS="-I/_temp_qemu/pixman/include/ -I/_temp_qemu/libcap-ng/include/"
-export PKG_CONFIG_PATH="/_temp_qemu/pixman/lib/pkgconfig/:/_temp_qemu/libcap-ng/lib/pkgconfig/:/usr/lib64/pkgconfig/"
+export LDFLAGS="-L/output/lib/"
+export CFLAGS="-I/output/include/"
+export PKG_CONFIG_PATH="/output/lib/pkgconfig/:/usr/lib64/pkgconfig/"
 patch meson.build /_temp_qemu/meson.build.patch
 ./configure \
-  --prefix=/output/qemu \
+  --prefix=/output \
   --static \
   --target-list=x86_64-softmmu \
   --disable-slirp \
