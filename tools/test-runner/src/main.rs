ab8977aed61f835af3179b6685cddf63d049130f
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2021-08-01 00:11:36 -0300

    Run tests

--- tools/test-runner/src/main.rs
+++ tools/test-runner/src/main.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufReader, BufRead};
 use std::process;
 use std::process::Stdio;
 use std::env;
@@ -9,10 +9,7 @@ use std::env;
 use structopt::{clap, StructOpt};
 use serde::Deserialize;
 use serde_json;
-use anyhow::{Result, Context, Error};
-
-
-// NOTE: all Optional<_> or defaulted fields are not documented in buck
+use anyhow::{Result, Context, bail};
 
 
 #[derive(Deserialize, Debug)]
@@ -38,20 +35,17 @@ struct TestSpec {
     /// Environment variables to be defined when running the test.
     env: HashMap<String, String>,
 
-    /// Labels that are defined on the test rule.
+    /// Labels that are defined on the test rule. NOTE: not used
     labels: Vec<String>,
 
-    /// Contacts that are defined on the test rule.
+    /// Contacts that are defined on the test rule. NOTE: not used
     contacts: Vec<String>,
 
-    /// Working directory the test should be run from.
+    /// Working directory the test should be run from. XXX: not documented
     cwd: Option<PathBuf>,
 
-    /// Absolute paths to any files required for this test to run.
+    /// Absolute paths to any files required for this test to run. XXX: not documented
     required_paths: Option<Vec<PathBuf>>,
-
-    /// Tuples of required (<test coverage percentage>, <files...>)
-    needed_coverage: Option<Vec<(f32, Vec<PathBuf>)>>,
 }
 
 /// This is the actual test structure used internally.
@@ -67,20 +61,20 @@ fn validate(spec: TestSpec) -> Result<Test> {
     let mut err = String::new();
 
     if spec.command.len() < 1 {
-        let msg = format!("Error: Empty command line\n");
+        let msg = format!("    Error: Empty command line\n");
         err.push_str(&msg);
     }
 
     // if requirements were specified, we better verify they're there
     for req in spec.required_paths.unwrap_or(vec![]) {
         if !req.exists() {
-            let msg = format!("Error: Missing requirement {}\n", req.display());
+            let msg = format!("    Error: Missing requirement {}\n", req.display());
             err.push_str(&msg);
         }
     }
 
     if !err.is_empty() {
-        return Err(Error::msg(err));
+        bail!("Invalid context for running test {}:\n{}", spec.target, err);
 
     // refer to https://doc.rust-lang.org/std/process/struct.Command.html#impl
     // notably, a Command will inherit the current env, working dir and stdin/out/err
@@ -96,7 +90,7 @@ fn validate(spec: TestSpec) -> Result<Test> {
 
         // if no path was specified, use the current one
         let dir = match spec.cwd {
-            None => env::current_dir().unwrap(),
+            None => env::current_dir()?,
             Some(path) => path
         };
         command.current_dir(dir);
@@ -113,10 +107,60 @@ fn validate(spec: TestSpec) -> Result<Test> {
     }
 }
 
-fn run_all(specs: Vec<TestSpec>, threads: u32) -> Result<()> {
-    let tests: Vec<Result<Test>> = specs.into_iter().map(validate).collect();
-    println!("{:?}", tests);
-    Ok(())
+fn run_all(specs: Vec<TestSpec>, threads: u32) -> Result<i32> {
+    let specs: Vec<Result<Test>> = specs.into_iter().map(validate).collect();
+    let mut tests: Vec<Test> = Vec::with_capacity(specs.len());
+
+    // print all errors and exit if there were any, while collecting valid tests
+    let mut quit = false;
+    for spec in specs {
+        match spec {
+            Err(e) => {
+                eprintln!("{}", e);
+                quit = true;
+            }
+            Ok(t) => {
+                tests.push(t);
+            }
+        };
+    }
+    if quit {
+        bail!("Found one or more invalid test specifications, see above");
+    }
+
+    // at this point, we should be able to safely run tests, even if they fail
+    let mut passed = 0;
+    let mut errors: Vec<String> = Vec::new();
+    for test in tests.iter_mut() {
+        let target = &test.target;
+        let mut child = test.command.spawn()?;
+        let status = child.wait()?;
+        if status.success() {
+            println!("[PASS] {}", target);
+            passed += 1;
+        } else {
+            println!("[FAIL] {} ({})", target, status);
+            let mut message = format!("\nTarget {} failed:\n", target);
+            let stderr = child.stderr.take().unwrap();
+            for line in BufReader::new(stderr).lines() {
+                let line = format!("    {}\n", line.unwrap());
+                message.push_str(&line);
+            }
+            errors.push(message);
+        }
+    }
+
+    // if there were any errors, we let the user know what they were
+    for error in errors {
+        eprintln!("{}", error);
+    }
+
+    // whether tests fail or not, we print a summary with a grep-able format
+    let total = tests.len();
+    let percent = 100.0 * passed as f32 / total as f32;
+    println!("Summary: {:.2}% test targets passed ({} out of {})", percent, passed, total);
+
+    Ok((total - passed) as i32)
 }
 
 
@@ -126,15 +170,16 @@ fn run_all(specs: Vec<TestSpec>, threads: u32) -> Result<()> {
     setting = clap::AppSettings::AllowLeadingHyphen, // allows ignored options
 )]
 struct Options {
-    /// [BUCK] JSON file containing test descriptions
+    /// JSON file containing test descriptions. Passed in by buck test
     #[structopt(parse(from_os_str), long = "buck-test-info")]
     spec: PathBuf,
 
-    /// [BUCK] Maximum number of threads used
+    /// Maximum number of threads used. Passed in by buck test
+    // XXX: not documented
     #[structopt(long = "jobs", default_value = "1")]
     threads: u32,
 
-    /// [BUCK] Warns on any further options for forward compatibility
+    /// Warns on any further options for forward compatibility with buck test
     #[structopt(hidden = true)]
     ignored: Vec<String>,
 }
@@ -143,7 +188,7 @@ fn main() -> Result<()> {
     // parse command line
     let options = Options::from_args();
     if options.ignored.len() > 0 {
-        println!(
+        eprintln!(
             "Warning: Unimplemented options were ignored: {:?}\n",
             options.ignored
         );
@@ -159,5 +204,6 @@ fn main() -> Result<()> {
     )?;
 
     // run them
-    run_all(tests, options.threads)
+    let retcode = run_all(tests, options.threads)?;
+    process::exit(retcode);
 }
