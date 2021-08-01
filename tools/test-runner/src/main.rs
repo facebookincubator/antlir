f5c1da5a55571b8ec997f14670c776ffd8f1bb13
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2021-08-01 00:22:37 -0300

    Run rustfmt and move code around

--- tools/test-runner/src/main.rs
+++ tools/test-runner/src/main.rs
@@ -1,16 +1,125 @@
-use std::path::PathBuf;
 use std::collections::HashMap;
+use std::env;
 use std::fs::File;
-use std::io::{BufReader, BufRead};
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
 use std::process;
 use std::process::Stdio;
-use std::env;
 
-use structopt::{clap, StructOpt};
+use anyhow::{bail, Context, Result};
 use serde::Deserialize;
 use serde_json;
-use anyhow::{Result, Context, bail};
+use structopt::{clap, StructOpt};
 
+#[derive(StructOpt, Debug)]
+#[structopt(
+    about = "A custom buck test runner for Antlir's CI",
+    setting = clap::AppSettings::AllowLeadingHyphen, // allows ignored options
+)]
+struct Options {
+    /// JSON file containing test descriptions. Passed in by buck test
+    #[structopt(parse(from_os_str), long = "buck-test-info")]
+    spec: PathBuf,
+
+    /// Maximum number of threads used. Passed in by buck test
+    // XXX: not documented
+    #[structopt(long = "jobs", default_value = "1")]
+    threads: u32,
+
+    /// Warns on any further options for forward compatibility with buck test
+    #[structopt(hidden = true)]
+    ignored: Vec<String>,
+}
+
+fn main() -> Result<()> {
+    // parse command line
+    let options = Options::from_args();
+    if options.ignored.len() > 0 {
+        eprintln!(
+            "Warning: Unimplemented options were ignored: {:?}\n",
+            options.ignored
+        );
+    }
+
+    // collect test information
+    let file = File::open(&options.spec)
+        .with_context(|| format!("Failed to read test specs from {}", options.spec.display()))?;
+    let reader = BufReader::new(file);
+    let tests: Vec<TestSpec> = serde_json::from_reader(reader)
+        .with_context(|| format!("Failed to parse JSON spec from {}", options.spec.display()))?;
+
+    // then run them
+    let retcode = run_all(tests, options.threads)?;
+    process::exit(retcode);
+}
+
+/// This is the actual test structure used internally.
+#[derive(Debug)]
+struct Test {
+    target: String,
+    command: process::Command,
+}
+
+fn run_all(specs: Vec<TestSpec>, threads: u32) -> Result<i32> {
+    // try to validate all test specs before running anything
+    let specs: Vec<Result<Test>> = specs.into_iter().map(validate).collect();
+    let mut tests: Vec<Test> = Vec::with_capacity(specs.len());
+
+    // print all errors and exit if there were any, while collecting valid tests
+    let mut quit = false;
+    for spec in specs {
+        match spec {
+            Err(e) => {
+                eprintln!("{}", e);
+                quit = true;
+            }
+            Ok(t) => {
+                tests.push(t);
+            }
+        };
+    }
+    if quit {
+        bail!("Found one or more invalid test specifications, see above");
+    }
+
+    // at this point, we should be able to safely run tests, even if they fail
+    let total = tests.len();
+    println!("Running {} test targets...", total);
+    let mut passed = 0;
+    let mut errors: Vec<String> = Vec::new();
+    for test in tests.iter_mut() {
+        let target = &test.target;
+        let mut child = test.command.spawn()?;
+        let status = child.wait()?;
+        if status.success() {
+            println!("[PASS] {}", target);
+            passed += 1;
+        } else {
+            println!("[FAIL] {} ({})", target, status);
+            let mut message = format!("\nTarget {} failed:\n", target);
+            let stderr = child.stderr.take().unwrap();
+            for line in BufReader::new(stderr).lines() {
+                let line = format!("    {}\n", line.unwrap());
+                message.push_str(&line);
+            }
+            errors.push(message);
+        }
+    }
+
+    // if there were any errors, we let the user know what they were
+    for error in errors {
+        eprintln!("{}", error);
+    }
+
+    // put a summary in the output, as well in this runner's return code
+    let percent = 100.0 * passed as f32 / total as f32;
+    println!(
+        "  Summary: {:.2}% test targets passed ({} out of {})",
+        percent, passed, total
+    );
+    let fail_count = (total - passed) as i32;
+    Ok(fail_count)
+}
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "snake_case")]
@@ -46,13 +155,6 @@ struct TestSpec {
 
     /// Absolute paths to any files required for this test to run. XXX: not documented
     required_paths: Option<Vec<PathBuf>>,
-}
-
-/// This is the actual test structure used internally.
-#[derive(Debug)]
-struct Test {
-    target: String,
-    command: process::Command,
 }
 
 // validates a spec into an actual test
@@ -91,7 +193,7 @@ fn validate(spec: TestSpec) -> Result<Test> {
         // if no path was specified, use the current one
         let dir = match spec.cwd {
             None => env::current_dir()?,
-            Some(path) => path
+            Some(path) => path,
         };
         command.current_dir(dir);
 
@@ -105,105 +207,4 @@ fn validate(spec: TestSpec) -> Result<Test> {
             command: command,
         });
     }
-}
-
-fn run_all(specs: Vec<TestSpec>, threads: u32) -> Result<i32> {
-    let specs: Vec<Result<Test>> = specs.into_iter().map(validate).collect();
-    let mut tests: Vec<Test> = Vec::with_capacity(specs.len());
-
-    // print all errors and exit if there were any, while collecting valid tests
-    let mut quit = false;
-    for spec in specs {
-        match spec {
-            Err(e) => {
-                eprintln!("{}", e);
-                quit = true;
-            }
-            Ok(t) => {
-                tests.push(t);
-            }
-        };
-    }
-    if quit {
-        bail!("Found one or more invalid test specifications, see above");
-    }
-
-    // at this point, we should be able to safely run tests, even if they fail
-    let mut passed = 0;
-    let mut errors: Vec<String> = Vec::new();
-    for test in tests.iter_mut() {
-        let target = &test.target;
-        let mut child = test.command.spawn()?;
-        let status = child.wait()?;
-        if status.success() {
-            println!("[PASS] {}", target);
-            passed += 1;
-        } else {
-            println!("[FAIL] {} ({})", target, status);
-            let mut message = format!("\nTarget {} failed:\n", target);
-            let stderr = child.stderr.take().unwrap();
-            for line in BufReader::new(stderr).lines() {
-                let line = format!("    {}\n", line.unwrap());
-                message.push_str(&line);
-            }
-            errors.push(message);
-        }
-    }
-
-    // if there were any errors, we let the user know what they were
-    for error in errors {
-        eprintln!("{}", error);
-    }
-
-    // whether tests fail or not, we print a summary with a grep-able format
-    let total = tests.len();
-    let percent = 100.0 * passed as f32 / total as f32;
-    println!("Summary: {:.2}% test targets passed ({} out of {})", percent, passed, total);
-
-    Ok((total - passed) as i32)
-}
-
-
-#[derive(StructOpt, Debug)]
-#[structopt(
-    about = "A custom buck test runner for Antlir's CI",
-    setting = clap::AppSettings::AllowLeadingHyphen, // allows ignored options
-)]
-struct Options {
-    /// JSON file containing test descriptions. Passed in by buck test
-    #[structopt(parse(from_os_str), long = "buck-test-info")]
-    spec: PathBuf,
-
-    /// Maximum number of threads used. Passed in by buck test
-    // XXX: not documented
-    #[structopt(long = "jobs", default_value = "1")]
-    threads: u32,
-
-    /// Warns on any further options for forward compatibility with buck test
-    #[structopt(hidden = true)]
-    ignored: Vec<String>,
-}
-
-fn main() -> Result<()> {
-    // parse command line
-    let options = Options::from_args();
-    if options.ignored.len() > 0 {
-        eprintln!(
-            "Warning: Unimplemented options were ignored: {:?}\n",
-            options.ignored
-        );
-    }
-
-    // collect test information
-    let file = File::open(&options.spec).with_context(||
-        format!("Failed to read test specs from {}", options.spec.display())
-    )?;
-    let reader = BufReader::new(file);
-    let tests: Vec<TestSpec> = serde_json::from_reader(reader).with_context(||
-        format!("Failed to parse JSON spec from {}", options.spec.display())
-    )?;
-
-    // run them
-    let retcode = run_all(tests, options.threads)?;
-    process::exit(retcode);
 }
