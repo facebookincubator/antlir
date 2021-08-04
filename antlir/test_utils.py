e7a2625c314e7ac645b9e8cc063498b816621926
Justin Trudell <jtru@fb.com>
2021-08-04 15:56:50 -0700

    Refactor test_fbpkg_builder_impl.py
    Reviewed By: snarkmaster
    
    Differential Revision: D29921896
    
    fbshipit-source-id: 96c7afd66050a9c30c2ca29b86f0ac8460fbe640

--- antlir/test_utils.py
+++ antlir/test_utils.py
@@ -12,11 +12,19 @@ class TestUtilsMixin:
     useful helpers.
     """
 
+    def assert_call_count(self, mock, expected_count):
+        self.assertEqual(
+            len(mock.mock_calls),
+            expected_count,
+            f"Mock had {len(mock.mock_calls)} calls but we expected it to have "
+            f"{expected_count}: {mock.mock_calls}",
+        )
+
     def assert_call_equality(self, mock, expected_calls, **kwargs):
         """Helper to ensure a given mock had *only* the expected calls by also
         asserting the length of the iterable.
         """
-        self.assertEqual(len(mock.mock_calls), len(expected_calls))
+        self.assert_call_count(mock, len(expected_calls))
         mock.assert_has_calls(expected_calls, **kwargs)
 
     @contextmanager
@@ -30,3 +38,21 @@ class TestUtilsMixin:
         """
         with to_patch as patched:
             yield patched.return_value.__enter__.return_value
+
+    def set_context_manager_retval(self, patched_ctx_mgr, return_value):
+        """Sets return value for a given 'entered' context manager to the
+        provided value.
+
+        Example:
+
+        ### Mocking in the following way:
+        with mock.patch.object(mod, "attr") as patched:
+            self.set_context_manager_retval(patched, 123)
+
+        ### Will cause the following to be true in the system under test
+        from mod import attr
+
+        with attr() as x: assert x == 123
+        """
+        patched_ctx_mgr.return_value.__enter__.return_value = return_value
+        return patched_ctx_mgr
