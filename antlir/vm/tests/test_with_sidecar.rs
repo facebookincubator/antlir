caf16bc0caddaf28b3997a292e08b7da35a6cb65
Vinnie Magro <vmagro@fb.com>
2021-09-15 08:19:51 -0700

    vmtest: make sidecar test more resilient
    Summary:
    - Start sidecars after creating tap device
    - Retry sidecar test a few times in case the network isn't configured
      immediately (for example, non-link-local routes may not yet be setup)
    
    Reviewed By: zeroxoneb
    
    Differential Revision: D30133299
    
    fbshipit-source-id: f02d1f0935f71b9de3450526c7cc41880fc7f892

--- antlir/vm/tests/test_with_sidecar.rs
+++ antlir/vm/tests/test_with_sidecar.rs
@@ -5,23 +5,41 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+use anyhow::{Error, Result};
 use std::net::SocketAddrV6;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 
-#[tokio::test]
-async fn sidecar() {
-    let sock = SocketAddrV6::new("fd00::1".parse().unwrap(), 8080, 0, 2);
-    let mut stream = TcpStream::connect(sock).await.unwrap();
+async fn do_test() -> Result<()> {
+    let sock = SocketAddrV6::new("fd00::1".parse()?, 8080, 0, 2);
+    let mut stream = TcpStream::connect(sock).await?;
 
     // Write some data.
-    stream.write_all(b"hello world!").await.unwrap();
+    stream.write_all(b"hello world!").await?;
     let mut buf = [0; 1024];
-    stream.read(&mut buf).await.unwrap();
+    stream.read(&mut buf).await?;
     assert_eq!(
         std::str::from_utf8(&buf)
             .unwrap()
             .trim_end_matches(char::from(0)),
         "hello world!"
     );
+    Ok(())
+}
+
+#[tokio::test]
+async fn sidecar() -> Result<()> {
+    let mut errors = vec![];
+    // retry a few times in case the network isn't configured immediately
+    for i in 0..3 {
+        match do_test().await {
+            Ok(_) => return Ok(()),
+            Err(e) => {
+                eprintln!("error in attempt {}: {:?}", i, e);
+                errors.push(format!("{:?}", e));
+            }
+        }
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
+    Err(Error::msg(format!("test repeatedly failed: {:#?}", errors)))
 }
