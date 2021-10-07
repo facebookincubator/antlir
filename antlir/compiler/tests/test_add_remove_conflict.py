2430257d56123722d4ca3b2ae6b704b6d9cfe1a6
Naveed Golafshani <naveedgol@fb.com>
2021-10-07 12:42:26 -0700

    Use buck integration to test feature conflict
    Summary:
    Test conflicting features resulting in an image layer build failure in the new buck integration environment.
    
    This diff was motivated by the now deleted comment
    ```
    # We cannot make this an `image.layer` target, since Buck
    # doesn't (yet) have a nice story for testing targets whose
    # builds are SUPPOSED to fail.
    ```
    
    Reviewed By: snarkmaster
    
    Differential Revision: D31404658
    
    fbshipit-source-id: a83dc4fcd26a55cc83757aeb2986eddccd62c605

--- antlir/compiler/tests/test_add_remove_conflict.py
+++ antlir/compiler/tests/test_add_remove_conflict.py
@@ -4,17 +4,11 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import tempfile
 import unittest
 
-from antlir.config import repo_config
-from antlir.fs_utils import Path
-from antlir.subvol_utils import TempSubvolumes
 from antlir.tests.flavor_helpers import render_flavor
-from antlir.tests.layer_resource import layer_resource, layer_resource_subvol
+from antlir.tests.layer_resource import layer_resource_subvol
 from antlir.tests.subvol_helpers import render_subvol
-
-from ..compiler import build_image, parse_args
 
 
 def _test_feature_target(feature_target):
@@ -59,63 +53,3 @@ class AddRemoveConflictTestCase(unittest.TestCase):
             ["(Dir)", {**meta}],
             render_subvol(layer_resource_subvol(__package__, "child")),
         )
-
-    def test_conflict(self):
-        build_appliance_layer_path = layer_resource(
-            __package__, "test-build-appliance"
-        )
-        with TempSubvolumes() as tmp_subvols, (
-            tempfile.NamedTemporaryFile()
-        ) as tf, Path.resource(
-            __package__, "feature_both", exe=False
-        ) as feature_both, Path.resource(
-            __package__, "feature_add", exe=False
-        ) as feature_add, Path.resource(
-            __package__, "feature_remove", exe=False
-        ) as feature_remove, self.assertRaisesRegex(
-            # Removes get built before adds; a conflict means nothing to remove
-            AssertionError,
-            "Path does not exist",
-        ):
-            flavor_config = repo_config().flavor_to_config["antlir_test"]
-
-            # Write the targets_and_outputs file
-            tf.write(
-                Path.json_dumps(
-                    {
-                        _test_feature_target(
-                            "feature_addremove_conflict_add"
-                        ): feature_add,
-                        _test_feature_target(
-                            "feature_addremove_conflict_remove"
-                        ): feature_remove,
-                        flavor_config.build_appliance: (
-                            build_appliance_layer_path
-                        ),
-                    }
-                ).encode()
-            )
-            tf.seek(0)
-
-            subvol = tmp_subvols.caller_will_create("test_conflict")
-            # We cannot make this an `image.layer` target, since Buck
-            # doesn't (yet) have a nice story for testing targets whose
-            # builds are SUPPOSED to fail.
-            with subvol.maybe_create_externally():
-                build_image(
-                    parse_args(
-                        [
-                            "--subvolumes-dir",
-                            subvol.path().dirname(),
-                            "--subvolume-rel-path",
-                            subvol.path().basename(),
-                            "--child-layer-target",
-                            "unused",
-                            f"--child-feature-json={feature_both}",
-                            "--targets-and-outputs",
-                            tf.name,
-                            "--flavor-config",
-                            flavor_config.json(),
-                        ]
-                    )
-                )
