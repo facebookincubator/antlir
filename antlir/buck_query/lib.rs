cd2a17206858219cdd33984f37b8277e6373c82e
Nick Davies <nickdavies@fb.com>
2021-10-26 15:18:35 -0700

    Refactor reusable buck query and target deserialization out of ossify_rust_deps
    Summary: Move some of the code out of the facebook specific directories up into the root of antlir so I can reuse it for my shapes experiment
    
    Reviewed By: vmagro
    
    Differential Revision: D31767523
    
    fbshipit-source-id: 53bde760003b09ac6c4ac311201d2b9c906da223

---
+++ antlir/buck_query/lib.rs
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+mod query;
+mod targets;
+
+pub use query::*;
+pub use targets::*;
---
+++ antlir/buck_query/query.rs
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+use anyhow::{Context, Result};
+use serde::Deserialize;
+use std::path::Path;
+use std::process::{Command, Stdio};
+
+const BUCK_ABS_PATH: &str = "/usr/local/bin/buck";
+
+pub fn buck_query<T: for<'de> Deserialize<'de>>(query: &str, attrs: bool) -> Result<T> {
+    let mut args = vec![];
+    if attrs {
+        args.push("--output-all-attributes");
+    }
+
+    let cmd = if Path::new(BUCK_ABS_PATH).exists() {
+        BUCK_ABS_PATH
+    } else {
+        "buck"
+    };
+
+    let proc = Command::new(cmd)
+        .arg("query")
+        .arg("-c")
+        .arg("antlir.buck-query=1")
+        .arg("--json")
+        .args(args)
+        .arg(query)
+        .stdout(Stdio::piped())
+        .spawn()
+        .context("failed to spawn 'buck query'")?;
+    serde_json::from_reader(proc.stdout.unwrap()).context("failed to parse query output")
+}
