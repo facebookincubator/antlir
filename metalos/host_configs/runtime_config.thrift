54a5dd8e8b6b653f28e450045146a0b4eeea88de
Vinnie Magro <vmagro@fb.com>
2022-01-04 21:41:14 -0800

    metalos: start defining package_manifest and runtime_config thrift files
    Summary:
    package_manifest.thrift defines the set of images to be downloaded on a host
    
    runtime_config.thrift defines the set of software that should be running on a
    host, as well as some options for how to apply that configuration to a running
    system.
    
    Reviewed By: nickdavies
    
    Differential Revision: D33257031
    
    fbshipit-source-id: e21d703077d11ebd30582f8505dbe5028fcea454

---
+++ metalos/host_configs/runtime_config.thrift
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+include "metalos/host_configs/package_manifest.thrift"
+
+// A service is composed of a service image containing the binary and any
+// supporting files, and a config override package to apply.
+struct Service {
+  1: package_manifest.Image image;
+  2: package_manifest.Image config;
+}
+
+// Define the acceptable levels of disruptiveness that are allowed during a
+// config application. MetalOS can compare this with the set of necessary
+// changes in a new Config to determine if the update is safe or not.
+//
+// Order is extremely important in this enum, and each successive value implies
+// that all the previous disruptions are safe.
+enum DisruptivenessAllowed {
+  // It's safe to switch-root to a new version of the MetalOS rootfs, provided
+  // that it does not take down any MetalOS-managed services.
+  NoServiceDowntime = 100,
+  // It's safe to restart any MetalOS-native service that is switching to a new
+  // image/config version.
+  ServiceRestart = 200,
+  // It's safe to restart the WDS container to a new version/config. This is
+  // a separate level because it's more dangerous than restarting MetalOS-native
+  // services.
+  WdsRestart = 250,
+  // It's safe to do anything up to and including rebooting the box. Required
+  // for things like kernel version switches.
+  FullyDown = 1000,
+}
+
+// Options to give to MetalOS when applying a config.
+struct ApplyOpts {
+  1: DisruptivenessAllowed disruptiveness_allowed;
+}
+
+// Config describes the complete set of software that should be running on a
+// host.
+struct Config {
+  1: package_manifest.Image rootfs;
+  2: package_manifest.Image kernel;
+  3: list<Service> services;
+}
+
+// When trying to apply a runtime config, the packages must already be persisted
+// to disk.
+safe permanent client exception ImageNotOnDisk {
+  1: package_manifest.Image image;
+}
+
+// Switch into a new Config. All packages must be downloaded ahead-of-time, or
+// this request will fail.
+struct ApplyConfigRequest {
+  1: ApplyOpts opts;
+  2: Config config;
+}
