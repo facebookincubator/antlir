35132dea2b89be94345aa85f13774f5e63a72e7b
Vinnie Magro <vmagro@fb.com>
2022-01-12 20:02:56 -0800

    metalos: fix switch-root tests
    Summary:
    The switch-root tests are `local_only`, and were broken again.
    This diff adds support for `file://` uris for host configs and a buck macro to
    build static files of host configs for tests.
    
    Reviewed By: nickdavies
    
    Differential Revision: D33551555
    
    fbshipit-source-id: 9cfdb275ba84457a9230577bbf0b91fcb3445346

--- metalos/metalctl/src/apply_host_config.rs
+++ metalos/metalctl/src/apply_host_config.rs
@@ -7,66 +7,86 @@
 
 use std::path::PathBuf;
 
-use anyhow::{bail, Context, Result};
+use anyhow::{anyhow, bail, Context, Result};
 use bytes::Buf;
 use hyper::header::LOCATION;
 use hyper::{StatusCode, Uri};
 use slog::{debug, info, o, Logger};
 use structopt::StructOpt;
+use url::Url;
 
 use evalctx::{Generator, Host};
 
 #[derive(StructOpt)]
 pub struct Opts {
-    host_config_uri: Uri,
+    host_config_uri: Url,
     root: PathBuf,
 }
 
 pub async fn apply_host_config(log: Logger, opts: Opts) -> Result<()> {
     let log = log.new(o!("host-config-uri" => opts.host_config_uri.to_string(), "root" => opts.root.display().to_string()));
 
-    let client = crate::http::client(log.clone()).context("failed to create https client")?;
+    let host: Host = match opts.host_config_uri.scheme() {
+        "http" | "https" => {
+            let client =
+                crate::http::client(log.clone()).context("failed to create https client")?;
 
-    // hyper is a low level client (which is good for our dns connector), but
-    // then we have to do things like follow redirects manually
-    let mut uri = opts.host_config_uri;
-    let mut redirects = 0u8;
-    let resp = loop {
-        let resp = client.get(uri.clone()).await?;
-        if resp.status().is_redirection() {
-            let mut new_uri = resp.headers()[LOCATION]
-                .to_str()?
-                .parse::<Uri>()
-                .context("invalid redirect uri")?
-                .into_parts();
-            if new_uri.scheme.is_none() {
-                new_uri.scheme = uri.scheme().map(|s| s.to_owned());
+            // hyper is a low level client (which is good for our dns connector), but
+            // then we have to do things like follow redirects manually
+            let mut uri: Uri = opts
+                .host_config_uri
+                .as_str()
+                .parse()
+                .context("while converting Url into hyper::Uri")?;
+            let mut redirects = 0u8;
+            let resp = loop {
+                let resp = client.get(uri.clone()).await?;
+                if resp.status().is_redirection() {
+                    let mut new_uri = resp.headers()[LOCATION]
+                        .to_str()?
+                        .parse::<Uri>()
+                        .context("invalid redirect uri")?
+                        .into_parts();
+                    if new_uri.scheme.is_none() {
+                        new_uri.scheme = uri.scheme().map(|s| s.to_owned());
+                    }
+                    if new_uri.authority.is_none() {
+                        new_uri.authority = uri.authority().map(|a| a.to_owned());
+                    }
+                    let new_uri = Uri::from_parts(new_uri)?;
+                    debug!(log, "redirected from {:?} to {:?}", uri, new_uri);
+                    uri = new_uri;
+                    redirects += 1;
+                    if redirects > 10 {
+                        bail!("too many redirects");
+                    }
+                    continue;
+                }
+                info!(log, "downloading image from {:?}", uri);
+                break resp;
+            };
+
+            let status = resp.status();
+            if status != StatusCode::OK {
+                bail!("http response was not OK: {:?}", status);
             }
-            if new_uri.authority.is_none() {
-                new_uri.authority = uri.authority().map(|a| a.to_owned());
-            }
-            let new_uri = Uri::from_parts(new_uri)?;
-            debug!(log, "redirected from {:?} to {:?}", uri, new_uri);
-            uri = new_uri;
-            redirects += 1;
-            if redirects > 10 {
-                bail!("too many redirects");
-            }
-            continue;
+            let body = hyper::body::aggregate(resp.into_body())
+                .await
+                .context("failed to load json body")?;
+            serde_json::from_reader(body.reader()).context("failed to deserialize host json")
         }
-        info!(log, "downloading image from {:?}", uri);
-        break resp;
-    };
+        "file" => {
+            let f = std::fs::File::open(opts.host_config_uri.path())
+                .with_context(|| format!("while opening file {}", opts.host_config_uri.path()))?;
+            serde_json::from_reader(f).context("while deserializing json")
+        }
+        scheme => Err(anyhow!(
+            "Unsupported scheme {} in {:?}",
+            scheme,
+            opts.host_config_uri
+        )),
+    }?;
 
-    let status = resp.status();
-    if status != StatusCode::OK {
-        bail!("http response was not OK: {:?}", status);
-    }
-    let body = hyper::body::aggregate(resp.into_body())
-        .await
-        .context("failed to load json body")?;
-    let host: Host =
-        serde_json::from_reader(body.reader()).context("failed to deserialize host json")?;
     let generators = Generator::load("/usr/lib/metalos/generators")
         .context("failed to load generators from /usr/lib/metalos/generators")?;
     for gen in generators {
