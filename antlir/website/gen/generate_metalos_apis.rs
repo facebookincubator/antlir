2c607bc20aa0a584c404c49f5e6ae934ef7ffbb8
Vinnie Magro <vmagro@fb.com>
2022-01-20 12:56:19 -0800

    build rustdoc sites for oss
    Summary:
    FB hosts these internally automatically, but for oss we have to build them
    ourselves.
    
    Differential Revision: D33691004
    
    fbshipit-source-id: 6400047625f1f2a0f9469261157f567eb77aa49d

--- antlir/website/gen/generate_metalos_apis.rs
+++ antlir/website/gen/generate_metalos_apis.rs
@@ -5,9 +5,11 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use serde::Deserialize;
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, HashMap};
+use std::path::PathBuf;
+use std::process::Command;
 
 use buck_query::buck_query;
 
@@ -24,6 +26,15 @@ impl Library {
     fn intern_link(&self) -> String {
         format!(
             "https://www.internalfb.com/intern/rustdoc/{}:{}/{}/index.html",
+            self.base_path,
+            self.name,
+            self.cr8.as_ref().unwrap_or(&self.name)
+        )
+    }
+
+    fn public_link(&self) -> String {
+        format!(
+            "https://facebookincubator.github.io/antlir/rustdoc/{}:{}/{}/index.html",
             self.base_path,
             self.name,
             self.cr8.as_ref().unwrap_or(&self.name)
@@ -48,6 +59,42 @@ fn main() -> Result<()> {
     }
     println!("\n</FbInternalOnly>");
 
-    println!("<OssOnly>Rustdoc links coming soon</OssOnly>");
+    let cwd = std::env::current_dir().unwrap();
+    let cell_root = find_root::find_buck_cell_root(&cwd)?;
+
+    // for oss, we have to build the rust doc targets and put them in the right
+    // place
+    let out = Command::new("buck")
+        .arg("build")
+        .arg("--show-full-json-output")
+        .args(targets.keys().map(|t| format!("{}#doc", t)))
+        .output()
+        .context("while 'buck build'ing the doc sites")?;
+    let sites: HashMap<String, PathBuf> =
+        serde_json::from_slice(&out.stdout).context("while deserializing buck output")?;
+    let opts = fs_extra::dir::CopyOptions {
+        overwrite: true,
+        ..Default::default()
+    };
+    for (target, out) in sites {
+        let dstdir = cell_root.join(format!(
+            "antlir/website/build/rustdoc/{}",
+            target.trim_start_matches('/').trim_end_matches("#doc")
+        ));
+        std::fs::create_dir_all(&dstdir).context("while creating destination directory")?;
+        fs_extra::dir::copy(&out, &dstdir, &opts)
+            .with_context(|| format!("while copying {} -> {}", out.display(), dstdir.display()))?;
+    }
+
+    println!("<OssOnly>\n");
+    for library in targets.values() {
+        println!(
+            "[//{}:{}]({})  ",
+            library.base_path,
+            library.name,
+            library.public_link()
+        );
+    }
+    println!("\n</OssOnly>");
     Ok(())
 }
