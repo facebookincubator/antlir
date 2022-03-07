9b27e1800f2cbfb9bf58cb5bb262525bb6a00cf2
Vinnie Magro <vmagro@fb.com>
2022-03-07 11:06:51 -0800

    serialize-only serde_systemd crate
    Summary:
    Start implementing `serde_systemd` to serialize systemd unit files from
    strictly-typed rust structs.
    
    Implementing a serde format is useful because it will allow easier interop with
    not just unit files on disk, but also dbus objects.
    
    Granted, serializing is the easy part, but gotta start somewhere.
    
    Reviewed By: pallotron
    
    Differential Revision: D33984859
    
    fbshipit-source-id: 9f3c7d1874e225aafb305488812abe0192590b84

---
+++ metalos/lib/serde_systemd/src/lib.rs
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+pub mod ser;
+
+pub use ser::{to_bytes, to_string, to_writer};
