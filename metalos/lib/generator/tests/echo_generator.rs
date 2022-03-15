fcc81e163c49b7be48c1b9ff2f904fc5f7d637c2
Vinnie Magro <vmagro@fb.com>
2022-03-15 09:27:35 -0700

    library for sandboxed metalos config generators
    Summary:
    Wrapper on top of metalos/lib/sandbox that handles the I/O over thrift with the
    sandboxed generator process.
    
    Reviewed By: pallotron
    
    Differential Revision: D34790182
    
    fbshipit-source-id: 402621c86b3114f06c121f61fc25b47759d81d86

---
+++ metalos/lib/generator/tests/echo_generator.rs
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+use std::io::{Read, Write};
+
+use anyhow::{Context, Result};
+use fbthrift::binary_protocol::{deserialize, serialize};
+
+fn main() -> Result<()> {
+    let mut buf = Vec::new();
+    std::io::stdin()
+        .read_to_end(&mut buf)
+        .context("while reading stdin to buffer")?;
+    eprintln!("read {} bytes from stdin", buf.len());
+    let input: test_if::Input = deserialize(buf).context("while deserializing thrift")?;
+    let output = serialize(test_if::Output { echo: input.hello });
+    std::io::stdout()
+        .write_all(&output)
+        .context("while writing thrift to stdout")?;
+    Ok(())
+}
