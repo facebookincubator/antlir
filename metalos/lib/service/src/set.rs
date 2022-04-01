8cee03cd5ff5ee9cfe699372b42e6074795f2947
Vinnie Magro <vmagro@fb.com>
2022-04-01 10:51:19 -0700

    diff of service sets
    Summary:
    Base library to calculate difference operations to mutate a system from one set
    of service versions to another.
    
    Reviewed By: pallotron
    
    Differential Revision: D34767604
    
    fbshipit-source-id: a3df5d40c8ea03be3416a8cd13ace65c4f941381

---
+++ metalos/lib/service/src/set.rs
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+use std::collections::{BTreeMap, BTreeSet};
+
+use crate::Version;
+
+#[derive(Debug)]
+/// Describe a full set of native services. This may be either the versions that
+/// are currently running, or desired versions.
+pub(crate) struct ServiceSet(BTreeMap<String, Version>);
+
+impl ServiceSet {
+    fn new(map: BTreeMap<String, Version>) -> Self {
+        Self(map)
+    }
+
+    /// Compute a set of service modifications that must be done in order to
+    /// make the `self` set match the desired `other` set.
+    fn diff(&self, other: &Self) -> Diff {
+        let mut diff: BTreeMap<String, _> = BTreeMap::new();
+        let self_keys: BTreeSet<_> = self.0.keys().collect();
+        let other_keys: BTreeSet<_> = other.0.keys().collect();
+        for removed in self_keys.difference(&other_keys) {
+            diff.insert((*removed).clone(), ServiceDiff::Stop(self.0[*removed]));
+        }
+        for added in other_keys.difference(&self_keys) {
+            diff.insert((*added).clone(), ServiceDiff::Start(other.0[*added]));
+        }
+        for maybe_changed in self_keys.intersection(&other_keys) {
+            let old = self.0[*maybe_changed];
+            let new = other.0[*maybe_changed];
+            if old != new {
+                diff.insert(
+                    (*maybe_changed).clone(),
+                    ServiceDiff::Swap {
+                        current: old,
+                        next: new,
+                    },
+                );
+            }
+        }
+
+        Diff(diff)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub(crate) enum ServiceDiff {
+    /// Start this version when no version of the service is currently running
+    Start(Version),
+    /// Swap from the current version to the next
+    Swap { current: Version, next: Version },
+    /// Stop this service
+    Stop(Version),
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub(crate) struct Diff(pub(crate) BTreeMap<String, ServiceDiff>);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use maplit::btreemap;
+    use pretty_assertions::assert_eq;
+
+    impl From<(Option<Version>, Option<Version>)> for ServiceDiff {
+        fn from(pair: (Option<Version>, Option<Version>)) -> ServiceDiff {
+            match pair {
+                (Some(current), Some(next)) => ServiceDiff::Swap { current, next },
+                (Some(current), None) => ServiceDiff::Stop(current),
+                (None, Some(next)) => ServiceDiff::Start(next),
+                (None, None) => panic!("None => None is invalid"),
+            }
+        }
+    }
+
+    macro_rules! service_set {
+        ($($n:literal => $v:literal,)*) => (
+            ServiceSet::new(btreemap! {
+                $($n.into() => format!("00000000-0000-4000-8000-000000000{:03}", $v).parse().unwrap()),*
+            })
+        );
+        ($n:literal => $v:literal) => (service_set! {$n => $v,});
+        () => (ServiceSet::new(BTreeMap::new()));
+    }
+
+    macro_rules! diff {
+        ($($n:literal: $old:expr => $new:expr,)*) => (
+            Diff(btreemap! {
+                $($n.into() => ServiceDiff::from((
+                    $old.map(|x: u8| format!("00000000-0000-4000-8000-000000000{:03}", x).parse().unwrap()),
+                    $new.map(|x: u8| format!("00000000-0000-4000-8000-000000000{:03}", x).parse().unwrap()),
+                ))),*
+            })
+        );
+        ($n:literal: $old:expr => $new:expr) => (diff! {$n: $old => $new,});
+    }
+
+    #[test]
+    fn diff() {
+        assert_eq!(
+            diff! { "a": None => Some(1) },
+            service_set!().diff(&service_set! {"a" => 1})
+        );
+        assert_eq!(
+            diff! { "a": Some(2) => None },
+            service_set! {"a" => 2}.diff(&service_set! {})
+        );
+        assert_eq!(
+            diff! { "a": Some(2) => Some(3) },
+            service_set! {"a" => 2}.diff(&service_set! {"a" => 3})
+        );
+        assert_eq!(
+            diff! { "a": Some(2) => Some(3), "b": Some(3) => None, "c": None => Some(2), },
+            service_set! {"a" => 2, "b" => 3, }.diff(&service_set! {"a" => 3, "c" => 2, })
+        );
+    }
+}
