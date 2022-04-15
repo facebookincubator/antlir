94989132f049e1057d55c34b01b148bd556889c8
Vinnie Magro <vmagro@fb.com>
2022-04-15 11:53:46 -0700

    add generic image downloading
    Summary: Needed for initrd
    
    Reviewed By: nickdavies
    
    Differential Revision: D35677623
    
    fbshipit-source-id: c935e1967f9dd4cf0c855384a30ce073bb10664e

--- metalos/lib/image/src/lib.rs
+++ metalos/lib/image/src/lib.rs
@@ -1,8 +1,10 @@
 use anyhow::Context;
 use async_trait::async_trait;
+use futures::StreamExt;
 use slog::{debug, Logger};
 use std::path::PathBuf;
 use thiserror::Error;
+use tokio::io::AsyncWriteExt;
 
 use btrfs::{SendstreamExt, Subvolume};
 
@@ -21,6 +23,9 @@ pub(crate) mod __private {
 #[async_trait]
 pub trait Package: __private::Sealed {
     type Artifacts;
+
+    /// All of the individual package ids that make up this logical Package
+    fn ids(&self) -> Vec<PackageId>;
 
     /// Load the artifacts(s) associated with this package from disk, if they
     /// exist.
@@ -49,6 +54,10 @@ trait SingleSubvolumePackage: __private::Sealed {
 #[async_trait]
 impl<T: SingleSubvolumePackage + Sync> Package for T {
     type Artifacts = Subvolume;
+
+    fn ids(&self) -> Vec<PackageId> {
+        vec![self.id().clone()]
+    }
 
     fn on_disk(&self) -> Option<Self::Artifacts> {
         let dest = self.path_on_disk();
@@ -98,6 +107,56 @@ impl SingleSubvolumePackage for Kernel {
     const KIND: &'static str = "kernel";
     fn id(&self) -> &PackageId {
         &self.kernel
+    }
+}
+
+impl __private::Sealed for Initrd {}
+
+trait InitrdExt {
+    fn path_on_disk(&self) -> PathBuf;
+}
+
+impl InitrdExt for Initrd {
+    fn path_on_disk(&self) -> PathBuf {
+        metalos_paths::images()
+            .join("initrd")
+            .join(format!("{}:{}", self.id.name, self.id.uuid))
+    }
+}
+
+#[async_trait]
+impl Package for Initrd {
+    type Artifacts = PathBuf;
+
+    fn ids(&self) -> Vec<PackageId> {
+        vec![self.id.clone()]
+    }
+
+    fn on_disk(&self) -> Option<Self::Artifacts> {
+        let dest = self.path_on_disk();
+        if dest.exists() { Some(dest) } else { None }
+    }
+
+    async fn download<D>(&self, log: Logger, dl: D) -> anyhow::Result<Self::Artifacts>
+    where
+        D: download::Downloader + Send + Sync,
+        <D as download::Downloader>::Sendstream: Send,
+    {
+        if let Some(artifacts) = self.on_disk() {
+            return Ok(artifacts);
+        }
+
+        let mut dest = tokio::fs::File::create(self.path_on_disk()).await?;
+        let mut stream = dl
+            .open_bytes_stream(log, &self.id)
+            .await
+            .with_context(|| format!("while starting http stream for {:?}", self.id))?;
+
+        while let Some(item) = stream.next().await {
+            dest.write_all(&item?).await?;
+        }
+
+        Ok(self.path_on_disk())
     }
 }
 
