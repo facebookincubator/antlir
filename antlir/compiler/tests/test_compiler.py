9947bd3e4cea4d7162c9c389bde39073278311ef
Naveed Golafshani <naveedgol@fb.com>
2022-04-21 10:56:54 -0700

    Augment integration test for compiler for 100% coverage
    Summary:
    We wanted to kill the current `test_compiler.py` in favor of `test_compiler_integration.py` beacuse
    - the original `test_compiler.py` was overly brittle due to its dependance on mocks, making it hard to make compiler changes
    - Was not as functionally exhaustive at testing as `test_compiler_integration.py` as its mocks skip over the important guts of compiler items
    
    However, it was still useful for test coverage purposes as lines executed within the nspawned inner compiler aren't counted in  `test_compiler_integration.py`.
    
    This diff raises `test_compiler_integration.py` coverage up to 100% via the following
    - Migrating flavor related tests from `test_compiler.py`
    - Adding a no BA test to cover the lines that aren't accounted for by inner compiler execution
    - Moves testing the profiler to its own test (profiling disables the multithreading path in the compiler)
    
    This test only compiles a single item/feature (install `empty`) to reach 100% coverage.
    
    Reviewed By: vmagro
    
    Differential Revision: D35581071
    
    fbshipit-source-id: f104435895ee3c7369e2cb6667ab7a3de5747d37

--- antlir/compiler/tests/test_compiler.py
+++ antlir/compiler/tests/test_compiler.py
@@ -12,6 +12,7 @@ import os
 import socket
 import tempfile
 import unittest
+from typing import List, Optional
 from uuid import UUID
 
 from antlir.btrfsutil import subvolume_info
@@ -29,13 +30,28 @@ from antlir.tests.subvol_helpers import render_subvol
 from .. import subvolume_on_disk as svod
 from ..compiler import build_image, parse_args
 
+_TEST_BA_TARGET_PATH = (
+    "fbcode//antlir/compiler/test_images:build_appliance_testing"
+)
+_TEST_BA = layer_resource(__package__, "test-build-appliance")
+_TEST_BA_SUBVOL = layer_resource_subvol(__package__, "test-build-appliance")
+
 
 class CompilerIntegrationTestCase(unittest.TestCase):
-    def test_compile(self):
-        with TempSubvolumes() as temp_subvolumes, temp_dir() as profile_dir:  # noqa: E501
+    def compile(
+        self,
+        *,
+        extra_args: Optional[List[str]] = None,
+        include_flavor_config: bool = True,
+        include_build_appliance: bool = True,
+    ):
+        extra_args = extra_args or []
+        with TempSubvolumes() as temp_subvolumes:
             flavor_config = flavor_config_t(
                 name="antlir_test",
-                build_appliance="build-appliance-testing",
+                build_appliance=_TEST_BA_TARGET_PATH
+                if include_build_appliance
+                else None,
                 rpm_installer="dnf",
                 rpm_repo_snapshot=RPM_DEFAULT_SNAPSHOT_FOR_INSTALLER_DIR
                 / "dnf",
@@ -49,9 +65,8 @@ class CompilerIntegrationTestCase(unittest.TestCase):
             subvol_rel_path = b"test/nested"
 
             deps = {
-                "build-appliance-testing": layer_resource(
-                    __package__, "test-build-appliance"
-                )
+                _TEST_BA_TARGET_PATH: _TEST_BA,
+                "fbcode//antlir:empty": layer_resource(__package__, "empty"),
             }
 
             # We write out tf to the temp subvol dir because it provides
@@ -66,15 +81,25 @@ class CompilerIntegrationTestCase(unittest.TestCase):
                     subvolumes_dir,
                     "--subvolume-rel-path",
                     subvol_rel_path,
-                    "--flavor-config",
-                    flavor_config.json(),
+                    *(
+                        (
+                            "--flavor-config",
+                            flavor_config.json(),
+                        )
+                        if include_flavor_config
+                        else ()
+                    ),
                     "--compiler-binary",
                     os.environ["test_antlir_compiler_binary_path"],
                     "--child-layer-target",
                     "CHILD_TARGET",
                     "--targets-and-outputs",
                     tf.name,
-                    f"--profile={profile_dir}",
+                    # We need to compile at least one feature to cover the code
+                    # for compiler item building in `compile_items_to_subvol`.
+                    "--child-feature-json",
+                    os.environ["test_compiler_feature"],
+                    *extra_args,
                 ]
                 res = build_image(parse_args(argv), argv)
 
@@ -103,6 +128,7 @@ class CompilerIntegrationTestCase(unittest.TestCase):
                                 ],
                             },
                         ],
+                        "empty": ["(File m444)"],
                     },
                 ],
                 render_subvol(sv_nested),
@@ -117,14 +143,38 @@ class CompilerIntegrationTestCase(unittest.TestCase):
                         svod._HOSTNAME: socket.gethostname(),
                         svod._SUBVOLUMES_BASE_DIR: subvolumes_dir,
                         svod._SUBVOLUME_REL_PATH: subvol_rel_path,
-                        svod._BUILD_APPLIANCE_PATH: layer_resource_subvol(
-                            __package__, "test-build-appliance"
-                        ).path(),
+                        svod._BUILD_APPLIANCE_PATH: _TEST_BA_SUBVOL.path()
+                        if include_build_appliance
+                        else None,
                     }
                 ),
                 res,
             )
 
+    def test_profiler(self):
+        with temp_dir() as profile_dir:
+            self.compile(extra_args=[f"--profile={profile_dir}"])
             # This profile won't actually be populated since the profiling and
             # pstat dump should occur outside `build_image`.
             self.assertTrue((profile_dir / "CHILD_TARGET.pstat").exists())
+
+    def test_no_flavor_config_or_parent_layer_error(self):
+        with self.assertRaisesRegex(
+            AssertionError,
+            "Parent layer must be given if no flavor config is given",
+        ):
+            self.compile(include_flavor_config=False)
+
+    def test_write_parent_flavor(self):
+        self.compile(
+            extra_args=[
+                "--parent-layer",
+                _TEST_BA,
+            ],
+            include_flavor_config=False,
+        )
+
+    def test_compiler_no_ba(self):
+        # In the case where no BA is available, we still expect the compiler to
+        # successfully compile by skipping the compiler re-invocation nspawn.
+        self.compile(include_build_appliance=False)
