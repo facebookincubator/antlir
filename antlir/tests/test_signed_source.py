386cea8aeed6d6eb146bb3d8991b1f363d72cd74
Alexey Spiridonov <lesha@fb.com>
2022-05-12 17:04:27 -0700

    Library with `SignedSource` logic
    Summary:
    We use this in "update packages", and **should** use it in:
     - generated allowed version files
     - wanted-rpms files (next diff)
    
    Reviewed By: zeroxoneb
    
    Differential Revision: D36357152
    
    fbshipit-source-id: f23ed9358578831b76fbe2b5a1fb8bdffa384db4

---
+++ antlir/tests/test_signed_source.py
@@ -0,0 +1,41 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from ..errors import UserError
+from ..signed_source import (
+    signed_source_sigil,
+    sign_source,
+    assert_signed_source,
+)
+from .common import AntlirTestCase
+
+
+_HELLO_WORLD = "hello SignedSource<<9f94b0b1eddcee39813128cd51ef0e47>> world!"
+
+
+class SignedSourceTestCase(AntlirTestCase):
+    def test_sign(self):
+        self.assertEqual(
+            _HELLO_WORLD,
+            sign_source(f"hello {signed_source_sigil()} world!"),
+        )
+
+    def test_sign_error(self):
+        with self.assertRaisesRegex(
+            RuntimeError,
+            r"^First .* lack `signed_source_sigil\(\)`: aaa$",
+        ):
+            sign_source("aaa")
+
+    def test_verify(self):
+        assert_signed_source(_HELLO_WORLD, "_HELLO_WORLD")
+        with self.assertRaisesRegex(
+            UserError, "Invalid signed source: BAD1. .* lacks a SignedSource "
+        ):
+            assert_signed_source("look ma, no token", "BAD1")
+        with self.assertRaisesRegex(
+            UserError, "Invalid signed source: BAD2. .* should explain "
+        ):
+            assert_signed_source(_HELLO_WORLD + " bye.", "BAD2")
