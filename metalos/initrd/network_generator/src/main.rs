3e3ec055cd68cce926ee1769692ff243370e0879
Nick Davies <nickdavies@fb.com>
2022-05-16 11:11:21 -0700

    Make simplified disk boot initrd generator
    Summary: This generator now only configures the network and does nothing else.
    
    Reviewed By: r1mikey
    
    Differential Revision: D36363009
    
    fbshipit-source-id: ea025b8cd956b26be443735f9f8599e0ec5d1615

---
+++ metalos/initrd/network_generator/src/main.rs
@@ -0,0 +1,49 @@
+use anyhow::{Context, Result};
+use structopt::clap::AppSettings;
+use structopt::StructOpt;
+use strum_macros::EnumIter;
+
+use kernel_cmdline::{GenericCmdlineOpt, KernelCmdArgs, KnownArgs};
+use net_utils::get_mac;
+use network_generator_lib::generator_main;
+
+#[derive(EnumIter)]
+pub enum NetworkGeneratorKnownArgs {
+    MacAddress,
+}
+
+impl KnownArgs for NetworkGeneratorKnownArgs {
+    fn flag_name(&self) -> &'static str {
+        match self {
+            Self::MacAddress => "--macaddress",
+        }
+    }
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "kernel-cmdline", setting(AppSettings::NoBinaryName))]
+pub struct NetworkGeneratorArgs {
+    #[structopt(parse(from_str = GenericCmdlineOpt::parse_arg))]
+    #[allow(dead_code)]
+    non_metalos_opts: Vec<GenericCmdlineOpt>,
+
+    #[structopt(long = &NetworkGeneratorKnownArgs::MacAddress.flag_name())]
+    pub mac_address: Option<String>,
+}
+
+impl KernelCmdArgs for NetworkGeneratorArgs {
+    type Args = NetworkGeneratorKnownArgs;
+}
+
+fn main() -> Result<()> {
+    generator_main(|| {
+        let kargs = NetworkGeneratorArgs::from_proc_cmdline()
+            .context("Failed to read kernel command line")?;
+
+        Ok(match kargs.mac_address {
+            Some(mac) => mac,
+            None => get_mac()
+                .context("Failed to auto-detect mac address (not provided on kernel cmdline)")?,
+        })
+    })
+}
