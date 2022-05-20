8c5913da1f9d1f578b6e4381154b41e66f487d1b
Vinnie Magro <vmagro@fb.com>
2022-05-20 12:14:27 -0700

    serialize individual parts from buck macro
    Summary: It's useful to get the individual pieces for installing into disk images
    
    Reviewed By: zeroxoneb
    
    Differential Revision: D36551916
    
    fbshipit-source-id: 7392f9de22dc2bd95554d69e4a00643a8311f60d

--- metalos/host_configs/tests/serialize_host_struct.rs
+++ metalos/host_configs/tests/serialize_host_struct.rs
@@ -1,22 +1,44 @@
 use anyhow::Result;
 use bytes::{Buf, BufMut, Bytes, BytesMut};
+use clap::{ArgEnum, Parser};
 use example_host_for_tests::example_host_for_tests;
 use metalos_host_configs::host::HostConfig;
 
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(long)]
+    example: bool,
+    #[clap(arg_enum, default_value_t=Piece::Host)]
+    piece: Piece,
+}
+
+#[derive(ArgEnum, Clone, Debug)]
+enum Piece {
+    Host,
+    Provisioning,
+    Boot,
+    Runtime,
+}
+
 fn main() -> Result<()> {
-    let mut args: Vec<_> = std::env::args().collect();
-    args.remove(0);
-    let h: HostConfig = match args.first().map(String::as_str) {
-        Some("example") => example_host_for_tests(),
-        Some(arg) => panic!("arg must be example or not set, not '{}'", arg),
-        None => {
+    let args = Args::parse();
+
+    let h: HostConfig = match args.example {
+        true => example_host_for_tests(),
+        false => {
             let mut writer = BytesMut::new().writer();
             std::io::copy(&mut std::io::stdin(), &mut writer)?;
             let input: Bytes = writer.into_inner().into();
             fbthrift::simplejson_protocol::deserialize(&input)?
         }
     };
-    let json = fbthrift::simplejson_protocol::serialize(&h);
-    std::io::copy(&mut json.reader(), &mut std::io::stdout())?;
+
+    let out_json = match args.piece {
+        Piece::Host => fbthrift::simplejson_protocol::serialize(&h),
+        Piece::Provisioning => fbthrift::simplejson_protocol::serialize(&h.provisioning_config),
+        Piece::Boot => fbthrift::simplejson_protocol::serialize(&h.boot_config),
+        Piece::Runtime => fbthrift::simplejson_protocol::serialize(&h.runtime_config),
+    };
+    std::io::copy(&mut out_json.reader(), &mut std::io::stdout())?;
     Ok(())
 }
