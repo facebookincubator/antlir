652f2697448036f1b57bb38b796d5729a58d7cb4
Vinnie Magro <vmagro@fb.com>
2022-05-23 20:12:31 -0700

    better docs page for boot flows
    Summary: Rewrite text and include a diagram
    
    Reviewed By: mimir-d
    
    Differential Revision: D36599311
    
    fbshipit-source-id: c9e7fec2e8e384f155c31d38af95a8f358b1fc6c

--- antlir/website/sidebars.js
+++ antlir/website/sidebars.js
@@ -144,7 +144,7 @@ module.exports = {
         'metalos/lifecycle',
         'metalos/native-services',
         'metalos/update-flows',
-        'metalos/disk-booting',
+        'metalos/boot',
         'metalos/apis',
         ...fbInternalOnly([
           {
