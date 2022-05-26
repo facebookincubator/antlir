5847d3ca5b809a9d2cc1ba7a11f510d7f83771da
Nick Davies <nickdavies@fb.com>
2022-05-26 14:04:31 -0700

    Define an antlir image for the GPT partition and control layer
    Summary: This uses the library in the previous diff and defines some basic things this layer provides
    
    Reviewed By: pzmarzly
    
    Differential Revision: D36417875
    
    fbshipit-source-id: 3987f50a36e9605901c23084f90c5248b065c822

---
+++ metalos/disk/root_disk_image/src/lib.rs
@@ -0,0 +1,39 @@
+use antlir_image::path::VerifiedPath;
+use antlir_image::subvolume::AntlirSubvolume;
+use antlir_image::{generate_layer, generate_partitions, generate_paths, generate_subvolumes};
+
+generate_layer! {
+    ControlLayer {
+        paths: ControlPaths,
+    }
+}
+
+generate_paths! {
+    ControlPaths {
+        image_initrd(VerifiedPath, "image/initrd"),
+        image_kernel(VerifiedPath, "image/kernel"),
+        image_rootfs(VerifiedPath, "image/rootfs"),
+        image_service(VerifiedPath, "image/service"),
+        image_service_config_generator(VerifiedPath, "image/service-config-generator"),
+        run_boot(VerifiedPath, "run/boot"),
+        run_cache(VerifiedPath, "run/cache"),
+        run_logs(VerifiedPath, "run/logs"),
+        run_runtime(VerifiedPath, "run/runtime"),
+        run_scratch(VerifiedPath, "run/scratch"),
+        run_service_roots(VerifiedPath, "run/service-roots"),
+        run_state(VerifiedPath, "run/state"),
+        run_state_metalos(VerifiedPath, "run/state/metalos"),
+    }
+}
+
+generate_subvolumes! {
+    RootDiskSubvolumes {
+        volume(AntlirSubvolume<ControlLayer>, "/volume"),
+    }
+}
+
+generate_partitions! {
+    RootDiskGPT {
+        p2(RootDiskSubvolumes, 2),
+    }
+}
