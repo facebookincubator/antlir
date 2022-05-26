6a45809e3ab1ca77610b9f89095c21b68c553316
Nick Davies <nickdavies@fb.com>
2022-05-26 14:04:31 -0700

    Add safe mounter which uses the antlir_image library
    Summary:
    This provides a safe way to mount a known image type into a particular mountpoint.
    This means that you now get compile time guarantees that if this mount occurs the paths you use later in the image exist
    
    Reviewed By: epilatow
    
    Differential Revision: D36417876
    
    fbshipit-source-id: 55273849de6067ec524dc5ff46e97dd77c058751

--- metalos/lib/mount/src/lib.rs
+++ metalos/lib/mount/src/lib.rs
@@ -10,6 +10,11 @@ use std::path::{Path, PathBuf};
 use nix::mount::{MntFlags, MsFlags};
 use slog::{info, Logger};
 
+use antlir_image::layer::AntlirLayer;
+use antlir_image::partition::Partition;
+use antlir_image::path::VerifiedPath;
+use antlir_image::subvolume::{AntlirSubvolume, AntlirSubvolumes};
+
 #[derive(thiserror::Error, Debug)]
 pub enum MountError {
     #[error("No such file or directory: Mount target {0:?} doesn't exist")]
@@ -18,8 +23,78 @@ pub enum MountError {
     MissingTarget(PathBuf),
     #[error("No such file or directory: Unknown reason - both source/target exist")]
     MissingUnknown,
+    #[error("Path {0:?} provided as subvolume path is not valid unicode")]
+    InvalidSubvolume(PathBuf),
     #[error("Unknown error occured: {0:?}")]
     Unknown(#[from] nix::errno::Errno),
+}
+
+pub trait SafeMounter {
+    fn mount_btrfs<'a, S, L, F>(
+        &'a self,
+        source: &Partition<S>,
+        target: VerifiedPath,
+        flags: MsFlags,
+        make_subvolume: F,
+        //TODO support data
+    ) -> Result<L, MountError>
+    where
+        S: AntlirSubvolumes,
+        L: AntlirLayer,
+        F: Fn(&Partition<S>) -> AntlirSubvolume<L>;
+
+    fn umount(&self, mountpoint: &Path, force: bool) -> Result<(), nix::errno::Errno>;
+}
+
+pub struct RealSafeMounter {
+    pub log: Logger,
+}
+impl SafeMounter for RealSafeMounter {
+    fn mount_btrfs<'a, S, L, F>(
+        &'a self,
+        source: &Partition<S>,
+        target: VerifiedPath,
+        flags: MsFlags,
+        make_subvolume: F,
+    ) -> Result<L, MountError>
+    where
+        S: AntlirSubvolumes,
+        L: AntlirLayer,
+        F: Fn(&Partition<S>) -> AntlirSubvolume<L>,
+    {
+        let subvolume = make_subvolume(source);
+
+        let data = format!(
+            "subvol={}",
+            match subvolume.relative_path.to_str() {
+                Some(p) => p,
+                None => {
+                    return Err(MountError::InvalidSubvolume(
+                        subvolume.relative_path.to_path_buf(),
+                    ));
+                }
+            }
+        );
+
+        RealMounter {
+            log: self.log.clone(),
+        }
+        .mount(
+            source.path.path(),
+            target.path(),
+            Some("btrfs"),
+            flags,
+            Some(&data),
+        )?;
+        Ok(subvolume.mount_unchecked(target))
+    }
+
+    fn umount(&self, mountpoint: &Path, force: bool) -> Result<(), nix::errno::Errno> {
+        RealMounter {
+            log: self.log.clone(),
+        }
+        .umount(mountpoint, force)
+    }
 }
 
 #[mockall::automock]
