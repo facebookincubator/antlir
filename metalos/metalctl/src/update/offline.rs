e7203ead81be7184b6501b5431ab2a23441fa7a9
Vinnie Magro <vmagro@fb.com>
2022-06-02 14:02:33 -0700

    metalctl offline-update
    Summary:
    Basic (fully working, but not amazing error classification) version of
    `metalctl offline-update {stage,commit}`
    
    Reviewed By: nickdavies
    
    Differential Revision: D35665284
    
    fbshipit-source-id: a67f6322917d0f4ffb997cd8c91077bd097444fa

--- metalos/metalctl/src/update/offline.rs
+++ metalos/metalctl/src/update/offline.rs
@@ -5,24 +5,107 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-use anyhow::Result;
-use slog::{info, Logger};
+use anyhow::{Context, Result};
+use slog::{info, o, trace, Logger};
 
-use super::Subcommand;
-use super::Subcommand::{Commit, Stage};
+use metalos_host_configs::api::{
+    OfflineUpdateCommitError as CommitError, OfflineUpdateCommitErrorCode as CommitErrorCode,
+    UpdateStageError as StageError, UpdateStageResponse as StageResponse,
+};
+use metalos_host_configs::boot_config::BootConfig;
+use metalos_host_configs::host::HostConfig;
+use metalos_kexec::KexecInfo;
+use state::State;
 
-pub(super) async fn run(log: Logger, sub: Subcommand) -> Result<()> {
-    match sub {
-        Stage(opts) => {
-            info!(
-                log,
-                "I'm staging an offline update! {:?}",
-                opts.load::<String>()
-            );
-            unimplemented!("offline-update stage")
-        }
-        Commit => {
-            unimplemented!("offline-update commit")
-        }
+fn map_stage_err<E>(prefix: &'static str) -> impl Fn(E) -> StageError
+where
+    E: std::fmt::Display,
+{
+    move |e: E| StageError {
+        message: format!("{}: {}", prefix, e),
+        // TODO(T111087410): include the list of packages
+        packages: vec![],
     }
+}
+
+pub(super) async fn stage(
+    log: Logger,
+    boot_config: BootConfig,
+) -> Result<StageResponse, StageError> {
+    lifecycle::stage(log.clone(), boot_config)
+        .await
+        .map_err(map_stage_err("while staging BootConfig"))?;
+    // TODO(T111087410): return the list of packages
+    Ok(StageResponse { packages: vec![] })
+}
+
+fn map_commit_err<E>(prefix: &'static str) -> impl Fn(E) -> CommitError
+where
+    E: std::fmt::Display,
+{
+    move |e: E| CommitError {
+        code: CommitErrorCode::Other,
+        message: format!("{}: {}", prefix, e),
+    }
+}
+
+pub(super) async fn commit(log: Logger, boot_config: BootConfig) -> Result<(), CommitError> {
+    let log = log.new(o!("boot-config" => format!("{:?}", boot_config)));
+    trace!(log, "beginning offline-update commit");
+
+    let staged_config = BootConfig::staged()
+        .ok()
+        .flatten()
+        .ok_or_else(|| CommitError {
+            code: CommitErrorCode::NotStaged,
+            message: "no boot config is staged yet".to_string(),
+        })?;
+    if staged_config != boot_config {
+        return Err(CommitError {
+            code: CommitErrorCode::NotStaged,
+            message: format!(
+                "{:?} does not match the staged boot config {:?}",
+                boot_config, staged_config
+            ),
+            ..Default::default()
+        });
+    }
+
+    // merge the new BootConfig with the full HostConfig
+    let mut host_config = HostConfig::current()
+        .context("while loading committed HostConfig")
+        .map_err(map_commit_err("could not load committed HostConfig"))?
+        .ok_or_else(|| CommitError {
+            code: CommitErrorCode::Other,
+            message: "no committed HostConfig".into(),
+        })?;
+    host_config.boot_config = boot_config;
+    let host_config_token = host_config
+        .save()
+        .context("while saving merged HostConfig")
+        .map_err(map_commit_err("could not save merged HostConfig"))?;
+    host_config_token
+        .stage()
+        .context("while staging merged HostConfig")
+        .map_err(map_commit_err("could not stage merged HostConfig"))?;
+    info!(log, "marked merged HostConfig as staged");
+    // TODO(T121845483): mark this as pre-committed, and have the setup initrd
+    // mark it as committed
+    host_config_token
+        .commit()
+        .context("while committing merged HostConfig")
+        .map_err(map_commit_err("could not commit merged HostConfig"))?;
+    info!(log, "marked merged HostConfig as committed");
+
+    let kexec_info = KexecInfo::new_from_packages(
+        &host_config.boot_config.kernel,
+        &host_config.boot_config.initrd,
+        host_config.boot_config.kernel.cmdline.clone(),
+    )
+    .map_err(map_commit_err("could not build KexecInfo"))?;
+
+    kexec_info
+        .kexec(log)
+        .await
+        .map_err(map_commit_err("could not kexec"))
 }
