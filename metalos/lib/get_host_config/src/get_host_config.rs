50445e7d971fd92417c4b1b703fc4355a43ab742
Vinnie Magro <vmagro@fb.com>
2022-06-02 14:14:27 -0700

    prefer downloading binary thrift
    Summary:
    Binary thrift is easier to maintain backwards compatibility, so prefer to
    download that but still take json if that is what we're given
    
    Reviewed By: pallotron
    
    Differential Revision: D36874845
    
    fbshipit-source-id: a25edea883f407129aac089d0fe10f73c665b299

--- metalos/lib/get_host_config/src/get_host_config.rs
+++ metalos/lib/get_host_config/src/get_host_config.rs
@@ -6,11 +6,15 @@
  */
 
 use anyhow::{anyhow, Context, Result};
-use fbthrift::simplejson_protocol::deserialize;
+use fbthrift::binary_protocol::deserialize as binary_deserialize;
+use fbthrift::simplejson_protocol::deserialize as json_deserialize;
 use metalos_host_configs::host::HostConfig;
+use reqwest::header::HeaderValue;
 use reqwest::Client;
 use std::path::Path;
 use url::Url;
+
+static THRIFT_CONTENT_TYPE: HeaderValue = HeaderValue::from_static("application/x-thrift");
 
 pub fn client() -> Result<Client> {
     Client::builder()
@@ -23,22 +27,35 @@ pub fn client() -> Result<Client> {
 pub fn get_host_config_from_file(path: &Path) -> Result<HostConfig> {
     let blob =
         std::fs::read(path).with_context(|| format!("while opening file {}", path.display()))?;
-    deserialize(blob).context("while deserializing from json")
+    match json_deserialize(&blob).context("while deserializing json thrift") {
+        Ok(hc) => Ok(hc),
+        Err(json_err) => binary_deserialize(blob)
+            .context("while deserializing binary thrift")
+            .context(json_err),
+    }
 }
 
 pub async fn get_host_config(uri: &Url) -> Result<HostConfig> {
     match uri.scheme() {
         "http" | "https" => {
             let client = client()?;
-            let bytes = client
+            let resp = client
                 .get(uri.clone())
+                .header(http::header::CONTENT_TYPE, THRIFT_CONTENT_TYPE.clone())
                 .send()
                 .await
-                .with_context(|| format!("while GETting {}", uri))?
+                .with_context(|| format!("while GETting {}", uri))?;
+            let content_type: Option<HeaderValue> =
+                resp.headers().get(http::header::CONTENT_TYPE).cloned();
+            let bytes = resp
                 .bytes()
                 .await
-                .context("while downloading host json")?;
-            deserialize(bytes).context("while deserializing from json")
+                .context("while downloading response body")?;
+            if Some(&THRIFT_CONTENT_TYPE) == content_type.as_ref() {
+                binary_deserialize(bytes).context("while deserializing binary thrift")
+            } else {
+                json_deserialize(bytes).context("while deserializing json thrift")
+            }
         }
         "file" => get_host_config_from_file(Path::new(uri.path())),
         scheme => Err(anyhow!("Unsupported scheme {} in {:?}", scheme, uri)),
