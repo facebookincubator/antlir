8509d2ead1973fb3db12274280c21c1800df12bd
Vinnie Magro <vmagro@fb.com>
2022-06-07 19:34:48 -0700

    start rust crate exposed to python
    Summary:
    Start with just a wrapper around `antlir.fs_utils.Path` which makes it possible
    to pass a `Path` to and from Rust without any hacks.
    
    This demonstrates how to add a Rust-based Python module to antlir and expose it
    with a nice interface without a lot of boilerplate.
    
    Reviewed By: nickdavies
    
    Differential Revision: D36712446
    
    fbshipit-source-id: 11ca276f8fb0b8ef9b9bb26eb8bc35e9765dfa13

---
+++ antlir/rust/src/fs_utils.rs
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+use std::ffi::OsStr;
+use std::os::unix::ffi::OsStrExt;
+use std::path::PathBuf;
+
+use pyo3::exceptions::PyTypeError;
+use pyo3::prelude::*;
+use pyo3::types::{PyBytes, PyString};
+
+/// Wrapper for Antlir Paths (antlir.fs_utils.Path)
+/// In Rust, this is a PathBuf, and in Python it is an antlir.fs_utils.Path
+/// This intentionally implements very few useful traits directly, aside from
+/// converting to a Path(Buf), because it should almost never be passed around
+/// internally, instead it should only be used as function parameters or return
+/// values.
+#[repr(transparent)]
+pub struct AntlirPath(PathBuf);
+
+impl From<PathBuf> for AntlirPath {
+    fn from(p: PathBuf) -> Self {
+        Self(p)
+    }
+}
+
+impl From<AntlirPath> for PathBuf {
+    fn from(p: AntlirPath) -> PathBuf {
+        p.0
+    }
+}
+
+impl IntoPy<PyObject> for AntlirPath {
+    fn into_py(self, py: Python) -> PyObject {
+        // This is breaking our unwritten golden rule of never go
+        // Python->Rust->Python, but it's a necessary indirection evil
+        // until/unless we replace antlir.fs_utils.Path with a Rust
+        // implementation
+        let fs_utils =
+            PyModule::import(py, "antlir.fs_utils").expect("antlir.fs_utils must be available");
+        let bytes = PyBytes::new(py, self.0.as_os_str().as_bytes());
+        // finally, create a fs_utils.Path from the bytes object
+        fs_utils
+            .getattr("Path")
+            .expect("antlir.fs_utils always has Path")
+            .call1((bytes,))
+            .expect("antlir.fs_utils.Path failed to take a byte string")
+            .into()
+    }
+}
+
+impl<'source> FromPyObject<'source> for AntlirPath {
+    fn extract(p: &'source PyAny) -> PyResult<Self> {
+        // first attempt to get a raw bytes string, which most paths should
+        // already be
+        if let Ok(bytes) = p.cast_as::<PyBytes>() {
+            Ok(PathBuf::from(OsStr::from_bytes(bytes.as_bytes())).into())
+        } else {
+            // if it's not already `bytes`, then hopefully it's a `str`,
+            // otherwise we are out of ideas and can't convert it
+            match p.cast_as::<PyString>() {
+                Ok(str) => Ok(Self(str.to_str()?.into())),
+                Err(_) => Err(PyTypeError::new_err(format!(
+                    "{} is neither bytes nor str",
+                    p.repr()?
+                ))),
+            }
+        }
+    }
+}
+
+#[pymodule]
+pub(crate) fn fs_utils(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    /// Largely just useful for tests from Python, this will take the given
+    /// input and attempt to round-trip it through [Path] and back into an
+    /// `antlir.fs_utils.Path`
+    #[pyfn(m)]
+    #[pyo3(name = "Path")]
+    fn path(p: &PyAny) -> PyResult<AntlirPath> {
+        p.extract()
+    }
+    Ok(())
+}
---
+++ antlir/rust/src/lib.rs
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+use pyo3::prelude::*;
+
+mod fs_utils;
+
+/// Register a Rust module as a Python submodule. The Rust module must provide a
+/// crate-public function with the same name as the module itself, that uses the
+/// #[pymodule] macro as described in the PyO3 docs
+/// (https://pyo3.rs/v0.16.4/module.html)
+macro_rules! submodule {
+    ($module:ident, $py:ident, $parent_module:ident) => {{
+        let child_module = PyModule::new($py, stringify!($module))?;
+        $module::$module($py, child_module)?;
+        $parent_module.add_submodule(child_module)?;
+
+        // Small hack to enable intuitive Python import resolution - for example
+        // `import antlir.rust.fs_utils` should work directly as expected,
+        // instead of requiring a user to `import antlir.rust` and then use
+        // `antlir.rust.fs_utils`.
+        // Small note: there is no extra initialization cost to doing this like
+        // there might be in pure-Python module imports, since all the
+        // initialization is done as part of importing the top level
+        // `antlir.rust` module in the first place, so this exists solely to
+        // make import mechanics work as expected
+        $py.import("sys")?
+            .getattr("modules")?
+            .set_item(concat!("antlir.rust.", stringify!($module)), child_module)?;
+        Ok::<_, pyo3::PyErr>(())
+    }};
+}
+
+#[pymodule]
+fn native(py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    submodule!(fs_utils, py, m)?;
+    Ok(())
+}
