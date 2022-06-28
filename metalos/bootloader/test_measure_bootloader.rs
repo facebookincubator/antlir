ccba1c3b559c1d6ac66642ccfc365d8ac444b9df
Adrian Enache <aeh@fb.com>
2022-06-28 06:21:58 -0700

    add UEFI event log parsing
    Summary:
    - add test_measure_bootloader::can_read_eventlog to get data from the UEFI
      event log in the booted system
    
    Reviewed By: dimkup
    
    Differential Revision: D37342981
    
    fbshipit-source-id: 903b1972215fd64fba37b3400b3f8416205de0f8

--- metalos/bootloader/test_measure_bootloader.rs
+++ metalos/bootloader/test_measure_bootloader.rs
@@ -11,6 +11,62 @@ use tss_esapi::{
     structures::{PcrSelectionListBuilder, PcrSlot},
     Context as TssContext, Tcti,
 };
+use uefi_eventlog::Parser as EventlogParser;
+
+mod eventlog {
+    use serde::Deserialize;
+    use std::collections::BTreeMap;
+    use uefi_eventlog::Event;
+
+    pub const UEFI_EVENTLOG_PATH: &str = "/sys/kernel/security/tpm0/binary_bios_measurements";
+
+    #[derive(PartialEq, Eq, PartialOrd, Ord, Copy, Clone, Deserialize, Debug)]
+    #[serde(rename_all = "lowercase")]
+    pub enum DigestAlgorithm {
+        Sha1,
+        Sha256,
+        Sha384,
+        Sha512,
+    }
+
+    fn deserialize_as_base64<'d, D>(deserialize: D) -> Result<Vec<u8>, D::Error>
+    where
+        D: serde::Deserializer<'d>,
+    {
+        let hexstr = String::deserialize(deserialize)?;
+        base64::decode(&hexstr).map_err(serde::de::Error::custom)
+    }
+
+    #[derive(Clone, Deserialize, Debug)]
+    pub struct Digest {
+        method: DigestAlgorithm,
+
+        #[allow(dead_code)]
+        #[serde(deserialize_with = "deserialize_as_base64")]
+        digest: Vec<u8>,
+    }
+
+    #[derive(derive_more::Index, Debug)]
+    pub struct DigestMap(BTreeMap<DigestAlgorithm, Digest>);
+
+    impl TryFrom<Event> for DigestMap {
+        type Error = serde_json::Error;
+
+        fn try_from(event: Event) -> Result<Self, Self::Error> {
+            // amazingly the uefi_eventlog::Event::digests does not present any interface
+            // to extract data and all fields are private; the only thing it derives is
+            // serde serialization; so use that
+            let json = serde_json::to_string(&event.digests)?;
+            let digests: Vec<Digest> = serde_json::from_str(&json)?;
+
+            let mut map = BTreeMap::new();
+            for dig in digests {
+                map.insert(dig.method, dig);
+            }
+            Ok(DigestMap(map))
+        }
+    }
+}
 
 #[test]
 fn can_use_tpm_device() {
@@ -30,4 +86,29 @@ fn can_use_tpm_device() {
         .expect("Call to pcr_read failed");
 
     debug!(log, "sha256:4 = {:?}", digests.value()[0]);
+}
+
+#[test]
+fn can_read_eventlog() {
+    use fallible_iterator::FallibleIterator;
+    let log = Logger::root(slog_glog_fmt::default_drain(), o!());
+
+    let evlog = std::fs::File::open(eventlog::UEFI_EVENTLOG_PATH)
+        .expect("failed to open UEFI event log file");
+    let mut parser = EventlogParser::new(evlog);
+
+    while let Some(event) = parser.next().expect("failed to get events") {
+        if event.pcr_index == 0 {
+            let evtype = event.event.clone();
+            let digests: eventlog::DigestMap =
+                event.try_into().expect("failed to convert event digest");
+
+            debug!(
+                log,
+                "event <{:?}> sha1:0 = {:?}",
+                evtype,
+                digests[&eventlog::DigestAlgorithm::Sha1]
+            );
+        }
+    }
 }
