f2884c2800fd28c492456849b90be15dc235a864
Leo DiPerna <dipernalz@fb.com>
2022-07-07 15:53:15 -0700

    Migrated image.genrule_layer to buck2 providers
    Summary: Replaced target tagger logic with buck2 provider logic.
    
    Differential Revision: D37670808
    
    fbshipit-source-id: 6faaa55173a75d823439e35e9fc82d4ddea1b226

---
+++ antlir/compiler/tests/buck2/test_image_layer.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import importlib.resources
+import json
+import subprocess
+import unittest
+from contextlib import contextmanager
+
+from antlir.config import antlir_dep
+from antlir.find_built_subvol import find_built_subvol
+from antlir.tests.layer_resource import layer_resource, LAYER_SLASH_ENCODE
+from antlir.tests.subvol_helpers import pop_path, render_subvol
+
+TARGET_RESOURCE_PREFIX = "test_layer_"
+TARGET_TO_PATH = {
+    target[len(TARGET_RESOURCE_PREFIX) :]: path
+    for target, path in [
+        (
+            target.replace(LAYER_SLASH_ENCODE, "/"),
+            str(layer_resource(__package__, target)),
+        )
+        for target in importlib.resources.contents(__package__)
+        if target.startswith(TARGET_RESOURCE_PREFIX)
+    ]
+}
+
+
+class ImageFeatureTestCase(unittest.TestCase):
+    def setUp(self):
+        # More output for easier debugging
+        unittest.util._MAX_LENGTH = 12345
+        self.maxDiff = 12345
+
+    @contextmanager
+    def target_subvol(self, target, mount_config=None):
+        with self.subTest(target):
+            # The mount configuration is very uniform, so we can check it here.
+            expected_config = {
+                "is_directory": True,
+                "build_source": {
+                    "type": "layer",
+                    "source": antlir_dep(
+                        "compiler/test_images/buck2/image_layer:" + target
+                    ),
+                },
+            }
+            if mount_config:
+                expected_config.update(mount_config)
+            with open(TARGET_TO_PATH[target] + "/mountconfig.json") as infile:
+                self.assertEqual(expected_config, json.load(infile))
+            yield find_built_subvol(TARGET_TO_PATH[target])
+
+    def test_genrule_layer(self):
+        with self.target_subvol("genrule-layer") as subvol:
+            subvol_path = subvol.path()
+            rendered_subvol = render_subvol(subvol)
+
+        genrule_dir = pop_path(rendered_subvol, "genrule_output")
+        self.assertIn("test_genrule_layer.txt", genrule_dir[1])
+
+        self.assertEquals(
+            subprocess.check_output(
+                [
+                    "cat",
+                    str(subvol_path) + "/genrule_output/test_genrule_layer.txt",
+                ]
+            ),
+            b"test_genrule_layer\n",
+        )
