6ef117620d186bdb9cb76baf80aa4880b5791e92
Natalie Jameson <nmj@fb.com>
2022-07-08 12:36:54 -0700

    Remove the buck2_docs project
    Summary: This has been wholly incorporated into buck2, so this project an be deleted. Fixed up some other teams' contbuilds too that used this tool.
    
    Reviewed By: ndmitchell, vmagro
    
    Differential Revision: D37696697
    
    fbshipit-source-id: 5d9372560963e64e4ed3e94f615bbc812fdbd027

--- antlir/website/bzldoc.js
+++ antlir/website/bzldoc.js
@@ -13,26 +13,16 @@ const {isInternal} = require('internaldocs-fb-helpers');
 const args = require('minimist')(process.argv.slice(2))
 
 const cp_cmd = ' && cp -r $(pwd)/docs/api/starlark/fbcode/antlir/* $(pwd)/docs/api';
-const internal_cmd = 'buck run fbcode//buck2/buck2_docs:buck2_docs --  --destination-dir=$(pwd)/docs/api ';
-const oss_cmd = 'buck run //generated/buck2/buck2_docs:buck2_docs --  --destination-dir=$(pwd)/docs/api '
+const cmd = 'buck2 docs starlark --format markdown_files --markdown-files-destination-dir=$(pwd)/docs/api ';
 
 // eslint-disable-next-line no-unused-vars
 let bzldoc = (files_to_generate) => {
-  if (isInternal()) {
     exec(
-      internal_cmd + files_to_generate + cp_cmd,
+      cmd + files_to_generate + cp_cmd,
       {
         shell: '/bin/bash',
       },
     );
-  } else {
-    exec(
-      oss_cmd + files_to_generate + cp_cmd,
-      {
-        shell: '/bin/bash',
-      },
-    );
-  }
 };
 
 bzldoc(args["files"]);
