07a8e3df01873926141034ca79c5670ff614e8f4
Leo DiPerna <dipernalz@fb.com>
2022-07-11 11:44:44 -0700

    Added get_layer_target_to_path helper method for tests
    Summary: Added a helper method for buck2 feature tests after noticing identical code in multiple locations.
    
    Differential Revision: D37722411
    
    fbshipit-source-id: 176b446f03c20a2e1b8a3818bf33ad9424aba0e9

--- antlir/compiler/tests/buck2/helpers.py
+++ antlir/compiler/tests/buck2/helpers.py
@@ -4,12 +4,30 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from antlir.tests.layer_resource import layer_resource, LAYER_SLASH_ENCODE
 
-def get_layer(resources, target_resource_prefix):
+
+def get_layer_by_prefix(resources, target_resource_prefix):
     for target in resources:
         if target.startswith(target_resource_prefix):
             return target[len(target_resource_prefix) :]
     raise RuntimeError("layer resource undefined")
+
+
+def get_layer_target_to_path_by_prefix(
+    resources, package, target_resource_prefix
+):
+    return {
+        target[len(target_resource_prefix) :]: path
+        for target, path in [
+            (
+                target.replace(LAYER_SLASH_ENCODE, "/"),
+                str(layer_resource(package, target)),
+            )
+            for target in resources
+            if target.startswith(target_resource_prefix)
+        ]
+    }
 
 
 def generate_group_str(
