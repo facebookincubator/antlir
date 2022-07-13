ec729c12a79e2f237f9cf10ca626a122340c3c47
Vinnie Magro <vmagro@fb.com>
2022-07-13 10:33:04 -0700

    use service name as template instance
    Summary: This is much easier to read as a human being looking at a metalos host
    
    Reviewed By: zeroxoneb
    
    Differential Revision: D37343921
    
    fbshipit-source-id: 7765a4ee99bc18b90bf59a656e21c726ba256522

--- metalos/lib/service/src/dropin.rs
+++ metalos/lib/service/src/dropin.rs
@@ -18,6 +18,7 @@ use serde::Deserialize;
 use serde::Serialize;
 use uuid::Uuid;
 
+use state::Alias;
 use state::State;
 use state::Token;
 use systemd::UnitName;
@@ -81,21 +82,9 @@ struct ServiceSection {
 #[serde(rename_all = "PascalCase")]
 pub(crate) struct Dropin {
     #[serde(skip)]
-    token: Token<ServiceInstance>,
+    alias: Alias<ServiceInstance>,
     unit: UnitSection,
     service: ServiceSection,
-}
-
-#[derive(Debug, Serialize, PartialEq, Eq)]
-#[serde(rename_all = "PascalCase")]
-pub(crate) struct ManagerDropin {
-    unit: ManagerUnitSection,
-}
-
-#[derive(Debug, Serialize, PartialEq, Eq)]
-#[serde(rename_all = "PascalCase")]
-pub(crate) struct ManagerUnitSection {
-    part_of: UnitName,
 }
 
 impl Dropin {
@@ -110,27 +99,16 @@ impl Dropin {
         let token = svc
             .save()
             .context("while saving ServiceInstance to the state store")?;
+        let alias = token
+            .alias(Alias::custom(svc.name.clone()))
+            .context("while writing alias for ServiceInstance")?;
         let manager_unit: UnitName = format!(
             "metalos-native-service@{}.service",
-            systemd::escape(token.to_string())
+            systemd::escape(svc.name())
         )
         .into();
-        // annoyingly, we also have to generate a dropin for the native service
-        // manager so that it will be restarted when the native service is
-        // restarted
-        let manager_dropin = ManagerDropin {
-            unit: ManagerUnitSection {
-                part_of: svc.unit_name().clone(),
-            },
-        };
-        let manager_dropin_dir: PathBuf = format!("/run/systemd/system/{}.d", manager_unit).into();
-        std::fs::create_dir_all(&manager_dropin_dir)
-            .context("while creating manager dropin dir")?;
-        let f = std::fs::File::create(manager_dropin_dir.join("99-native-service.conf"))
-            .context("while creating manager dropin file")?;
-        serde_systemd::to_writer(f, &manager_dropin).context("while writing manager dropin")?;
         Ok(Dropin {
-            token,
+            alias,
             unit: UnitSection {
                 after: manager_unit.clone(),
                 requires: manager_unit.clone(),
@@ -178,9 +156,9 @@ mod tests {
         pretty_assertions::assert_eq!(
             format!(
                 "[Unit]\n\
-                 After=metalos-native-service@{token}.service\n\
-                 Requires=metalos-native-service@{token}.service\n\
-                 PropagatesStopTo=metalos-native-service@{token}.service\n\
+                 After=metalos-native-service@{alias}.service\n\
+                 Requires=metalos-native-service@{alias}.service\n\
+                 PropagatesStopTo=metalos-native-service@{alias}.service\n\
                  Description={{\"native_service\":\"metalos.service.demo\",\"version\":\"00000000-0000-4000-8000-000000000001\"}}\n\
                  [Service]\n\
                  RootDirectory=/run/fs/control/run/service-roots/metalos.service.demo-00000000000040008000000000000001-{uuid}\n\
@@ -196,7 +174,7 @@ mod tests {
                  BindPaths=/run/fs/control/run/runtime/metalos.service.demo-00000000000040008000000000000001-{uuid}:/metalos/runtime\n\
                  BindPaths=/run/fs/control/run/state/metalos.service.demo:/metalos/state\n\
                  \n",
-                token = systemd::escape(di.token.to_string()),
+                alias = systemd::escape(di.alias.to_string()),
                 uuid = svc.run_uuid.to_simple(),
             ),
             serde_systemd::to_string(&di)?
