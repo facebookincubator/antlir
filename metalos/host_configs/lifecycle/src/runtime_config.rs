04797e680da9c645169ded816e383fb0384da128
Zeh Fernando <zeh@fb.com>
2022-08-10 13:29:03 -0700

    Cleanup unused imports
    Summary: Removes unused imports for linting. No behavior changes.
    
    Reviewed By: dtolnay
    
    Differential Revision: D38583392
    
    fbshipit-source-id: 15b96474229a8674ca70a47af0cb61292b79076e

--- metalos/host_configs/lifecycle/src/runtime_config.rs
+++ metalos/host_configs/lifecycle/src/runtime_config.rs
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-use anyhow::ensure;
 use metalos_host_configs::packages::generic::Package;
 use metalos_host_configs::runtime_config::RuntimeConfig;
 
