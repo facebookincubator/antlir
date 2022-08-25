68c77f2d43171aacd8a7e744151ce3895d6b2aa3
Surya Palavalasa <sunteja@fb.com>
2022-08-24 18:17:47 -0700

    Repo server changes
    Reviewed By: vmagro
    
    Differential Revision: D38854667
    
    fbshipit-source-id: 3689fc7b39eb3485c2c3a712a94882ce5b868f39

--- antlir/debian/proxy.rs
+++ antlir/debian/proxy.rs
@@ -11,6 +11,7 @@ use std::os::unix::io::FromRawFd;
 use std::os::unix::io::RawFd;
 use std::sync::Arc;
 
+use anyhow::anyhow;
 use anyhow::Error;
 use anyhow::Result;
 use blob_store::PackageBackend;
@@ -19,8 +20,11 @@ use fbinit::FacebookInit;
 use log::info;
 use manifold_client::cpp_client::ClientOptionsBuilder;
 use manifold_client::cpp_client::ManifoldCppClient;
+use snapshotter_helpers::ANTLIR_SNAPSHOTS_BUCKET;
+use snapshotter_helpers::API_KEY;
 use tokio::net::TcpListener as TokioTcpListener;
 use tokio_stream::wrappers::TcpListenerStream;
+use urlencoding::decode;
 use warp::reject::Reject;
 use warp::Filter;
 
@@ -39,9 +43,27 @@ async fn serve_blob(
     hash: String,
     client: Arc<dyn PackageBackend>,
 ) -> std::result::Result<Vec<u8>, warp::reject::Rejection> {
-    match client.get(&hash).await {
-        Ok(res) => Ok(res.to_vec()),
-        Err(e) => Err(warp::reject::custom(ProxyError(e))),
+    let d_hash = decode(&hash);
+    match d_hash {
+        Ok(value) => match client.get(&value).await {
+            Ok(res) => Ok(res.to_vec()),
+            Err(e) => Err(warp::reject::custom(ProxyError(e))),
+        },
+        Err(e) => Err(warp::reject::custom(ProxyError(anyhow!(e)))),
+    }
+}
+
+async fn serve_repo_artifact(
+    hash_file_name: String,
+    client: Arc<dyn PackageBackend>,
+) -> std::result::Result<Vec<u8>, warp::reject::Rejection> {
+    let hash_name = hash_file_name.split_once('-');
+    match hash_name {
+        Some((hash, _name)) => serve_blob(hash.to_string(), client.clone()).await,
+        None => Err(warp::reject::custom(ProxyError(anyhow!(
+            "{} must match the format '$hash-$name'",
+            hash_file_name
+        )))),
     }
 }
 
@@ -58,15 +80,35 @@ async fn serve(client: Arc<dyn PackageBackend>, socket_fd: RawFd) -> Result<()> 
         serve_blob(format!("{}:{}", hash_key, hash), cl2.clone())
     });
 
+    let cl2 = client.clone();
     let deb_file =
         warp::path!("deb" / String).and_then(move |hash: String| serve_blob(hash, client.clone()));
     let log = warp::log("apt::proxy");
 
-    let routes = release_file.or(package_file).or(deb_file).with(log);
+    let client = cl2.clone();
+    let repomd_xml = warp::path!("dists" / String / "repodata" / "repomd.xml")
+        .and_then(move |hash: String| serve_blob(hash, cl2.clone()));
+
+    let cl2 = client.clone();
+    let repo_artifacts = warp::path!("dists" / String / "repodata" / String).and_then(
+        move |_repo_hash, artifact_hash_file: String| {
+            serve_repo_artifact(artifact_hash_file, client.clone())
+        },
+    );
+
+    let rpms = warp::path!("dists" / String / "rpm" / String)
+        .and_then(move |_repo_hash, rpm_hash| serve_blob(rpm_hash, cl2.clone()));
+    let routes = release_file
+        .or(package_file)
+        .or(deb_file)
+        .or(repomd_xml)
+        .or(repo_artifacts)
+        .or(rpms)
+        .with(log);
 
     info!(
         "\n\tI don't know who you are,\t
-        I don't know the deb you are looking for,\t
+        I don't know the deb or rpm you are looking for,\t
         but i have hashes, a very particular set of hashes,\t
         hashes that I've acquired with snapshotter,\t
         I will find it and serve it",
@@ -75,12 +117,9 @@ async fn serve(client: Arc<dyn PackageBackend>, socket_fd: RawFd) -> Result<()> 
     tcp_listener.set_nonblocking(true)?;
     let tokio_tcp_listner = TokioTcpListener::from_std(tcp_listener)?;
     let incoming = TcpListenerStream::new(tokio_tcp_listner);
-
     warp::serve(routes).serve_incoming(incoming).await;
     Ok(())
 }
-const API_KEY: &str = "antlir_snapshots-key";
-const ANTLIR_DEB_BUCKET: &str = "antlir_snapshots";
 
 #[fbinit::main]
 async fn main(fb: FacebookInit) -> Result<()> {
@@ -91,7 +130,7 @@ async fn main(fb: FacebookInit) -> Result<()> {
         .build()
         .map_err(Error::msg)?;
     let manifold_client =
-        ManifoldCppClient::from_options(fb, ANTLIR_DEB_BUCKET, &manifold_client_opts)
+        ManifoldCppClient::from_options(fb, ANTLIR_SNAPSHOTS_BUCKET, &manifold_client_opts)
             .map_err(Error::from)?;
     serve(Arc::new(manifold_client), args.socket_fd).await
 }
