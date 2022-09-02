e1c65f55c3b9b3f842568b417570577779d5e19b
Rohit Singh <rh0@fb.com>
2022-09-02 10:39:38 -0700

    btrfs send stream upgrade v0.7: Finish up prefetcher
    Summary: This change finishes up the prefetch worker
    
    Reviewed By: boryas
    
    Differential Revision: D38616190
    
    fbshipit-source-id: 59aaa1bda769a4d704181e070ae8431a7379a03d

--- antlir/btrfs_send_stream_upgrade/src/mp/threads/prefetch_worker.rs
+++ antlir/btrfs_send_stream_upgrade/src/mp/threads/prefetch_worker.rs
@@ -5,6 +5,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+use anyhow::Context;
+
 use crate::mp::threads::worker::Worker;
 use crate::upgrade::send_stream_upgrade_context::SendStreamUpgradeContext;
 
@@ -21,7 +23,26 @@ impl Worker for PrefetchWorker {
         // Only the writer can get the destination
         false
     }
-    fn run_worker(_context: SendStreamUpgradeContext) -> anyhow::Result<()> {
+    fn run_worker(context: SendStreamUpgradeContext) -> anyhow::Result<()> {
+        let mut context = context;
+        // Detatch the container from the buffer cache
+        let sync_container = context
+            .ssuc_sync_container
+            .as_mut()
+            .context("Prefetching with None container")?;
+        let buffer_cache = sync_container
+            .take_buffer_cache()
+            .context("Prefetching with None buffer cache")?;
+
+        // Run the prefetch loop
+        (*buffer_cache).prefetcher_loop(&mut context)?;
+
+        // On our way out, tally up the stats
+        context
+            .ssuc_sync_container
+            .as_ref()
+            .context("Prefetching with None container")?
+            .rollover_stats(&context.ssuc_stats)?;
         Ok(())
     }
 }
