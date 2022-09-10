88a076dceda70a3d7650cc6e6b666e28b5cbec13
Rohit Singh <rh0@fb.com>
2022-09-10 10:53:32 -0700

    btrfs send stream upgrade v0.7: Finish up batcher
    Summary: This change finishes up the batcher worker
    
    Reviewed By: boryas
    
    Differential Revision: D38616198
    
    fbshipit-source-id: e49909bd7b317ec999a635a650dbaa62c3ec3c71

--- antlir/btrfs_send_stream_upgrade/src/mp/send_elements/command_batch_info.rs
+++ antlir/btrfs_send_stream_upgrade/src/mp/send_elements/command_batch_info.rs
@@ -10,6 +10,7 @@ use std::fmt::Display;
 use crate::mp::sync::ordered_element::OrderedElement;
 use crate::mp::sync::unordered_element::UnorderedElement;
 use crate::send_elements::send_command::SendCommand;
+use crate::upgrade::send_stream_upgrade_context::SendStreamUpgradeContext;
 
 pub struct CommandBatchInfo {
     /// The first id associated with the batch
@@ -18,17 +19,46 @@ pub struct CommandBatchInfo {
     cbi_last_id: u64,
     /// A list of commands to be batched together
     cbi_command_queue: Vec<SendCommand>,
+    /// A running tally of the data payload
+    cbi_data_payload_size: usize,
 }
 
 impl CommandBatchInfo {
     pub fn new(id: u64, send_command: SendCommand) -> anyhow::Result<Self> {
+        let data_payload_size = send_command.get_cached_data_payload_size()?;
         Ok(Self {
             cbi_first_id: id,
             cbi_last_id: id,
             cbi_command_queue: vec![send_command],
+            cbi_data_payload_size: data_payload_size,
         })
     }
-    // To add: More methods like can append, append, flatten
+    pub fn can_append(
+        &self,
+        other: &Self,
+        context: &SendStreamUpgradeContext,
+    ) -> anyhow::Result<bool> {
+        let length = self.cbi_command_queue.len();
+        let last_command = &self.cbi_command_queue[length - 1];
+        let other_command = &other.cbi_command_queue[0];
+        Ok(last_command.can_append(other_command)
+            && (self.cbi_data_payload_size + other.cbi_data_payload_size)
+                <= context.ssuc_options.maximum_batched_extent_size
+            && (self.cbi_last_id + 1) == other.cbi_first_id)
+    }
+    pub fn append(&mut self, other: Self) {
+        let mut other = other;
+        self.cbi_command_queue
+            .push(other.cbi_command_queue.remove(0));
+        self.cbi_last_id += 1;
+        self.cbi_data_payload_size += other.cbi_data_payload_size;
+    }
+    pub fn is_end(&self) -> bool {
+        self.cbi_command_queue[0].is_end()
+    }
+    pub fn is_appendable(&self) -> bool {
+        self.cbi_command_queue[0].is_appendable()
+    }
 }
 
 impl UnorderedElement for CommandBatchInfo {}
