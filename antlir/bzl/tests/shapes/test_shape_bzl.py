4e91854ecc0c80eea07d637591bd99450e5e4a67
Vinnie Magro <vmagro@meta.com>
2022-09-28 13:52:14 -0700

    move test from selftest to unit test
    Summary: Selftests are super annoying
    
    Reviewed By: naveedgol
    
    Differential Revision: D39831929
    
    fbshipit-source-id: 9b82ef40fc9607f9ac5cbe8b64a0d5444e9c5b87

--- antlir/bzl/tests/shapes/test_shape_bzl.py
+++ antlir/bzl/tests/shapes/test_shape_bzl.py
@@ -497,3 +497,14 @@ class TestShapeBzl(unittest.TestCase):
             instance_json,
             '{"items":["two","one"],"v1":null,"v2":123,"v3":false}',
         )
+
+    def test_hash(self) -> None:
+        a = shape.shape(x=int)
+        b = shape.shape(
+            y=shape.dict(str, shape.list(int)),
+            z=a,
+        )
+
+        test_1 = shape.hash(b(y={"c": [6, 5, 4], "d": [1, 3, 2]}, z=a(x=5)))
+        test_2 = shape.hash(b(y={"c": [5, 6, 4], "d": [3, 1, 2]}, z=a(x=5)))
+        self.assertEqual(test_1, test_2)
