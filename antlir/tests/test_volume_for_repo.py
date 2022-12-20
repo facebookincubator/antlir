b5d296a551cad1d2edbe20408d72d323b6403989
Vinnie Magro <vmagro@meta.com>
2022-12-20 13:36:17 -0800

    [antlir][volume] refactor volume checks and remediations, exhaustive tests
    Summary:
    D41745332 (https://github.com/facebookincubator/antlir/commit/854b0865a386ec3dee6efff24fc704e3844ce4d4) has been on master for a week. Refactor the fallback code to be less
    "automagical" and be more liberal with asking for manual intervention.
    
    Note that the side effect of this is that some user interaction may be required
    to bring their build environment forward, but it's better than obtuse
    `subprocess.CalledProcessError`s being raised that have very little context.
    Ultimately there are lots of things that can go wrong trying to migrate from
    loopback->subvolume, but an engineer will be very capable of quickly fixing it.
    
    This diff also adds much more exhaustive testing for this code, to explicitly
    exercise all of the ostensibly-handled error conditions, instead of sprinkling
    `# pragma: no cover` on except blocks
    
    Test Plan:
    ```
    ❯ buck2 test fbcode//antlir:test-volume-for-repo
    Buck UI: https://www.internalfb.com/buck2/6385a205-f94b-4028-b81c-b57652349e7b
    Test UI: https://www.internalfb.com/intern/testinfra/testrun/3377699863839130
    RE: reSessionID-97ee84da-965e-4ec8-8a9e-c0697402dd86  Up: 434 B  Down: 0 B
    Jobs completed: 8. Time elapsed: 5.8s.
    Tests finished: Pass 6. Fail 0. Fatal 0. Skip 0. 0 builds failed
    ```
    
    Reviewed By: snarkmaster
    
    Differential Revision: D42147692
    
    fbshipit-source-id: ea64eb7f88f83c234641afb390a2da8bb0a2175d

--- antlir/tests/test_volume_for_repo.py
+++ antlir/tests/test_volume_for_repo.py
@@ -8,45 +8,48 @@ import os
 import shutil
 import subprocess
 import tempfile
+import time
 import unittest
 
 from antlir import btrfsutil, volume_for_repo as vfr
+from antlir.artifacts_dir import find_repo_root
+
+from antlir.errors import InfraError
 
 from antlir.fs_utils import Path
 
 
 class VolumeForRepoTestCase(unittest.TestCase):
-    def test_volume_repo(self) -> None:
-        artifacts_dir = Path(tempfile.mkdtemp(prefix="test_volume_repo"))
-        volume_dir = artifacts_dir / vfr.VOLUME_DIR
-
-        try:
-            self.assertEqual(
-                vfr.get_volume_for_current_repo(
-                    artifacts_dir=artifacts_dir,
-                ),
-                volume_dir,
-            )
-
-            self.assertTrue(btrfsutil.is_subvolume(volume_dir))
-        finally:
-            try:
-                btrfsutil.delete_subvolume(volume_dir, recursive=True)
-            except Exception:
-                pass  # Might not have been created in case of an earlier error
-            shutil.rmtree(artifacts_dir)
-
-    def test_upgrade(self):
+    def _scratch_subvolume(self) -> str:
         """
-        Can upgrade from loopback to on-host subvol
+        Crate a scratch subvolume on a BTRFS filesystem where we can create
+        pathological test cases for volume_for_repo.py. We can't use existing
+        Antlir mechanisms like TempSubvolumes, because volume_for_repo is the
+        thing that sets up the environment for those to work.
         """
-        artifacts_dir = Path(tempfile.mkdtemp(prefix="test_upgrade_"))
-        volume_dir = artifacts_dir / vfr.VOLUME_DIR
-        artifacts_dir_src = Path(tempfile.mkdtemp(prefix="test_upgrade_src_"))
-        os.makedirs(volume_dir)
-        subprocess.run(
-            ["sudo", "mount", "--bind", artifacts_dir_src, volume_dir], check=True
+        repo_root = find_repo_root()
+        # hopefully the parent of the repo is on a btrfs filesystem
+        hopefully_btrfs = repo_root.dirname()
+        scratch_path = (
+            hopefully_btrfs / f"test_volume_for_repo_{self.id()}_{time.time()}"
         )
+        self.addCleanup(
+            lambda scratch_path: btrfsutil.delete_subvolume(
+                scratch_path, recursive=True
+            ),
+            scratch_path,
+        )
+        btrfsutil.create_subvolume(scratch_path)
+        subprocess.run(
+            ["sudo", "chown", f"{os.getuid()}:{os.getgid()}", scratch_path], check=True
+        )
+        return str(scratch_path)
+
+    def test_volume_repo(self) -> None:
+        artifacts_dir = Path(
+            tempfile.mkdtemp(prefix=self.id(), dir=self._scratch_subvolume())
+        )
+        volume_dir = artifacts_dir / vfr.VOLUME_DIR
 
         try:
             self.assertEqual(
@@ -55,6 +58,7 @@ class VolumeForRepoTestCase(unittest.TestCase):
                 ),
                 volume_dir,
             )
+
             self.assertTrue(btrfsutil.is_subvolume(volume_dir))
         finally:
             try:
@@ -62,7 +66,96 @@ class VolumeForRepoTestCase(unittest.TestCase):
             except Exception:
                 pass  # Might not have been created in case of an earlier error
             shutil.rmtree(artifacts_dir)
-            shutil.rmtree(artifacts_dir_src)
+
+    def test_exists_but_not_subvolume_and_not_empty(self):
+        """
+        Existing volume dir that is not a subvolume but is not empty should ask
+        the user to delete the contents themselves just in case.
+        """
+        artifacts_dir = Path(tempfile.mkdtemp(prefix=self.id()))
+        volume_dir = artifacts_dir / vfr.VOLUME_DIR
+        os.mkdir(volume_dir)
+        (volume_dir / "file").touch()
+        with self.assertRaisesRegex(InfraError, "is not a subvolume"):
+            vfr.get_volume_for_current_repo(artifacts_dir=artifacts_dir)
+
+    def test_is_subvolid5(self):
+        """
+        Something that is already btrfs but is subvolid=5, indicating that it's
+        a loopback. When not in use, this will cleanly unmount.
+        """
+        artifacts_dir = Path(
+            tempfile.mkdtemp(prefix=self.id(), dir=self._scratch_subvolume())
+        )
+        volume_dir = artifacts_dir / vfr.VOLUME_DIR
+        os.mkdir(volume_dir)
+        with tempfile.NamedTemporaryFile() as loopback:
+            loopback.truncate(500 * 1024 * 1024)
+            subprocess.run(["mkfs.btrfs", loopback.name], check=True)
+            subprocess.run(["sudo", "mount", loopback.name, volume_dir], check=True)
+            self.assertEqual(btrfsutil.subvolume_id(volume_dir), 5)
+            self.assertEqual(
+                vfr.get_volume_for_current_repo(
+                    artifacts_dir=artifacts_dir,
+                ),
+                volume_dir,
+            )
+            self.assertTrue(btrfsutil.is_subvolume(volume_dir))
+            self.assertNotEqual(btrfsutil.subvolume_id(volume_dir), 5)
+
+    def test_subvolid5_with_contents_underneath(self):
+        """
+        Something that is already btrfs but is subvolid=5, indicating that it's
+        a loopback. When not in use, this will cleanly unmount. However, if
+        there are contents that were being shadowed by the mount, we ask the
+        user to remove them.
+        """
+        artifacts_dir = Path(
+            tempfile.mkdtemp(prefix=self.id(), dir=self._scratch_subvolume())
+        )
+        volume_dir = artifacts_dir / vfr.VOLUME_DIR
+        os.mkdir(volume_dir)
+        (volume_dir / "file").touch()
+        with tempfile.NamedTemporaryFile() as loopback:
+            loopback.truncate(500 * 1024 * 1024)
+            subprocess.run(["mkfs.btrfs", loopback.name], check=True)
+            subprocess.run(["sudo", "mount", loopback.name, volume_dir], check=True)
+            self.assertEqual(btrfsutil.subvolume_id(volume_dir), 5)
+            with self.assertRaisesRegex(InfraError, "could not be removed"):
+                vfr.get_volume_for_current_repo(
+                    artifacts_dir=artifacts_dir,
+                )
+
+    def test_is_subvolid5_and_in_use(self):
+        """
+        Something that is already btrfs but is subvolid=5, indicating that it's
+        a loopback. We will try to clean this up automatically, but require
+        manual intervention if it could not be unmounted.
+        """
+        artifacts_dir = Path(tempfile.mkdtemp(prefix=self.id()))
+        volume_dir = artifacts_dir / vfr.VOLUME_DIR
+        os.mkdir(volume_dir)
+        with tempfile.NamedTemporaryFile() as loopback:
+            loopback.truncate(500 * 1024 * 1024)
+            subprocess.run(["mkfs.btrfs", loopback.name], check=True)
+            subprocess.run(["sudo", "mount", loopback.name, volume_dir], check=True)
+            subprocess.run(
+                [
+                    "sudo",
+                    "chown",
+                    f"{os.getuid()}:{os.geteuid()}",
+                    loopback.name,
+                    volume_dir,
+                ],
+                check=True,
+            )
+            with open(volume_dir / "force-in-use", "w") as f:
+                f.write("foo")
+                with self.assertRaisesRegex(
+                    InfraError, "appears to be a mounted btrfs"
+                ):
+                    vfr.get_volume_for_current_repo(artifacts_dir=artifacts_dir)
+            subprocess.run(["sudo", "umount", volume_dir])
 
 
 if __name__ == "__main__":
