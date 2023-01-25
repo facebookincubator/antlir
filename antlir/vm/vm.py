3070775b5de29dc79522773610e2e23a191c5ef6
Serge Dubrouski <dubros@meta.com>
2023-01-25 14:20:40 -0800

    [antlir/metalos] Prevent network interface renaming in vmtests
    Summary:
    User reported an issue with "eth0" missing in vmtest after we enabled network access in initrd with D42281354. Discussion is here: https://fb.workplace.com/groups/2569684070020639/permalink/3496655543990149/
    
    After some debugging I found that `eth0` gets renamed in initrd stage. Not always, but basically 3 times out of 5.
    
    ```
    [    4.225255] virtio_net virtio2 enp0s5: renamed from eth0
    ```
    
    This diff should prevent that.
    
    Test Plan:
    ```
    buck2 test //ti/shiv_mk2/testutil/tests:local_test-5.6.13-0_fbk21_6114_gcde3142b1765 -- --run-disabled --stress-runs 5
    ```
    
    Reviewed By: vmagro
    
    Differential Revision: D42749069
    
    fbshipit-source-id: 06e7ccc1265d207d3bde115726f83cb7922dd35d

--- antlir/vm/vm.py
+++ antlir/vm/vm.py
@@ -436,6 +436,7 @@ async def vm(
                     " audit=0"
                     " selinux=0"
                     " systemd.hostname=vmtest "
+                    " net.ifnames=0 "
                     f" macaddress={tapdev.guest_mac} "
                     f" root=LABEL={opts.root_label} "
                     + " ".join(root_disk.kernel_args)
