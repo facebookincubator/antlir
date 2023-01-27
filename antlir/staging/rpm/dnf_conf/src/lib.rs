fdda1fd98aa58fc4e73d83facd5a639535ef5f2f
Vinnie Magro <vmagro@meta.com>
2023-01-27 14:49:24 -0800

    [antlir][staging][repo-server] generate dnf.conf
    Summary:
    Endpoint for the repo-server to provide a dnf.conf that includes all the
    repositories that the repo-server knows about.
    The repo proxy knows about all the repos, and it can generate a simple dnf.conf
    using the URL from the full client as the base, so it does not need to be aware
    of how it's going to be proxied into the build container.
    
    Test Plan:
    ```
    ❯ buck run 'fbcode//antlir/staging/rpm/dnf2buck/demo:test-repo[serve]' -- --bind /tmp/antlir2/sock
    
    ❯ curl -v --unix-socket /tmp/antlir2/sock http://localhost:9266/yum/dnf.conf
    [fb-runtime]
    baseurl=http://localhost:9266/yum/fb-runtime
    
    ❯ socat TCP6-LISTEN:9866,fork UNIX-CONNECT:/tmp/antlir2/sock &
    ❯ curl http://localhost:9866/yum/dnf.conf
    [fb-runtime]
    baseurl=http://localhost:9866/yum/fb-runtime
    ```
    
    Reviewed By: zeroxoneb
    
    Differential Revision: D42537708
    
    fbshipit-source-id: 3afc8cf335cae5b048b4abd8111d2c66d1ba8ed4

---
+++ antlir/staging/rpm/dnf_conf/src/lib.rs
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+use std::collections::HashMap;
+use std::fmt::Display;
+
+use configparser::ini::Ini;
+use itertools::Itertools;
+use url::Url;
+
+#[derive(Debug, Clone, Default)]
+pub struct DnfConf {
+    repos: HashMap<String, RepoConf>,
+}
+
+impl DnfConf {
+    pub fn builder() -> DnfConfBuilder {
+        DnfConfBuilder::default()
+    }
+
+    pub fn new() -> Self {
+        Self {
+            repos: HashMap::new(),
+        }
+    }
+
+    pub fn add_repo(&mut self, id: String, repo_cfg: RepoConf) {
+        self.repos.insert(id, repo_cfg);
+    }
+}
+
+impl Display for DnfConf {
+    #[deny(unused_variables)]
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let Self { repos } = self;
+        let mut config = Ini::new();
+        for (id, repo) in repos {
+            config.set(
+                id,
+                "baseurl",
+                Some(
+                    repo.base_urls
+                        .iter()
+                        .map(|u| u.to_string())
+                        .join("\n        "),
+                ),
+            );
+            if let Some(name) = &repo.name {
+                config.set(id, "name", Some(name.clone()));
+            }
+        }
+        write!(f, "{}", config.writes())
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct DnfConfBuilder(DnfConf);
+
+impl DnfConfBuilder {
+    pub fn add_repo(&mut self, id: String, cfg: impl Into<RepoConf>) -> &mut Self {
+        self.0.add_repo(id, cfg.into());
+        self
+    }
+
+    pub fn build(&self) -> DnfConf {
+        self.0.clone()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct RepoConf {
+    base_urls: Vec<Url>,
+    name: Option<String>,
+}
+
+impl From<Vec<Url>> for RepoConf {
+    fn from(urls: Vec<Url>) -> Self {
+        Self {
+            base_urls: urls,
+            name: None,
+        }
+    }
+}
+
+impl From<Url> for RepoConf {
+    fn from(u: Url) -> Self {
+        vec![u].into()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn builder() {
+        let dnf_conf = DnfConf::builder()
+            .add_repo(
+                "foo".into(),
+                vec![
+                    Url::parse("https://repo.repo/yum/my/repo").expect("valid url"),
+                    Url::parse("https://mirror.repo/yum/my/repo").expect("valid url"),
+                ],
+            )
+            .build();
+        assert_eq!(
+            dnf_conf.to_string(),
+            r#"[foo]
+baseurl=https://repo.repo/yum/my/repo
+        https://mirror.repo/yum/my/repo
+"#
+        );
+    }
+}
