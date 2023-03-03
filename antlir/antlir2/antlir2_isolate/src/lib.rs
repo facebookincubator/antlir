28ca5c05424de37c87217359cd76a2bed0a23401
Vinnie Magro <vmagro@meta.com>
2023-03-02 16:04:30 -0800

    [antlir2][antlir2_isolate] refactor into Builder
    Summary:
    This will primarily make it easier for the cases where a caller needs to
    customize the destination path of a bind mount, but it also just makes it
    easier to call in general
    
    Test Plan:
    ```
    ❯ buck2 test fbcode//antlir/antlir2/...
    Buck UI: https://www.internalfb.com/buck2/fbcc0b4e-7ba3-4d13-a4a2-c579a4826244
    Test UI: https://www.internalfb.com/intern/testinfra/testrun/2251799971658365
    RE: reSessionID-e72f4ed6-75b3-423a-b5b4-3a30d675fed9  Up: 14 MiB  Down: 5.7 MiB
    Jobs completed: 325. Time elapsed: 137.0s. Cache hits: 1%. Commands: 99 (cached: 1, remote: 1, local: 97)
    Tests finished: Pass 56. Fail 0. Fatal 0. Skip 0. 0 builds failed
    ```
    
    Reviewed By: zeroxoneb
    
    Differential Revision: D43739502
    
    fbshipit-source-id: 614e19d4139a7672869a59b14b53e99e6c18ac81

--- antlir/antlir2/antlir2_isolate/src/lib.rs
+++ antlir/antlir2/antlir2_isolate/src/lib.rs
@@ -16,8 +16,11 @@
 use std::borrow::Cow;
 use std::collections::BTreeMap;
 use std::collections::BTreeSet;
+use std::collections::HashMap;
 use std::ffi::OsStr;
+use std::ffi::OsString;
 use std::path::Path;
+use std::path::PathBuf;
 use std::process::Command;
 
 mod sys;
@@ -25,40 +28,155 @@ mod sys;
 /// Everything needed to know how to isolate an image compilation.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct IsolationContext<'a> {
-    /// A pre-built image (or the host rootfs when bootstrapping a completely
-    /// new one from scratch) that contains tools necessary to run the isolated
-    /// binary.
+    layer: Cow<'a, Path>,
+    /// See [IsolationContextBuilder::platform]
+    platform: BTreeMap<Cow<'a, Path>, Cow<'a, Path>>,
+    /// Directory in which to invoke the provided command.
+    working_directory: Option<Cow<'a, Path>>,
+    /// See [IsolationContextBuilder::setenv]
+    setenv: BTreeMap<Cow<'a, OsStr>, Cow<'a, OsStr>>,
+    /// See [IsolationContextBuilder::inputs]
+    inputs: BTreeMap<Cow<'a, Path>, Cow<'a, Path>>,
+    /// See [IsolationContextBuilder::outputs]
+    outputs: BTreeMap<Cow<'a, Path>, Cow<'a, Path>>,
+}
+
+impl<'a> IsolationContext<'a> {
+    /// Start making an [IsolationContext] with a pre-built image (or the host
+    /// rootfs when bootstrapping a completely new one from scratch) that
+    /// contains tools necessary to run the isolated binary.
     ///
     /// On Linux, it must be a pre-mounted directory that contains a full OS
     /// tree that `systemd-nspawn(1)` will accept.
-    pub layer: &'a Path,
+    pub fn builder<P: Into<Cow<'a, Path>>>(layer: P) -> IsolationContextBuilder<'a> {
+        IsolationContextBuilder {
+            ctx: Self {
+                layer: layer.into(),
+                platform: Default::default(),
+                working_directory: None,
+                setenv: Default::default(),
+                inputs: Default::default(),
+                outputs: Default::default(),
+            },
+        }
+    }
+
+    /// Build an [IsolationContext] using _only_ a layer. This is almost never useful
+    pub fn new<P: Into<Cow<'a, Path>>>(layer: P) -> Self {
+        Self::builder(layer).build()
+    }
+}
+
+/// Build an [IsolationContext] more easily than passing every struct field
+#[derive(Debug, Clone)]
+pub struct IsolationContextBuilder<'a> {
+    ctx: IsolationContext<'a>,
+}
+
+impl<'a> IsolationContextBuilder<'a> {
     /// Set of paths to share into the isolated environment so that the isolated
     /// binary has files that it depends on (for example, .so libraries linked
     /// into the binary). This also should include the isolated binary itself!
-    pub platform: BTreeSet<&'a Path>,
-    /// Directory in which to invoke the provided command.
-    pub working_directory: Option<&'a Path>,
-    /// Set environment variables within the isolated environment.
-    pub setenv: BTreeMap<&'a str, Cow<'a, OsStr>>,
+    pub fn platform<P: IntoBinds<'a>>(&mut self, paths: P) -> &mut Self {
+        self.ctx.platform.extend(paths.into_binds());
+        self
+    }
+
     /// Set of paths to share into the isolated environment that the compilation
     /// will need to do the actual image build (for example, directories with
     /// files that are being copied into the image).
-    pub image_sources: BTreeSet<&'a Path>,
+    pub fn inputs<P: IntoBinds<'a>>(&mut self, paths: P) -> &mut Self {
+        self.ctx.inputs.extend(paths.into_binds());
+        self
+    }
+
     /// Set of paths that should be writable from within the isolated
     /// environment (for functions that require writing output files).
-    pub writable_outputs: BTreeSet<&'a Path>,
+    pub fn outputs<P: IntoBinds<'a>>(&mut self, paths: P) -> &mut Self {
+        self.ctx.outputs.extend(paths.into_binds());
+        self
+    }
+
+    /// Set environment variables within the isolated environment.
+    pub fn setenv<E: IntoEnv<'a>>(&mut self, env: E) -> &mut Self {
+        self.ctx.setenv.extend(env.into_env());
+        self
+    }
+
+    /// Directory in which to invoke the provided command.
+    pub fn working_directory<P: Into<Cow<'a, Path>>>(&mut self, path: P) -> &mut Self {
+        self.ctx.working_directory = Some(path.into());
+        self
+    }
+
+    /// Finalize the IsolationContext
+    pub fn build(&mut self) -> IsolationContext<'a> {
+        self.ctx.clone()
+    }
 }
 
-impl Default for IsolationContext<'static> {
-    fn default() -> Self {
-        Self {
-            layer: Path::new("/"),
-            platform: Default::default(),
-            working_directory: None,
-            setenv: Default::default(),
-            image_sources: Default::default(),
-            writable_outputs: Default::default(),
-        }
+/// Anything that can be turned into a set of bind mounts
+pub trait IntoBinds<'a> {
+    /// Map dst->src
+    fn into_binds(self) -> HashMap<Cow<'a, Path>, Cow<'a, Path>>;
+}
+
+impl<'a> IntoBinds<'a> for &'a Path {
+    fn into_binds(self) -> HashMap<Cow<'a, Path>, Cow<'a, Path>> {
+        HashMap::from([(Cow::Borrowed(self), Cow::Borrowed(self))])
+    }
+}
+
+impl<'a> IntoBinds<'a> for PathBuf {
+    fn into_binds(self) -> HashMap<Cow<'a, Path>, Cow<'a, Path>> {
+        HashMap::from([(Cow::Owned(self.clone()), Cow::Owned(self))])
+    }
+}
+
+impl<'a> IntoBinds<'a> for (&'a Path, &'a Path) {
+    fn into_binds(self) -> HashMap<Cow<'a, Path>, Cow<'a, Path>> {
+        HashMap::from([(Cow::Borrowed(self.0), Cow::Borrowed(self.1))])
+    }
+}
+
+impl<'a> IntoBinds<'a> for &[&'a Path] {
+    fn into_binds(self) -> HashMap<Cow<'a, Path>, Cow<'a, Path>> {
+        self.iter()
+            .map(|path| (Cow::Borrowed(*path), Cow::Borrowed(*path)))
+            .collect()
+    }
+}
+
+impl<'a, const N: usize> IntoBinds<'a> for [&'a Path; N] {
+    fn into_binds(self) -> HashMap<Cow<'a, Path>, Cow<'a, Path>> {
+        self.into_iter()
+            .map(|path| (Cow::Borrowed(path), Cow::Borrowed(path)))
+            .collect()
+    }
+}
+
+impl<'a> IntoBinds<'a> for BTreeSet<&'a Path> {
+    fn into_binds(self) -> HashMap<Cow<'a, Path>, Cow<'a, Path>> {
+        self.into_iter()
+            .map(|path| (Cow::Borrowed(path), Cow::Borrowed(path)))
+            .collect()
+    }
+}
+
+/// Anything that can be turned into a set of env variables
+pub trait IntoEnv<'a> {
+    fn into_env(self) -> HashMap<Cow<'a, OsStr>, Cow<'a, OsStr>>;
+}
+
+impl<'a> IntoEnv<'a> for (&'a OsStr, &'a OsStr) {
+    fn into_env(self) -> HashMap<Cow<'a, OsStr>, Cow<'a, OsStr>> {
+        HashMap::from([(Cow::Borrowed(self.0), Cow::Borrowed(self.1))])
+    }
+}
+
+impl<'a> IntoEnv<'a> for (&'a str, OsString) {
+    fn into_env(self) -> HashMap<Cow<'a, OsStr>, Cow<'a, OsStr>> {
+        HashMap::from([(Cow::Borrowed(OsStr::new(self.0)), Cow::Owned(self.1))])
     }
 }
 
