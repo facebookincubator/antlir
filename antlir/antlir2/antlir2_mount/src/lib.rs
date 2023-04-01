0e725ff77a523cbb6205cf25df819214b64c8cdb
Nick Davies <nickdavies@meta.com>
2023-03-31 21:29:58 -0700

    [antlir2] Make MountHandle which enforces umount ordering is correct
    Summary: This extends the `Mounter` trait to return a new type `MountHandle` which won't allow you to umount things if you have mounted things inside of it using the same chain of handles.
    
    Test Plan:
    Made a test that shouldn't compile:
    {P671900260}
    
    and saw that it won't:
    ```
    error[E0505]: cannot move out of `parent` because it is borrowed
       --> antlir/antlir2/antlir2_package/antlir2_btrfs_packager/src/main.rs:205:5
        |
    195 |       let child = parent
        |  _________________-
    196 | |         .mount_relative(
    197 | |             Path::new("/dev/sda2"),
    198 | |             Path::new("home"),
    ...   |
    201 | |             None,
    202 | |         )
        | |_________- borrow of `parent` occurs here
    ...
    205 |       parent.umount(false);
        |       ^^^^^^ move out of `parent` occurs here
    206 |       child.umount(false);
        |       ----- borrow later used here
    
    ```
    
    Reviewed By: zeroxoneb
    
    Differential Revision: D44520962
    
    fbshipit-source-id: 89fc1f18106e098198d18974bb72e600842ae40f

--- antlir/antlir2/antlir2_mount/src/lib.rs
+++ antlir/antlir2/antlir2_mount/src/lib.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Facebook, Inc. and its affiliates.
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
@@ -117,15 +117,15 @@ impl SafeMounter for RealSafeMounter {
 }
 
 #[mockall::automock]
-pub trait Mounter {
-    fn mount<'a>(
+pub trait Mounter: Sized {
+    fn mount<'a, 'b>(
         &'a self,
-        source: &'a Path,
-        target: &'a Path,
-        fstype: Option<&'a str>,
+        source: &'b Path,
+        target: &'b Path,
+        fstype: Option<&'b str>,
         flags: MsFlags,
-        data: Option<&'a str>,
-    ) -> Result<(), MountError>;
+        data: Option<&'b str>,
+    ) -> Result<MountHandle<'a, Self>, MountError>;
 
     fn umount(&self, mountpoint: &Path, force: bool) -> Result<(), nix::errno::Errno>;
 }
@@ -136,14 +136,14 @@ pub struct RealMounter {
 }
 
 impl Mounter for RealMounter {
-    fn mount(
-        &self,
-        source: &Path,
-        target: &Path,
-        fstype: Option<&str>,
+    fn mount<'a, 'b>(
+        &'a self,
+        source: &'b Path,
+        target: &'b Path,
+        fstype: Option<&'b str>,
         flags: MsFlags,
-        data: Option<&str>,
-    ) -> Result<(), MountError> {
+        data: Option<&'b str>,
+    ) -> Result<MountHandle<'a, Self>, MountError> {
         info!(
             self.log,
             "Mounting {} to {} with fstype {:?}, flags {:?} and options {:?}",
@@ -154,7 +154,7 @@ impl Mounter for RealMounter {
             data
         );
         match nix::mount::mount(Some(source), target, fstype, flags, data) {
-            Ok(()) => Ok(()),
+            Ok(()) => Ok(MountHandle::new(target.to_path_buf(), self)),
             Err(nix::errno::Errno::ENOENT) => Err(if !target.exists() {
                 MountError::MissingTarget(target.to_path_buf())
             } else if !source.exists() {
@@ -178,5 +178,133 @@ impl Mounter for RealMounter {
             flags
         );
         nix::mount::umount2(mountpoint, flags)
+    }
+}
+
+/// This mounter is bounded to live at most as long as the
+/// mounter that it contains and will give out auto-unmounting
+/// mounts. The primary use for this is to have mounts that aren't
+/// meant to survive longer than something local to the binary.
+/// For example if a loopback device is created and things are mounted from it
+/// this can be used to ensure that the mounts are taken down before the loopback
+/// device is detatched
+pub struct BoundMounter<'a, M: Mounter>(&'a M);
+
+impl<'a, M> BoundMounter<'a, M>
+where
+    M: Mounter,
+{
+    pub fn new(binding_reference: &'a M) -> Self {
+        Self(binding_reference)
+    }
+}
+
+impl<'limit, M> Mounter for BoundMounter<'limit, M>
+where
+    M: Mounter,
+{
+    fn mount<'a, 'b>(
+        &'a self,
+        source: &'b Path,
+        target: &'b Path,
+        fstype: Option<&'b str>,
+        flags: MsFlags,
+        data: Option<&'b str>,
+    ) -> Result<MountHandle<'a, Self>, MountError> {
+        match self.0.mount(source, target, fstype, flags, data) {
+            Ok(mut handle) => {
+                handle.auto_umount();
+                Ok(handle.replace_mounter_unchecked(self))
+            }
+            Err(e) => Err(e),
+        }
+    }
+
+    fn umount(&self, mountpoint: &Path, force: bool) -> Result<(), nix::errno::Errno> {
+        self.0.umount(mountpoint, force)
+    }
+}
+
+pub struct MountHandle<'a, M>
+where
+    M: Mounter,
+{
+    target: PathBuf,
+    mounter: &'a M,
+    auto_umount: bool,
+    unmounted: bool,
+}
+
+impl<'a, M> MountHandle<'a, M>
+where
+    M: Mounter,
+{
+    fn new(target: PathBuf, mounter: &'a M) -> Self {
+        Self {
+            target,
+            mounter,
+            auto_umount: false,
+            unmounted: false,
+        }
+    }
+
+    pub fn umount(mut self, force: bool) -> Result<(), nix::errno::Errno> {
+        self.mounter.umount(&self.target, force)?;
+        self.unmounted = true;
+        Ok(())
+    }
+
+    fn replace_mounter_unchecked<'b, NM: Mounter>(
+        mut self,
+        new_mounter: &'b NM,
+    ) -> MountHandle<'b, NM> {
+        let auto_umount = self.auto_umount;
+        self.auto_umount = false;
+        MountHandle {
+            target: self.target.clone(),
+            mounter: new_mounter,
+            auto_umount,
+            unmounted: false,
+        }
+    }
+
+    pub fn auto_umount(&mut self) {
+        self.auto_umount = true;
+    }
+
+    pub fn mountpoint(&self) -> &Path {
+        &self.target
+    }
+
+    pub fn mount_relative<'b, 'c>(
+        &'a self,
+        source: &'c Path,
+        relative_target: &'c Path,
+        fstype: Option<&'c str>,
+        flags: MsFlags,
+        data: Option<&'c str>,
+    ) -> Result<MountHandle<'b, M>, MountError>
+    where
+        'a: 'b,
+    {
+        let target = self.target.join(relative_target);
+        let mut handle = self.mounter.mount(source, &target, fstype, flags, data)?;
+
+        if self.auto_umount {
+            handle.auto_umount();
+        }
+        Ok(handle)
+    }
+}
+
+impl<'a, M> Drop for MountHandle<'a, M>
+where
+    M: Mounter,
+{
+    fn drop(&mut self) {
+        if self.auto_umount && !self.unmounted {
+            let _ = self.mounter.umount(&self.target, true);
+            self.unmounted = true;
+        }
     }
 }
