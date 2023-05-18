df288e1a767374f9cf4949b1acd0896f29bafb99
Vinnie Magro <vmagro@meta.com>
2023-05-18 12:14:46 -0700

    [antlir2][chef-solo] ignore unknown dnf commands
    Summary:
    Title
    This will soon be unnecessary with sergeyfd's fancy new chef evaluation that
    will be much faster and also safer
    
    Test Plan:
    On D45669553, this failed before but now passes
    ```
    ❯ buck build fbcode//os_foundation/images/impl/centos9:base.c9.rc.antlir2
    ```
    
    Reviewed By: r1mikey
    
    Differential Revision: D45791876
    
    fbshipit-source-id: a17e4b17fd8cec90c8b624fc13cecf213b21f1cc

--- antlir/antlir2/antlir2_compile/dnf-recorder.py
+++ antlir/antlir2/antlir2_compile/dnf-recorder.py
@@ -8,11 +8,21 @@ import argparse
 import json
 
 parser = argparse.ArgumentParser()
-parser.add_argument("action", choices=["install", "remove"])
+parser.add_argument("action")
 parser.add_argument("package_specs", nargs="+")
 
 args, unknown = parser.parse_known_args()
 
-with open("/antlir2_dnf_log.jsonl", "a") as f:
-    json.dump({"action": args.action, "package_specs": args.package_specs}, f)
-    f.write("\n")
+if args.action in {
+    "install",
+    "install-n",
+    "install-na",
+    "install-nevra",
+    "remove",
+    "remove-n",
+    "remove-na",
+    "remove-nevra",
+}:
+    with open("/antlir2_dnf_log.jsonl", "a") as f:
+        json.dump({"action": args.action, "package_specs": args.package_specs}, f)
+        f.write("\n")
