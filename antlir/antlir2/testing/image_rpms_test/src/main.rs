2160ec4fce775621d93b26f4380aa5d0e23dc3f0
Vinnie Magro <vmagro@meta.com>
2023-05-26 05:53:50 -0700

    [antlir2] buck rule for rpm verify test
    Summary:
    Verify integrity of installed rpms to ensure that any runtime rpm
    re-installation will not undo changes done by image features.
    
    It might actually be useful to automatically add this to every antlir2 layer,
    but we can look at that later.
    
    I'm only doing this for antlir2 because D46185419 will enable antlir2 layers
    for all of metalos where we care about this and implementing it for antlir1
    will take me substantially more work.
    
    Test Plan:
    ```
    ❯ buck2 test fbcode//antlir/antlir2/testing/tests:test-foo-rpm-integrity fbcode//antlir/antlir2/testing/tests:test-foo-rpm-integrity-removed-file
    Buck UI: https://www.internalfb.com/buck2/f42e3f8a-6117-42a3-a855-98fd5f242be1
    Test UI: https://www.internalfb.com/intern/testinfra/testrun/2814749943915831
    RE: reSessionID-37118eaa-2a3f-4bb5-9c2c-1aa3ee5934ee  Up: 174 B  Down: 0 B
    Jobs completed: 9. Time elapsed: 7.1s.
    Tests finished: Pass 2. Fail 0. Fatal 0. Skip 0. Build failure 0
    ```
    
    Reviewed By: aijayadams
    
    Differential Revision: D46192520
    
    fbshipit-source-id: ab18c48a5448d06e740806cb6be3e971ab1ee2fc

--- antlir/antlir2/testing/image_rpms_test/src/main.rs
+++ antlir/antlir2/testing/image_rpms_test/src/main.rs
@@ -10,12 +10,17 @@ use std::collections::BTreeSet;
 use std::fmt::Display;
 use std::path::PathBuf;
 use std::process::Command;
+use std::str::FromStr;
 
+use anyhow::anyhow;
 use anyhow::ensure;
 use anyhow::Context;
+use anyhow::Error;
 use anyhow::Result;
 use clap::Parser;
 use itertools::Itertools;
+use once_cell::sync::Lazy;
+use regex::Regex;
 use size::Size;
 
 #[derive(Parser)]
@@ -26,6 +31,7 @@ enum Args {
         /// Print details about installed rpms, don't run test
         print: bool,
     },
+    RpmVerify,
 }
 
 #[derive(Debug)]
@@ -43,6 +49,89 @@ impl Display for InstalledInfo {
             write!(f, " required by {}", required_by.iter().join(" "))?;
         }
         Ok(())
+    }
+}
+
+static RPM_VERIFY_RE: Lazy<Regex> = Lazy::new(|| {
+    Regex::new(r#"^((?:missing)|(?:.{9}))(?:\s+(c|d|g|l|r))?\s+(.*)$"#)
+        .expect("definitely compiles")
+});
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+enum RpmTest {
+    /// file size differs
+    Size,
+    /// file mode (permissions and/or file type) differs
+    Mode,
+    /// checksum of file differs
+    Digest,
+    /// major/minor device number differs
+    Device,
+    /// symlink target differs
+    Link,
+    /// owner Uid differs
+    User,
+    /// owner Gid differs
+    Group,
+    /// Mtime differs
+    Time,
+    /// Capabilities differ
+    Capabilities,
+    /// file is entirely missing
+    Missing,
+    /// rpm --verify test passed
+    Pass,
+}
+
+impl TryFrom<char> for RpmTest {
+    type Error = Error;
+    fn try_from(c: char) -> Result<Self> {
+        match c {
+            'S' => Ok(Self::Size),
+            'M' => Ok(Self::Mode),
+            '5' => Ok(Self::Digest),
+            'D' => Ok(Self::Device),
+            'L' => Ok(Self::Link),
+            'U' => Ok(Self::User),
+            'G' => Ok(Self::Group),
+            'T' => Ok(Self::Time),
+            'P' => Ok(Self::Capabilities),
+            '.' => Ok(Self::Pass),
+            _ => Err(anyhow!("unrecognized test character: {}", c)),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+struct FileIntegrity {
+    path: String,
+    failed_tests: BTreeSet<RpmTest>,
+}
+
+impl FromStr for FileIntegrity {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        if let Some(m) = RPM_VERIFY_RE.captures(s) {
+            let tests = m.get(1).expect("must exist").as_str();
+            let path = m.get(3).expect("must exist").as_str();
+            let failed_tests = match tests {
+                "missing" => BTreeSet::from([RpmTest::Missing]),
+                _ => tests
+                    .chars()
+                    .map(RpmTest::try_from)
+                    .collect::<Result<Vec<_>>>()?
+                    .into_iter()
+                    .filter(|t| *t != RpmTest::Pass)
+                    .collect(),
+            };
+            Ok(Self {
+                path: path.to_owned(),
+                failed_tests,
+            })
+        } else {
+            Err(anyhow!("'{s}' did not match the regex"))
+        }
     }
 }
 
@@ -128,6 +217,31 @@ fn main() -> Result<()> {
                 installed: installed_names,
                 "Installed rpms don't match. `buck run` this test with `-- --print` to generate a new source file"
             );
+        }
+        Args::RpmVerify => {
+            let res = Command::new("rpm")
+                .arg("--root=/layer")
+                .arg("--verify")
+                .arg("--all")
+                .output()
+                .context("failed to execute rpm")?;
+            let mut failed = BTreeSet::new();
+            for line in std::str::from_utf8(&res.stdout)
+                .context("output not utf8")?
+                .lines()
+            {
+                let item: FileIntegrity = line.parse().context("while parsing rpm line")?;
+                if !item.failed_tests.is_empty() {
+                    failed.insert(item);
+                }
+            }
+            ensure!(
+                failed.is_empty(),
+                "one or more rpm-owned files failed verification: {failed:#?}"
+            );
+            // ensure that the rpm command did not fail in cases where we didn't
+            // get any output
+            ensure!(res.status.success(), "'rpm' failed");
         }
     }
     Ok(())
