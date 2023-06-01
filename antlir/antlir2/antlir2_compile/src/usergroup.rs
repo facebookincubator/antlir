934d29d70e4cdb02610720c99659a6e5e5c8591f
Vinnie Magro <vmagro@meta.com>
2023-06-01 11:03:51 -0700

    [antlir2][usergroup] restrict range that new ids are chosen in
    Summary:
    When building a layer that has `nobody` at a high uid, choosing the next one
    will cause weird and subtle problems (eg, systemd won't start a service with
    that `User=`)
    
    Test Plan:
    ```
    ❯ buck2 test fbcode//antlir/antlir2/antlir2_compile: fbcode//antlir/antlir2/test_images/usergroup:
    Buck UI: https://www.internalfb.com/buck2/2715b361-719e-4c5b-843c-4cc7e5834db8
    Test UI: https://www.internalfb.com/intern/testinfra/testrun/281475286559113
    RE: reSessionID-d4314558-3def-4c87-a022-ea3da4ce8a7f  Up: 4.5 MiB  Down: 0 B
    Jobs completed: 38. Time elapsed: 31.7s. Cache hits: 0%. Commands: 16 (cached: 0, remote: 0, local: 16)
    Tests finished: Pass 6. Fail 0. Fatal 0. Skip 0. Build failure 0
    ```
    
    Reviewed By: epilatow
    
    Differential Revision: D46208349
    
    fbshipit-source-id: d85c06ac50674fba61c9163e209f7c03a2cfec78

--- antlir/antlir2/antlir2_compile/src/usergroup.rs
+++ antlir/antlir2/antlir2_compile/src/usergroup.rs
@@ -13,12 +13,62 @@ use antlir2_features::usergroup::User;
 use antlir2_features::usergroup::UserMod;
 use antlir2_users::group::GroupRecord;
 use antlir2_users::passwd::UserRecord;
+use antlir2_users::Id;
 use antlir2_users::Password;
+use anyhow::Context;
 
 use crate::CompileFeature;
 use crate::CompilerContext;
 use crate::Error;
 use crate::Result;
+
+trait NextAvailableExt {
+    type Id: Id;
+    type UsedIds: Iterator<Item = Self::Id>;
+
+    fn used_ids(&self) -> Self::UsedIds;
+
+    fn next_available_id(&self) -> Option<Self::Id> {
+        let candidate = self
+            .used_ids()
+            .map(Self::Id::into_raw)
+            // this numbers is semi-magical, but matches the defaults in
+            // /etc/login.defs for choosing new ids
+            .filter(|&id| id >= 1000 && id <= 60000)
+            .max()
+            .unwrap_or(999)
+            + 1;
+        if candidate > 6000 {
+            None
+        } else {
+            Some(Self::Id::from_raw(candidate))
+        }
+    }
+}
+
+impl NextAvailableExt for antlir2_users::passwd::EtcPasswd<'_> {
+    type Id = antlir2_users::UserId;
+    type UsedIds = <Vec<Self::Id> as IntoIterator>::IntoIter;
+
+    fn used_ids(&self) -> Self::UsedIds {
+        self.records()
+            .map(|r| r.uid)
+            .collect::<Vec<_>>()
+            .into_iter()
+    }
+}
+
+impl NextAvailableExt for antlir2_users::group::EtcGroup<'_> {
+    type Id = antlir2_users::GroupId;
+    type UsedIds = <Vec<Self::Id> as IntoIterator>::IntoIter;
+
+    fn used_ids(&self) -> Self::UsedIds {
+        self.records()
+            .map(|r| r.gid)
+            .collect::<Vec<_>>()
+            .into_iter()
+    }
+}
 
 impl<'a> CompileFeature for User<'a> {
     #[tracing::instrument(name = "user", skip(ctx), ret, err)]
@@ -27,7 +77,9 @@ impl<'a> CompileFeature for User<'a> {
         let uid = match self.uid {
             Some(uid) => uid.id().into(),
             None => {
-                let uid = user_db.next_available_uid();
+                let uid = user_db
+                    .next_available_id()
+                    .context("no more uids available")?;
                 tracing::trace!("next available uid = {uid}");
                 uid
             }
@@ -91,7 +143,9 @@ impl<'a> CompileFeature for Group<'a> {
         let gid = match self.gid {
             Some(gid) => gid.id().into(),
             None => {
-                let gid = groups_db.next_available_gid();
+                let gid = groups_db
+                    .next_available_id()
+                    .context("no more gids available")?;
                 tracing::trace!("next available gid = {gid}");
                 gid
             }
@@ -105,5 +159,70 @@ impl<'a> CompileFeature for Group<'a> {
         groups_db.push(record);
         std::fs::write(ctx.dst_path("/etc/group"), groups_db.to_string())?;
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    use antlir2_users::group::EtcGroup;
+    use antlir2_users::passwd::EtcPasswd;
+    use antlir2_users::GroupId;
+    use antlir2_users::UserId;
+
+    use super::*;
+
+    #[test]
+    fn ids_from_correct_range() {
+        let passwd = EtcPasswd::parse(
+            r#"root:x:0:0:root:/root:/bin/bash
+bin:x:1:1:bin:/bin:/sbin/nologin
+nobody:x:65534:65534:Kernel Overflow User:/:/sbin/nologin
+"#,
+        )
+        .expect("failed to parse passwd");
+        assert_eq!(Some(UserId::from(1000)), passwd.next_available_id());
+        let groups = EtcGroup::parse(
+            r#"root:x:0:
+bin:x:1:root,daemon
+nobody:x:65534:
+"#,
+        )
+        .expect("failed to parse group");
+        assert_eq!(Some(GroupId::from(1000)), groups.next_available_id());
+    }
+
+    #[test]
+    fn id_space_exhausted() {
+        let mut passwd = EtcPasswd::default();
+        let mut group = EtcGroup::default();
+        for id in 0..=60000 {
+            passwd.push(UserRecord {
+                name: format!("u{id}").into(),
+                password: Password::Shadow,
+                uid: id.into(),
+                gid: id.into(),
+                comment: "".into(),
+                homedir: Path::new("/").into(),
+                shell: Path::new("/bin/nologin").into(),
+            });
+            group.push(GroupRecord {
+                name: format!("u{id}").into(),
+                password: Password::Shadow,
+                gid: id.into(),
+                users: Vec::new(),
+            });
+        }
+        assert_eq!(
+            passwd.next_available_id(),
+            None,
+            "id space was exhausted, no uid should be possible"
+        );
+        assert_eq!(
+            group.next_available_id(),
+            None,
+            "id space was exhausted, no gid should be possible"
+        );
     }
 }
