33e79a2689df18ab70c572ddee35fcadd9d1238d
Vinnie Magro <vmagro@meta.com>
2023-06-16 12:50:17 -0700

    [antlir2][working_volume] take lock while creating redirect
    Summary: This is flaky on sandcaslte
    
    Test Plan:
    Not sure if this will resolve the race condition, but this proves correctness:
    
    ```
    ❯ eden redirect del antlir2-out
    
    ❯ buck2 build fbcode//antlir/antlir2/facebook/images/build_appliance/centos9:build-appliance
    BUILD SUCCEEDED
    ```
    
    Reviewed By: Babar
    
    Differential Revision: D46805824
    
    fbshipit-source-id: b6df7463720521d9d04e263eec750785d6b5d718

--- antlir/antlir2/antlir2_working_volume/src/lib.rs
+++ antlir/antlir2/antlir2_working_volume/src/lib.rs
@@ -9,9 +9,17 @@ use std::io::Error;
 use std::io::ErrorKind;
 use std::io::Result;
 use std::ops::Deref;
+use std::os::fd::AsRawFd;
 use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
+
+use nix::dir::Dir;
+use nix::errno::Errno;
+use nix::fcntl::flock;
+use nix::fcntl::FlockArg;
+use nix::fcntl::OFlag;
+use nix::sys::stat::Mode;
 
 #[derive(Debug, Clone)]
 pub struct WorkingVolume(PathBuf);
@@ -24,32 +32,41 @@ impl WorkingVolume {
         } else {
             // If we're on Eden, create a new redirection
             // https://www.internalfb.com/intern/wiki/EdenFS/detecting-an-eden-mount/#on-linux-and-macos
-            if Path::new(".eden").exists() {
-                let res = Command::new("eden")
-                    .env("EDENFSCTL_ONLY_RUST", "1")
-                    .arg("redirect")
-                    .arg("add")
-                    .arg(&path)
-                    .arg("bind")
-                    .spawn()?
-                    .wait()?;
-                if path.exists() {
-                    // the redirect add may have failed due to a race condition that eden
-                    // doesn't guard against, but if the path exists now just call it good
-                    Ok(Self(path))
-                } else {
-                    Err(Error::new(
-                        ErrorKind::Other,
-                        format!("'eden redirect add' failed: {res}"),
-                    ))
+            match Dir::open(".eden", OFlag::O_RDONLY, Mode::empty()) {
+                Ok(dir) => {
+                    // There seems to be some racy behavior with eden adding
+                    // redirects, take an exclusive lock before adding
+                    flock(dir.as_raw_fd(), FlockArg::LockExclusive)?;
+                    let res = Command::new("eden")
+                        .env("EDENFSCTL_ONLY_RUST", "1")
+                        .arg("redirect")
+                        .arg("add")
+                        .arg(&path)
+                        .arg("bind")
+                        .spawn()?
+                        .wait()?;
+                    if res.success() {
+                        Ok(Self(path))
+                    } else {
+                        Err(Error::new(
+                            ErrorKind::Other,
+                            format!("'eden redirect add' failed: {res}"),
+                        ))
+                    }
                 }
-            } else if let Err(e) = std::fs::create_dir(&path) {
-                match e.kind() {
-                    ErrorKind::AlreadyExists => Ok(Self(path)),
-                    _ => Err(e),
-                }
-            } else {
-                Ok(Self(path))
+                Err(e) => match e {
+                    Errno::ENOENT => {
+                        if let Err(e) = std::fs::create_dir(&path) {
+                            match e.kind() {
+                                ErrorKind::AlreadyExists => Ok(Self(path)),
+                                _ => Err(e),
+                            }
+                        } else {
+                            Ok(Self(path))
+                        }
+                    }
+                    _ => Err(e.into()),
+                },
             }
         }
     }
