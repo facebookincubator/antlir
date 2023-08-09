1fedcb71e68dddb0b00b778161e70accc15724cc
Junjie Wu <junjiew@meta.com>
2023-08-09 16:22:21 -0700

    [antlir2][vm] add test wrapper for python tests
    Summary:
    We need some special handling for XAR dependency in order to run
    python tests in opt mode.
    
    Test Plan: Verify the new tests works. They should show up in test signal.
    
    Differential Revision: D48206017
    
    fbshipit-source-id: 0bbaf33ab914fd01bae95845f3c5fc7573c0f2ca

---
+++ antlir/antlir2/antlir2_vm/tests/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env fbpython
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import os
+import unittest
+
+
+class TestInstalledBinary(unittest.TestCase):
+    def test_is_root(self) -> None:
+        self.assertEqual(0, os.getuid())
+
+    def test_env_propagated(self) -> None:
+        self.assertEqual("1", os.getenv("ANTLIR2_TEST"))
