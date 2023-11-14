92a1f830e179f8db679e70216acef07bc1b52eac
Vinnie Magro <vmagro@meta.com>
2023-11-13 20:58:01 -0800

    [antlir2][image_test] refactor to take in a json file
    Summary:
    I thought this would help me make static listing work. It doesn't directly, but
    IMO the code is still cleaner so submitting this by itself.
    
    Test Plan:
    ```
    ❯ buck2 test fbcode//antlir/antlir2/testing/tests:
    Buck UI: https://www.internalfb.com/buck2/346233cf-329f-40aa-96b8-79f6c9db5c23
    Test UI: https://www.internalfb.com/intern/testinfra/testrun/14073748845945326
    Note:    Using experimental modern dice
    Network: Up: 19MiB  Down: 656MiB  (reSessionID-7ceea108-8182-487d-b363-d9d3d81cb6d4)
    Jobs completed: 572774. Time elapsed: 2:55.0s.
    Cache hits: 76%. Commands: 3922 (cached: 2970, remote: 0, local: 952). Fallback: 3/952
    Tests finished: Pass 111. Fail 0. Fatal 0. Skip 1. Build failure 0
    ```
    
    Reviewed By: epilatow
    
    Differential Revision: D51271548
    
    fbshipit-source-id: c8c92105ed0f0bb717459888f7983e44af4d795b

--- antlir/antlir2/testing/image_test/src/main.rs
+++ antlir/antlir2/testing/image_test/src/main.rs
@@ -8,6 +8,7 @@
 use std::collections::BTreeMap;
 use std::collections::BTreeSet;
 use std::collections::HashMap;
+use std::ffi::OsString;
 use std::fs::File;
 use std::fs::Permissions;
 use std::io::Write;
@@ -27,11 +28,11 @@ use anyhow::ensure;
 use anyhow::Context;
 use anyhow::Result;
 use clap::Parser;
-use image_test_lib::KvPair;
 use image_test_lib::Test;
 use json_arg::JsonFile;
 use loopdev::LoopControl;
 use mount::Mount;
+use serde::Deserialize;
 use tempfile::NamedTempFile;
 use tracing::debug;
 use tracing::trace;
@@ -41,42 +42,46 @@ fn make_log_files(_base: &str) -> Result<(NamedTempFile, NamedTempFile)> {
     Ok((NamedTempFile::new()?, NamedTempFile::new()?))
 }
 
+#[derive(Debug, Clone, Deserialize)]
+struct TestSpec {
+    /// Path to layer to run the test in
+    layer: PathBuf,
+    /// Run the test as this user
+    user: String,
+    #[serde(default)]
+    /// Set container hostname
+    hostname: Option<String>,
+    /// Boot the container with /init as pid1 before running the test
+    boot: Option<Boot>,
+    /// Set these env vars in the test environment
+    #[serde(default)]
+    setenv: BTreeMap<String, String>,
+    #[serde(default)]
+    /// Set these env vars in the test environment based on what is present in the parent
+    pass_env: Vec<String>,
+    #[serde(default)]
+    /// Mounts required by the layer-under-test
+    mounts: BTreeSet<Mount>,
+    #[serde(default)]
+    /// Allocate N loopback devices and bind them into the container
+    allocate_loop_devices: u8,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+struct Boot {
+    /// Add Requires= and After= dependencies on these units
+    requires_units: Vec<String>,
+    /// Add an After= dependency on these units
+    after_units: Vec<String>,
+    /// Add Wants= dependencies on these units
+    wants_units: Vec<String>,
+}
+
 #[derive(Parser, Debug)]
 /// Run a unit test inside an image layer.
 struct Args {
     #[clap(long)]
-    /// Path to layer to run the test in
-    layer: PathBuf,
-    #[clap(long, default_value = "root")]
-    /// Run the test as this user
-    user: String,
-    #[clap(long)]
-    /// Set container hostname
-    hostname: Option<String>,
-    #[clap(long)]
-    /// Boot the container with /init as pid1 before running the test
-    boot: bool,
-    #[clap(long = "requires-unit", requires = "boot")]
-    /// Add Requires= and After= dependencies on these units
-    requires_units: Vec<String>,
-    #[clap(long = "after-unit", requires = "boot")]
-    /// Add an After= dependency on these units
-    after_units: Vec<String>,
-    #[clap(long = "wants-unit", requires = "boot")]
-    /// Add Wants= dependencies on these units
-    wants_units: Vec<String>,
-    #[clap(long)]
-    /// Set these env vars in the test environment
-    setenv: Vec<KvPair>,
-    #[clap(long)]
-    /// Set these env vars in the test environment based on what is present in the parent
-    pass_env: Vec<String>,
-    #[clap(long)]
-    /// Mounts required by the layer-under-test
-    mounts: JsonFile<BTreeSet<Mount>>,
-    #[clap(long)]
-    /// Allocate N loopback devices and bind them into the container
-    allocate_loop_devices: u8,
+    spec: JsonFile<TestSpec>,
     #[clap(subcommand)]
     test: Test,
 }
@@ -105,10 +110,12 @@ fn main() -> Result<()> {
     )
     .context("while looking for repo root")?;
 
-    let mut setenv: BTreeMap<_, _> = args
+    let spec = args.spec.into_inner();
+
+    let mut setenv: BTreeMap<_, _> = spec
         .setenv
         .into_iter()
-        .map(|pair| (pair.key, pair.value))
+        .map(|(k, v)| (k, OsString::from(v)))
         .collect();
     // forward test runner env vars to the inner test
     for (key, val) in std::env::vars() {
@@ -116,7 +123,7 @@ fn main() -> Result<()> {
             setenv.insert(key, val.into());
         }
     }
-    for key in args.pass_env {
+    for key in spec.pass_env {
         let var =
             std::env::var_os(&key).with_context(|| format!("--pass-env var '{key}' missing"))?;
 
@@ -128,7 +135,7 @@ fn main() -> Result<()> {
 
     let working_directory = std::env::current_dir().context("while getting cwd")?;
 
-    let mut ctx = IsolationContext::builder(&args.layer);
+    let mut ctx = IsolationContext::builder(&spec.layer);
     ctx.platform([
         // test is built out of the repo, so it needs the
         // repo to be available
@@ -145,13 +152,12 @@ fn main() -> Result<()> {
     .working_directory(&working_directory)
     .setenv(setenv.clone())
     .outputs(args.test.output_dirs())
-    .invocation_type(match args.boot {
+    .invocation_type(match spec.boot.is_some() {
         true => InvocationType::BootReadOnly,
         false => InvocationType::Pid2Pipe,
     });
     ctx.inputs(
-        args.mounts
-            .into_inner()
+        spec.mounts
             .into_iter()
             .map(|mount| match mount {
                 Mount::Host(m) => (m.mountpoint, m.src),
@@ -169,7 +175,7 @@ fn main() -> Result<()> {
         ctx.outputs([Path::new("/dev/fuse")]);
     }
 
-    if let Some(hostname) = args.hostname {
+    if let Some(hostname) = spec.hostname {
         ctx.hostname(hostname);
     }
 
@@ -183,9 +189,9 @@ fn main() -> Result<()> {
     // hang on to open fds of loop devices so they don't get closed
     let mut loop_devices = Vec::new();
 
-    if args.allocate_loop_devices > 0 {
+    if spec.allocate_loop_devices > 0 {
         let lc = LoopControl::open().context("while opening loop control")?;
-        for i in 0..args.allocate_loop_devices {
+        for i in 0..spec.allocate_loop_devices {
             let ld = lc.next_free().context("while allocating loop device")?;
             let path = std::fs::read_link(format!("/proc/self/fd/{}", ld.as_raw_fd()))
                 .context("while getting path of loopdev")?;
@@ -195,138 +201,143 @@ fn main() -> Result<()> {
         }
     }
 
-    if args.boot {
-        let container_stdout = container_stdout_file()?;
-        let (mut test_stdout, mut test_stderr) = make_log_files("test")?;
+    match spec.boot {
+        Some(boot) => {
+            let container_stdout = container_stdout_file()?;
+            let (mut test_stdout, mut test_stderr) = make_log_files("test")?;
 
-        let mut test_unit_dropin = NamedTempFile::new()?;
-        writeln!(test_unit_dropin, "[Unit]")?;
+            let mut test_unit_dropin = NamedTempFile::new()?;
+            writeln!(test_unit_dropin, "[Unit]")?;
 
-        // If a test requires default.target, it really wants the _real_
-        // default.target, not the test itself which becomes default.target when
-        // we pass systemd.unit=
-        let res = Command::new("systemctl")
-            .arg("get-default")
-            .arg("--root")
-            .arg(&args.layer)
-            .output()
-            .context("while running systemctl get-default")?;
-        ensure!(
-            res.status.success(),
-            "systemctl get-default failed: {}",
-            String::from_utf8_lossy(&res.stderr)
-        );
-        let default_target = std::str::from_utf8(&res.stdout)
-            .context("systemctl get-default returned invalid utf8")?
-            .trim();
-        trace!("default target was {default_target}");
+            // If a test requires default.target, it really wants the _real_
+            // default.target, not the test itself which becomes default.target when
+            // we pass systemd.unit=
+            let res = Command::new("systemctl")
+                .arg("get-default")
+                .arg("--root")
+                .arg(&spec.layer)
+                .output()
+                .context("while running systemctl get-default")?;
+            ensure!(
+                res.status.success(),
+                "systemctl get-default failed: {}",
+                String::from_utf8_lossy(&res.stderr)
+            );
+            let default_target = std::str::from_utf8(&res.stdout)
+                .context("systemctl get-default returned invalid utf8")?
+                .trim();
+            trace!("default target was {default_target}");
 
-        for unit in &args.requires_units {
-            let unit = match unit.as_str() {
-                "default.target" => default_target,
-                unit => unit,
-            };
-            writeln!(test_unit_dropin, "Requires={unit}")?;
-        }
-        for unit in args.requires_units.iter().chain(&args.after_units) {
-            let unit = match unit.as_str() {
-                "default.target" => default_target,
-                unit => unit,
-            };
-            writeln!(test_unit_dropin, "After={unit}")?;
-        }
-        for unit in args.wants_units.iter() {
-            let unit = match unit.as_str() {
-                "default.target" => default_target,
-                unit => unit,
-            };
-            writeln!(test_unit_dropin, "Wants={unit}")?;
-        }
+            for unit in &boot.requires_units {
+                let unit = match unit.as_str() {
+                    "default.target" => default_target,
+                    unit => unit,
+                };
+                writeln!(test_unit_dropin, "Requires={unit}")?;
+            }
+            for unit in boot.requires_units.iter().chain(&boot.after_units) {
+                let unit = match unit.as_str() {
+                    "default.target" => default_target,
+                    unit => unit,
+                };
+                writeln!(test_unit_dropin, "After={unit}")?;
+            }
+            for unit in boot.wants_units.iter() {
+                let unit = match unit.as_str() {
+                    "default.target" => default_target,
+                    unit => unit,
+                };
+                writeln!(test_unit_dropin, "Wants={unit}")?;
+            }
 
-        writeln!(test_unit_dropin, "[Service]")?;
+            writeln!(test_unit_dropin, "[Service]")?;
 
-        writeln!(test_unit_dropin, "User={}", args.user)?;
-        write!(test_unit_dropin, "WorkingDirectory=")?;
-        let cwd = std::env::current_dir().context("while getting cwd")?;
-        test_unit_dropin.write_all(cwd.as_os_str().as_bytes())?;
-        test_unit_dropin.write_all(b"\n")?;
+            writeln!(test_unit_dropin, "User={}", spec.user)?;
+            write!(test_unit_dropin, "WorkingDirectory=")?;
+            let cwd = std::env::current_dir().context("while getting cwd")?;
+            test_unit_dropin.write_all(cwd.as_os_str().as_bytes())?;
+            test_unit_dropin.write_all(b"\n")?;
 
-        write!(test_unit_dropin, "Environment=PWD=")?;
-        test_unit_dropin.write_all(cwd.as_os_str().as_bytes())?;
-        test_unit_dropin.write_all(b"\n")?;
+            write!(test_unit_dropin, "Environment=PWD=")?;
+            test_unit_dropin.write_all(cwd.as_os_str().as_bytes())?;
+            test_unit_dropin.write_all(b"\n")?;
 
-        write!(test_unit_dropin, "ExecStart=")?;
-        let mut iter = args.test.into_inner_cmd().into_iter().peekable();
-        if let Some(exe) = iter.next() {
-            let realpath = std::fs::canonicalize(&exe)
-                .with_context(|| format!("while getting absolute path of {exe:?}"))?;
-            test_unit_dropin.write_all(realpath.as_os_str().as_bytes())?;
-            if iter.peek().is_some() {
-                test_unit_dropin.write_all(b" ")?;
+            write!(test_unit_dropin, "ExecStart=")?;
+            let mut iter = args.test.into_inner_cmd().into_iter().peekable();
+            if let Some(exe) = iter.next() {
+                let realpath = std::fs::canonicalize(&exe)
+                    .with_context(|| format!("while getting absolute path of {exe:?}"))?;
+                test_unit_dropin.write_all(realpath.as_os_str().as_bytes())?;
+                if iter.peek().is_some() {
+                    test_unit_dropin.write_all(b" ")?;
+                }
+            }
+            while let Some(arg) = iter.next() {
+                test_unit_dropin.write_all(arg.as_os_str().as_bytes())?;
+                if iter.peek().is_some() {
+                    test_unit_dropin.write_all(b" ")?;
+                }
+            }
+            test_unit_dropin.write_all(b"\n")?;
+
+            for (key, val) in &setenv {
+                write!(test_unit_dropin, "Environment=\"{key}=")?;
+                test_unit_dropin.write_all(val.as_bytes())?;
+                writeln!(test_unit_dropin, "\"")?;
+            }
+            // forward test runner env vars to the inner test
+            for (key, val) in std::env::vars() {
+                if key.starts_with("TEST_PILOT") {
+                    writeln!(test_unit_dropin, "Environment=\"{key}={val}\"")?;
+                }
+            }
+            // wire the test output to the parent process's std{out,err}
+            ctx.outputs(HashMap::from([
+                (Path::new("/antlir2/test_stdout"), test_stdout.path()),
+                (Path::new("/antlir2/test_stderr"), test_stderr.path()),
+            ]));
+            ctx.inputs((
+                Path::new("/run/systemd/system/antlir2_image_test.service.d/runtime.conf"),
+                test_unit_dropin.path(),
+            ));
+
+            // Register the test container with systemd-machined so manual debugging
+            // is a easier.
+            ctx.register(true);
+
+            let mut isol =
+                isolate(ctx.build())?.command("systemd.unit=antlir2_image_test.service")?;
+            isol.arg("systemd.journald.forward_to_console=1")
+                .arg("systemd.log_time=1")
+                .arg("systemd.setenv=ANTLIR2_IMAGE_TEST=1");
+            debug!("executing test in booted isolated container: {isol:?}");
+            let mut child = isol
+                // the stdout/err of the systemd inside the container is a pipe
+                // so that we can print it IFF the test fails
+                .stdout(container_stdout.try_clone()?)
+                .stderr(container_stdout.try_clone()?)
+                .spawn()
+                .context("while spawning systemd-nspawn")?;
+            let res = child.wait().context("while waiting for systemd-nspawn")?;
+
+            std::io::copy(&mut test_stdout, &mut std::io::stdout())?;
+            std::io::copy(&mut test_stderr, &mut std::io::stderr())?;
+
+            if !res.success() {
+                std::process::exit(res.code().unwrap_or(255))
+            } else {
+                Ok(())
             }
         }
-        while let Some(arg) = iter.next() {
-            test_unit_dropin.write_all(arg.as_os_str().as_bytes())?;
-            if iter.peek().is_some() {
-                test_unit_dropin.write_all(b" ")?;
-            }
+        None => {
+            ctx.user(spec.user);
+            let mut cmd = args.test.into_inner_cmd().into_iter();
+            let mut isol =
+                isolate(ctx.build())?.command(cmd.next().expect("must have program arg"))?;
+            isol.args(cmd);
+            debug!("executing test in isolated container: {isol:?}");
+            Err(anyhow::anyhow!("failed to exec test: {:?}", isol.exec()))
         }
-        test_unit_dropin.write_all(b"\n")?;
-
-        for (key, val) in &setenv {
-            write!(test_unit_dropin, "Environment=\"{key}=")?;
-            test_unit_dropin.write_all(val.as_bytes())?;
-            writeln!(test_unit_dropin, "\"")?;
-        }
-        // forward test runner env vars to the inner test
-        for (key, val) in std::env::vars() {
-            if key.starts_with("TEST_PILOT") {
-                writeln!(test_unit_dropin, "Environment=\"{key}={val}\"")?;
-            }
-        }
-        // wire the test output to the parent process's std{out,err}
-        ctx.outputs(HashMap::from([
-            (Path::new("/antlir2/test_stdout"), test_stdout.path()),
-            (Path::new("/antlir2/test_stderr"), test_stderr.path()),
-        ]));
-        ctx.inputs((
-            Path::new("/run/systemd/system/antlir2_image_test.service.d/runtime.conf"),
-            test_unit_dropin.path(),
-        ));
-
-        // Register the test container with systemd-machined so manual debugging
-        // is a easier.
-        ctx.register(true);
-
-        let mut isol = isolate(ctx.build())?.command("systemd.unit=antlir2_image_test.service")?;
-        isol.arg("systemd.journald.forward_to_console=1")
-            .arg("systemd.log_time=1")
-            .arg("systemd.setenv=ANTLIR2_IMAGE_TEST=1");
-        debug!("executing test in booted isolated container: {isol:?}");
-        let mut child = isol
-            // the stdout/err of the systemd inside the container is a pipe
-            // so that we can print it IFF the test fails
-            .stdout(container_stdout.try_clone()?)
-            .stderr(container_stdout.try_clone()?)
-            .spawn()
-            .context("while spawning systemd-nspawn")?;
-        let res = child.wait().context("while waiting for systemd-nspawn")?;
-
-        std::io::copy(&mut test_stdout, &mut std::io::stdout())?;
-        std::io::copy(&mut test_stderr, &mut std::io::stderr())?;
-
-        if !res.success() {
-            std::process::exit(res.code().unwrap_or(255))
-        } else {
-            Ok(())
-        }
-    } else {
-        ctx.user(args.user);
-        let mut cmd = args.test.into_inner_cmd().into_iter();
-        let mut isol = isolate(ctx.build())?.command(cmd.next().expect("must have program arg"))?;
-        isol.args(cmd);
-        debug!("executing test in isolated container: {isol:?}");
-        Err(anyhow::anyhow!("failed to exec test: {:?}", isol.exec()))
     }
 }
 
