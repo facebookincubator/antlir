64eabd280169485b8c914d86858650b8731d04a6
Vinnie Magro <vmagro@meta.com>
2023-12-13 05:47:45 -0800

    [antlir2][package] add CasDir package format
    Summary:
    This is an implementation detail of rootless image builds and the end-goal
    future of antlir2-builds-on-RE that will require putting image layers on CAS.
    
    CAS cannot preserve file modes and ownership info because it needs to be
    materialized on many different hosts, so this output format consists of a
    directory tree that is recursively owned and readable by the build user, paired
    with a JSON file that describes the ownership and mode metadata for every file
    and directory for later reconstitution.
    
    For builds on RE, this will eventually be the direct output of each buck
    action, at which point this may be removed as a packaging format and treated
    just as the output of all the buck2 rules.
    
    Test Plan:
    ```
    ❯ buck2 build --show-output fbcode//mode/opt fbcode//antlir/antlir2/facebook/images/build_appliance/centos9:build-appliance.impl.cas_dir
    Buck UI: https://www.internalfb.com/buck2/8d4f6a70-5666-4649-8219-29f618632b49
    Network: Up: 626 MiB  Down: 5.1 GiB  (reSessionID-9810dd77-d6b0-4f15-a782-9cb1470afe00)
    Jobs completed: 5. Time elapsed: 13.4s. Cache hits: 0%. Commands: 1 (cached: 0, remote: 0, local: 1)
    BUILD SUCCEEDED
    fbcode//antlir/antlir2/facebook/images/build_appliance/centos9:build-appliance.impl.cas_dir buck-out/v2/gen/fbcode/c8344b52091f4f7f/antlir/antlir2/facebook/images/build_appliance/centos9/__build-appliance.impl.cas_dir__/image
    
    ❯ ls -l buck-out/v2/gen/fbcode/c8344b52091f4f7f/antlir/antlir2/facebook/images/build_appliance/centos9/__build-appliance.impl.cas_dir__/image/contents/usr/bin | head
    total 52120
    -r--r--r-- 1 vmagro users    52856 Jun 22 08:04 [
    -r--r--r-- 1 vmagro users    28408 Jun 22 08:04 addr2line
    -r--r--r-- 1 vmagro users       33 Jun 22 08:04 alias
    -r--r--r-- 1 vmagro users     2672 Jun 22 08:04 amuFormat.sh
    -r--r--r-- 1 vmagro users    57328 Jun 22 08:04 ar
    -r--r--r-- 1 vmagro users    32224 Jun 22 08:04 arch
    -r--r--r-- 1 vmagro users   727376 Jun 22 08:04 as
    lrwxrwxrwx 1 vmagro users        4 Jun 22 08:04 awk -> gawk
    -r--r--r-- 1 vmagro users    52840 Jun 22 08:04 b2sum
    
    ❯ jq < buck-out/v2/gen/fbcode/c8344b52091f4f7f/antlir/antlir2/facebook/images/build_appliance/centos9/__build-appliance.impl.cas_dir__/image/manifest.json | head -20
    {
      "paths": {
        "afs": {
          "uid": 0,
          "gid": 0,
          "mode": 16749
        },
        "bin": {
          "uid": 0,
          "gid": 0,
          "mode": 41471
        },
        "boot": {
          "uid": 0,
          "gid": 0,
          "mode": 16749
        },
        "dev": {
          "uid": 0,
          "gid": 0,
    ...
    
    ❯ time frecli cas upload-directory \
        buck-out/v2/gen/fbcode/c8344b52091f4f7f/antlir/antlir2/facebook/images/build_appliance/centos9/__build-appliance.impl.cas_dir__/image/
    
    9574726963f5fc0eb2e0d7c58fed5ab5ba7662e4:125
    frecli cas upload-directory   3.76s user 3.02s system 80% cpu 8.468 total
    ```
    
    Reviewed By: epilatow
    
    Differential Revision: D46939716
    
    fbshipit-source-id: 62456cd06b6bd55282c4d3391608be3793983208

---
+++ antlir/antlir2/antlir2_cas_dir/src/lib.rs
@@ -0,0 +1,329 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+use std::collections::BTreeMap;
+use std::collections::HashMap;
+use std::fs::Permissions;
+use std::os::unix::fs::MetadataExt;
+use std::os::unix::fs::PermissionsExt;
+use std::path::Path;
+use std::path::PathBuf;
+
+use anyhow::ensure;
+use anyhow::Context;
+use anyhow::Result;
+use nix::sys::stat::SFlag;
+use nix::unistd::Gid;
+use nix::unistd::Uid;
+use serde::Deserialize;
+use serde::Serialize;
+use tracing::trace;
+use walkdir::DirEntryExt;
+use walkdir::WalkDir;
+
+#[derive(Debug)]
+/// [CasDir] is a directory that can represent a filesystem with all of its
+/// contents and metadata in a directory that is still owned by a single,
+/// unprivileged user.
+///
+/// All of the file contents and directory structure are represented inside the
+/// [CasDir], but ownership and mode is only preserved in the json
+/// [CasDirManifest] - all files are owned by the unprivileged build user.
+pub struct CasDir {
+    path: PathBuf,
+    contents_dir: PathBuf,
+    manifest: Manifest,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+struct Manifest {
+    paths: BTreeMap<PathBuf, PathEntry>,
+}
+
+#[derive(Debug, Copy, Clone, Deserialize, Serialize)]
+struct PathEntry {
+    ino: u64,
+    uid: u32,
+    gid: u32,
+    mode: u32,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct CasDirOpts {
+    uid: Uid,
+    gid: Gid,
+}
+
+impl Default for CasDirOpts {
+    fn default() -> Self {
+        Self {
+            uid: Uid::current(),
+            gid: Gid::current(),
+        }
+    }
+}
+
+impl CasDirOpts {
+    pub fn uid(mut self, uid: impl Into<Uid>) -> Self {
+        self.uid = uid.into();
+        self
+    }
+
+    pub fn gid(mut self, gid: impl Into<Gid>) -> Self {
+        self.gid = gid.into();
+        self
+    }
+}
+
+impl CasDir {
+    /// Create a [CasDir] from a regular directory. For performance, root and
+    /// dst should really be on the same (Copy-on-Write) filesystem. Otherwise
+    /// this will still work, but will have to actually copy file contents and
+    /// thus will be slower.
+    pub fn dehydrate(root: impl AsRef<Path>, dst: PathBuf, opts: CasDirOpts) -> Result<Self> {
+        let root = root.as_ref();
+        std::fs::create_dir(&dst).context("while creating output directory")?;
+        std::os::unix::fs::lchown(&dst, Some(opts.uid.as_raw()), Some(opts.gid.as_raw()))
+            .context("while chowning dst")?;
+        let contents_dir = dst.join("contents");
+        std::fs::create_dir(&contents_dir).context("while creating contents directory")?;
+        std::os::unix::fs::lchown(
+            &contents_dir,
+            Some(opts.uid.as_raw()),
+            Some(opts.gid.as_raw()),
+        )
+        .context("while chowning dst")?;
+        let mut manifest = Manifest::default();
+        let mut inodes = HashMap::new();
+        for entry in WalkDir::new(root) {
+            let entry = entry.context("while walking root")?;
+            let relpath = entry
+                .path()
+                .strip_prefix(root)
+                .expect("must be relative to root");
+            if relpath == Path::new("") {
+                continue;
+            }
+            let meta = entry.metadata().context("while statting")?;
+            trace!(
+                "copying {} ({}) {meta:?}",
+                relpath.display(),
+                entry.path().display()
+            );
+            let dst_path = contents_dir.join(relpath);
+            if entry.file_type().is_dir() {
+                std::fs::create_dir(&dst_path).context("while creating dir in contents")?;
+                std::fs::set_permissions(&dst_path, Permissions::from_mode(0o755))
+                    .context("while chmodding contents dir")?;
+            } else if entry.file_type().is_symlink() {
+                let target = std::fs::read_link(entry.path()).context("while reading symlink")?;
+                std::os::unix::fs::symlink(target, &dst_path)
+                    .context("while making symlink in contents")?;
+            } else if entry.file_type().is_file() {
+                if let Some(already_copied) = inodes.get(&entry.ino()) {
+                    std::fs::hard_link(already_copied, &dst_path)
+                        .context("while hardlinking copy")?;
+                } else {
+                    std::fs::copy(entry.path(), &dst_path)
+                        .context("while copying file out of layer")?;
+                    std::fs::set_permissions(&dst_path, Permissions::from_mode(0o444))
+                        .context("while setting permissions")?;
+                    inodes.insert(entry.ino(), dst_path.clone());
+                }
+            } else {
+                unreachable!();
+            }
+            std::os::unix::fs::lchown(dst_path, Some(opts.uid.as_raw()), Some(opts.gid.as_raw()))
+                .context("while chowning copy")?;
+            manifest.paths.insert(
+                relpath.to_path_buf(),
+                PathEntry {
+                    ino: meta.ino(),
+                    uid: meta.uid(),
+                    gid: meta.gid(),
+                    mode: meta.mode(),
+                },
+            );
+        }
+        std::fs::write(
+            dst.join("manifest.json"),
+            serde_json::to_vec(&manifest).context("while serializing manifest")?,
+        )
+        .context("while writing manifest")?;
+        std::os::unix::fs::lchown(
+            dst.join("manifest.json"),
+            Some(opts.uid.as_raw()),
+            Some(opts.gid.as_raw()),
+        )
+        .context("while chowning manifest")?;
+        Ok(Self {
+            path: dst,
+            contents_dir,
+            manifest,
+        })
+    }
+
+    pub fn path(&self) -> &Path {
+        self.path.as_ref()
+    }
+
+    /// Hydrate the [CasDir] into a regular directory with all the filesystem
+    /// metadata being reproduced. The directory must already exist and be
+    /// empty.
+    pub fn hydrate_into(&self, dst: impl AsRef<Path>) -> Result<()> {
+        let dst = dst.as_ref();
+        ensure!(
+            std::fs::read_dir(dst)
+                .context("while ensuring output directory is empty")?
+                .count()
+                == 0,
+            "output directory '{}' was not empty",
+            dst.display()
+        );
+
+        let mut new_inos = HashMap::<u64, PathBuf>::new();
+        for (relpath, entry) in &self.manifest.paths {
+            let received_path = dst.join(relpath);
+            if let Some(new_path) = new_inos.get(&entry.ino) {
+                std::fs::hard_link(new_path, &received_path).with_context(|| {
+                    format!(
+                        "while hard linking {} -> {}",
+                        received_path.display(),
+                        new_path.display()
+                    )
+                })?;
+                continue;
+            }
+            let sflag = SFlag::from_bits_truncate(entry.mode);
+            if sflag.contains(SFlag::S_IFDIR) {
+                std::fs::create_dir(&received_path).with_context(|| {
+                    format!("while creating directory {}", received_path.display())
+                })?;
+                std::fs::set_permissions(&received_path, Permissions::from_mode(entry.mode))
+                    .with_context(|| format!("while chmodding {}", received_path.display()))?;
+            } else if sflag.contains(SFlag::S_IFLNK) {
+                let target =
+                    std::fs::read_link(self.contents_dir.join(relpath)).with_context(|| {
+                        format!(
+                            "while reading link {}",
+                            self.contents_dir.join(relpath).display()
+                        )
+                    })?;
+                std::os::unix::fs::symlink(&target, &received_path).with_context(|| {
+                    format!(
+                        "while symlinking {} -> {}",
+                        received_path.display(),
+                        target.display()
+                    )
+                })?;
+            } else {
+                std::fs::copy(self.contents_dir.join(relpath), &received_path).with_context(
+                    || {
+                        format!(
+                            "while copying file {} -> {}",
+                            received_path.display(),
+                            self.contents_dir.join(relpath).display()
+                        )
+                    },
+                )?;
+                std::fs::set_permissions(&received_path, Permissions::from_mode(entry.mode))
+                    .with_context(|| format!("while chmodding {}", received_path.display()))?;
+            }
+            std::os::unix::fs::lchown(&received_path, Some(entry.uid), Some(entry.gid))
+                .with_context(|| format!("while chowning {}", received_path.display()))?;
+            new_inos.insert(entry.ino, received_path);
+        }
+        Ok(())
+    }
+}
+
+#[cfg(all(test, image_test))]
+mod tests {
+    use super::*;
+
+    fn dehydrate_and_hydrate(opts: CasDirOpts) -> CasDir {
+        let cas_dir = CasDir::dehydrate("/src", "/cas_dir".into(), opts)
+            .expect("failed to dehydrate from regular directory");
+        cas_dir
+            .hydrate_into("/hydrated")
+            .expect("failed to hydrate");
+        cas_dir
+    }
+
+    #[test]
+    fn hardlinks() {
+        // antlir2 doesn't provide a hardlink api, so do it here
+        std::fs::hard_link("/src/TARGETS", "/src/TARGETS.link").expect("failed to hardlink");
+        let cas_dir = dehydrate_and_hydrate(CasDirOpts::default());
+        {
+            let ino = std::fs::metadata(cas_dir.contents_dir.join("TARGETS"))
+                .expect("failed to stat TARGETS")
+                .ino();
+            let ino2 = std::fs::metadata(cas_dir.contents_dir.join("TARGETS.link"))
+                .expect("failed to stat TARGETS.link")
+                .ino();
+            assert_eq!(
+                ino, ino2,
+                "hardlinks should have been preserved during dehydration"
+            );
+        }
+        {
+            let ino = std::fs::metadata("/hydrated/TARGETS")
+                .expect("failed to stat /hydrated/TARGETS")
+                .ino();
+            let ino2 = std::fs::metadata("/hydrated/TARGETS.link")
+                .expect("failed to stat /hydrated/TARGETS.link")
+                .ino();
+            assert_eq!(
+                ino, ino2,
+                "hardlinks should have been preserved during hydration"
+            );
+        }
+    }
+
+    #[test]
+    fn symlink() {
+        // antlir2 doesn't provide a relative symlink api, so do it here
+        std::os::unix::fs::symlink("TARGETS", "/src/subdir/TARGETS.symlink.rel")
+            .expect("failed to make relative symlink");
+        let cas_dir = dehydrate_and_hydrate(CasDirOpts::default());
+        {
+            let target =
+                std::fs::read_link(cas_dir.contents_dir.join("subdir/TARGETS.symlink.abs"))
+                    .expect("failed to read abs link");
+            assert_eq!(
+                Path::new("/src/subdir/TARGETS"),
+                target,
+                "abs symlink should have been preserved during dehydration"
+            );
+            let target =
+                std::fs::read_link(cas_dir.contents_dir.join("subdir/TARGETS.symlink.rel"))
+                    .expect("failed to read rel link");
+            assert_eq!(
+                Path::new("TARGETS"),
+                target,
+                "rel symlink should have been preserved during dehydration"
+            );
+        }
+        {
+            let target = std::fs::read_link("/hydrated/subdir/TARGETS.symlink.abs")
+                .expect("failed to read hydrated abs link");
+            assert_eq!(
+                Path::new("/src/subdir/TARGETS"),
+                target,
+                "abs symlink should have been preserved during hydration"
+            );
+            let target = std::fs::read_link("/hydrated/subdir/TARGETS.symlink.rel")
+                .expect("failed to read hydrated rel link");
+            assert_eq!(
+                Path::new("TARGETS"),
+                target,
+                "rel symlink should have been preserved during hydration"
+            );
+        }
+    }
+}
---
+++ antlir/antlir2/antlir2_packager/src/cas_dir.rs
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+use std::path::Path;
+use std::path::PathBuf;
+
+use antlir2_cas_dir::CasDirOpts;
+use anyhow::Context;
+use anyhow::Result;
+use nix::unistd::Gid;
+use nix::unistd::Uid;
+use serde::Deserialize;
+
+use crate::PackageFormat;
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct CasDir {
+    layer: PathBuf,
+}
+
+impl PackageFormat for CasDir {
+    fn build(&self, out: &Path) -> Result<()> {
+        antlir2_cas_dir::CasDir::dehydrate(
+            &self.layer,
+            out.to_owned(),
+            CasDirOpts::default()
+                .uid(match std::env::var("SUDO_UID") {
+                    Err(_) => Uid::current(),
+                    Ok(id) => Uid::from_raw(id.parse().context("while parsing SUDO_UID")?),
+                })
+                .gid(match std::env::var("SUDO_GID") {
+                    Err(_) => Gid::current(),
+                    Ok(id) => Gid::from_raw(id.parse().context("while parsing SUDO_GID")?),
+                }),
+        )?;
+        Ok(())
+    }
+}
