aa6b77fbc3b3b20a3036809ce5e7cc5bfc6e86fd
Vinnie Magro <vmagro@meta.com>
2024-01-12 08:56:15 -0800

    [antlir1][delete] delete big chunk
    Summary:
    The only remaining usage of antlir1 is all internal to antlir1 itself, so I'm
    just deleting it in semi-arbitrary chunks as long as the target graph keeps
    evaluating.
    
    Test Plan:
    Build graph evaluates
    ```
    ❯ buck2 uquery 'deps(set(fbcode//antlir/...) - set(fbcode//antlir/antlir2/...), 1)'
    ```
    
    Reviewed By: sergeyfd
    
    Differential Revision: D52705578
    
    fbshipit-source-id: 57102c5ff963df0a52a6f4a34daf8a69084e1aac

--- antlir/compiler/test_images/print_ok_again.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-#
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
-# Prints the unicode string "ok again" via the `print` function to `stdout`, on
-# 1 line
-print("ok again")
