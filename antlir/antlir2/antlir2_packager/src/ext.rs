6bd4a751f50a7c17e42d147f4cc8d3d073a212ec
Vinnie Magro <vmagro@meta.com>
2024-04-11 09:56:18 -0700

    [antlir2][packager] switch ext3 to use unshare
    Summary: Support rootless builds (and non-subvol build appliances)
    
    Test Plan:
    ```
    ❯ buck2 test fbcode//antlir/antlir2/test_images/package/ext3:
    Buck UI: https://www.internalfb.com/buck2/f61139f1-c56b-44de-86a4-222fa64b8ef4
    Test UI: https://www.internalfb.com/intern/testinfra/testrun/3377699947466306
    Note:    Using experimental modern dice
    Network: Up: 604B  Down: 3.2KiB  (reSessionID-cd0f15bb-4b1d-43b5-a377-ea39e09d47aa)
    Jobs completed: 28. Time elapsed: 14.3s.
    Cache hits: 0%. Commands: 5 (cached: 0, remote: 0, local: 5)
    Tests finished: Pass 7. Fail 0. Fatal 0. Skip 0. Build failure 0
    ```
    
    Reviewed By: epilatow
    
    Differential Revision: D55972643
    
    fbshipit-source-id: c7118f35a0cb6f8f30192b7d04e0598008bd7512

--- antlir/antlir2/antlir2_packager/src/ext.rs
+++ antlir/antlir2/antlir2_packager/src/ext.rs
@@ -10,7 +10,7 @@ use std::path::Path;
 use std::path::PathBuf;
 use std::process::Stdio;
 
-use antlir2_isolate::nspawn;
+use antlir2_isolate::unshare;
 use antlir2_isolate::IsolationContext;
 use anyhow::ensure;
 use anyhow::Context;
@@ -37,28 +37,26 @@ impl PackageFormat for Ext3 {
         let rootless = antlir2_rootless::init().context("while initializing rootless")?;
         File::create(out).context("failed to create output file")?;
 
-        let layer_abs_path = self
-            .layer
-            .canonicalize()
-            .context("failed to build absolute path to layer")?;
-
-        let output_abs_path = out
-            .canonicalize()
-            .context("failed to build abs path to output")?;
-
         let isol_context = IsolationContext::builder(&self.build_appliance)
-            .inputs([layer_abs_path.as_path()])
-            .outputs([output_abs_path.as_path()])
-            .working_directory(std::env::current_dir().context("while getting cwd")?)
+            .ephemeral(false)
+            .readonly()
+            .tmpfs(Path::new("/__antlir2__/out"))
+            .outputs(("/__antlir2__/out/ext3", out))
+            .inputs((Path::new("/__antlir2__/root"), self.layer.as_path()))
+            .inputs((
+                PathBuf::from("/__antlir2__/working_directory"),
+                std::env::current_dir()?,
+            ))
+            .working_directory(Path::new("/__antlir2__/working_directory"))
             .build();
 
-        let isol = nspawn(isol_context)?;
+        let isol = unshare(isol_context)?;
         let mut cmd = isol.command("mkfs.ext3")?;
         if let Some(label) = &self.label {
             cmd.arg("-L").arg(label);
         }
-        cmd.arg("-d").arg(&layer_abs_path);
-        cmd.arg(&output_abs_path);
+        cmd.arg("-d").arg("/__antlir2__/root");
+        cmd.arg("/__antlir2__/out/ext3");
         if let Some(size_mb) = self.size_mb {
             cmd.arg(format!("{}M", size_mb));
             rootless.as_root(|| {
@@ -67,7 +65,7 @@ impl PackageFormat for Ext3 {
         } else {
             let total_file_size = ByteSize::b(rootless.as_root(|| {
                 Ok::<_, Error>(
-                    WalkDir::new(&layer_abs_path)
+                    WalkDir::new(&self.layer)
                         .into_iter()
                         .map(|entry| {
                             entry.context("while walking directory").and_then(|e| {
@@ -102,24 +100,24 @@ impl PackageFormat for Ext3 {
                 run_cmd(cmd.stdout(Stdio::piped())).context("failed to build ext3 archive")
             })??;
 
-            let out = rootless
-                .as_root(|| run_cmd(isol.command("resize2fs")?.arg("-M").arg(&output_abs_path)))?
+            let resize_out = rootless
+                .as_root(|| run_cmd(isol.command("resize2fs")?.arg("-M").arg(out)))?
                 .context("while minimizing fs size")?;
-            ensure!(out.status.success(), "resize2fs failed");
+            ensure!(resize_out.status.success(), "resize2fs failed");
 
             // Now, if the user asked for some free space, we need to give it to
             // them.
             if self.free_mb != 0 {
                 let f = std::fs::OpenOptions::new()
                     .write(true)
-                    .open(&output_abs_path)
+                    .open(out)
                     .context("while opening image file")?;
                 let size = f.metadata().context("while getting image size")?.len();
 
                 let new_size = size + ByteSize::mib(self.free_mb);
                 f.set_len(new_size.0).context("while growing image file")?;
                 rootless.as_root(|| {
-                    run_cmd(isol.command("resize2fs")?.arg(&output_abs_path))
+                    run_cmd(isol.command("resize2fs")?.arg("/__antlir2__/out/ext3"))
                         .context("failed to resize ext3 archive")
                 })??;
             }
