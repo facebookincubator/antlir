296fbf842c465f0e5ad92df1e7d27f65855277ce
Vinnie Magro <vmagro@meta.com>
2024-06-04 08:16:57 -0700

    [antlir2][overlay] materialize-to-subvol binary
    Summary:
    To gradually migrate to an overlayfs based disk layout, we need a way to
    convert back to a subvol in `antlir2-out/`.
    
    This diff introduces a `materialize-to-subvol` binary that does exactly that.
    For builds using the new overlayfs scheme, they can continue to provide a
    `subvol_symlink` that can materialize to a local subvol on demand.
    
    Test Plan:
    ```
    ❯ buck2 build --show-output fbcode//antlir/antlir2/antlir2_overlayfs/tests:child[subvol_symlink]
    Buck UI: https://www.internalfb.com/buck2/3b9b6004-58f3-4169-b10e-d8b8c59f89e8
    Network: Up: 596B  Down: 2.3KiB  (reSessionID-caf87f10-3534-4de2-a186-6282aac4f051)
    Jobs completed: 7. Time elapsed: 2.9s.
    Cache hits: 0%. Commands: 2 (cached: 0, remote: 0, local: 2)
    BUILD SUCCEEDED
    fbcode//antlir/antlir2/antlir2_overlayfs/tests:child[subvol_symlink] buck-out/v2/gen/fbcode/05af3bec81fdae30/antlir/antlir2/antlir2_overlayfs/tests/__child__/subvol_symlink
    
    ❯ file buck-out/v2/gen/fbcode/05af3bec81fdae30/antlir/antlir2/antlir2_overlayfs/tests/__child__/subvol_symlink
    buck-out/v2/gen/fbcode/05af3bec81fdae30/antlir/antlir2/antlir2_overlayfs/tests/__child__/subvol_symlink: symbolic link to /data/users/vmagro/fbsource/antlir2-out/09bb214f3e474c23a815786e600e0dca
    
    ❯ tree buck-out/v2/gen/fbcode/05af3bec81fdae30/antlir/antlir2/antlir2_overlayfs/tests/__child__/subvol_symlink
    
    ❯ tree -pugh buck-out/v2/gen/fbcode/05af3bec81fdae30/antlir/antlir2/antlir2_overlayfs/tests/__child__/subvol_symlink
    buck-out/v2/gen/fbcode/05af3bec81fdae30/antlir/antlir2/antlir2_overlayfs/tests/__child__/subvol_symlink
    └── [drwxr-xr-x vmagro   users       12]  foo
        ├── [-rw-r--r-- vmagro   users        8]  baz
        └── [-rw-r--r-- vmagro   users        4]  qux
    
    1 directory, 2 files
    ```
    
    Reviewed By: justintrudell
    
    Differential Revision: D57618149
    
    fbshipit-source-id: 36039764ce9cf352cd728a017549046935e1e1d2

---
+++ antlir/antlir2/antlir2_overlayfs/bin/materialize_to_subvol.rs
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+use std::fs::DirBuilder;
+use std::os::unix::fs::DirBuilderExt;
+use std::os::unix::fs::MetadataExt;
+use std::path::Path;
+use std::path::PathBuf;
+
+use antlir2_btrfs::Subvolume;
+use antlir2_overlayfs::OverlayFs;
+use antlir2_working_volume::WorkingVolume;
+use anyhow::Context;
+use anyhow::Result;
+use clap::Parser;
+use json_arg::JsonFile;
+use nix::sched::unshare;
+use nix::sched::CloneFlags;
+use tracing::trace;
+use tracing::warn;
+use walkdir::WalkDir;
+
+#[derive(Debug, Parser)]
+struct Args {
+    #[clap(long)]
+    model: JsonFile<antlir2_overlayfs::BuckModel>,
+    #[clap(long)]
+    subvol_symlink: PathBuf,
+    #[clap(long, default_value = "antlir2-out")]
+    working_dir: PathBuf,
+}
+
+fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::TRACE)
+        .init();
+    let args = Args::parse();
+
+    let working_volume = WorkingVolume::ensure(args.working_dir.clone())
+        .context("while setting up WorkingVolume")?;
+
+    if args.subvol_symlink.exists() {
+        trace!("removing existing output {}", args.subvol_symlink.display());
+        // Don't fail if the old subvol couldn't be deleted, just print
+        // a warning. We really don't want to fail someone's build if
+        // the only thing that went wrong is not being able to delete
+        // the last version of it.
+        match Subvolume::open(&args.subvol_symlink) {
+            Ok(old_subvol) => {
+                if let Err(e) = old_subvol.delete() {
+                    warn!(
+                        "couldn't delete old subvol '{}': {e:?}",
+                        args.subvol_symlink.display(),
+                    );
+                }
+            }
+            Err(e) => {
+                warn!(
+                    "couldn't open old subvol '{}': {e:?}",
+                    args.subvol_symlink.display(),
+                );
+            }
+        }
+    }
+
+    antlir2_rootless::unshare_new_userns().context("while entering new userns")?;
+    unshare(CloneFlags::CLONE_NEWNS).context("while entering new mount ns")?;
+
+    let overlay = OverlayFs::mount(
+        antlir2_overlayfs::Opts::builder()
+            .model(args.model.into_inner())
+            .build(),
+    )?;
+
+    let dst_root = working_volume
+        .allocate_new_path()
+        .context("while allocating new path for subvol")?;
+
+    Subvolume::create(&dst_root).context("while creating new subvol")?;
+
+    for entry in WalkDir::new(overlay.mountpoint()) {
+        let entry = entry.context("while walking overlayfs")?;
+        let relpath = entry
+            .path()
+            .strip_prefix(overlay.mountpoint())
+            .context("path not below overlayfs")?;
+        let dst = if relpath == Path::new(":") {
+            dst_root.clone()
+        } else {
+            dst_root.join(relpath)
+        };
+        let ft = entry.file_type();
+        let meta = entry.metadata()?;
+        if ft.is_dir() {
+            DirBuilder::new()
+                .recursive(true)
+                .mode(meta.mode())
+                .create(&dst)?;
+        } else if ft.is_symlink() {
+            let target = entry.path().read_link().context("while reading symlink")?;
+            std::os::unix::fs::symlink(target, dst)?;
+        } else if ft.is_file() {
+            std::fs::copy(entry.path(), &dst)?;
+        }
+    }
+
+    std::os::unix::fs::symlink(dst_root.canonicalize()?, &args.subvol_symlink)?;
+
+    Ok(())
+}
