f55f6a8d5d7a82d7464fd66e87cd1778ae2718d8
Vinnie Magro <vmagro@meta.com>
2024-08-16 12:25:29 -0700

    [antlir2][isolate_unshare] refactor and add error contexts (#254)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/antlir/pull/254
    
    Move code around and simplify it now that the pre-exec process doesn't have to
    be so constrained.
    
    Add context to most error points.
    
    Test Plan:
    ```
    ❯ buck2 test fbcode//antlir/antlir2/features/genrule/tests: fbcode//antlir/antlir2/features/rpm/tests:
    Buck UI: https://www.internalfb.com/buck2/457bb417-e0d1-4a27-9608-bfb0624dc59c
    Test UI: https://www.internalfb.com/intern/testinfra/testrun/16044073717563394
    Network: Up: 7.2MiB  Down: 2.2MiB  (reSessionID-6d45b2d5-319d-4493-b136-9abf1e187aca)
    Jobs completed: 88279. Time elapsed: 58.1s.
    Cache hits: 20%. Commands: 592 (cached: 120, remote: 0, local: 472)
    Tests finished: Pass 71. Fail 0. Fatal 0. Skip 0. Build failure 0
    ```
    
    Differential Revision: D61152830

--- antlir/antlir2/antlir2_isolate/isolate_unshare/src/lib.rs
+++ antlir/antlir2/antlir2_isolate/isolate_unshare/src/lib.rs
@@ -9,12 +9,8 @@
 compile_error!("only supported on linux");
 
 use std::ffi::OsStr;
-use std::ffi::OsString;
-use std::io::ErrorKind;
-use std::path::Path;
 use std::process::Command;
 
-use antlir2_users::passwd::EtcPasswd;
 use isolate_cfg::InvocationType;
 use isolate_cfg::IsolationContext;
 
@@ -30,6 +26,8 @@ pub enum Error {
     UserDb(#[from] antlir2_users::Error),
     #[error("user '{0}' not found in user database")]
     MissingUser(String),
+    #[error("failed to serialize isolation settings: {0} - {1}")]
+    Serialize(serde_json::Error, String),
 }
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
@@ -38,177 +36,29 @@ pub type Result<T, E = Error> = std::result::Result<T, E>;
 pub struct IsolatedContext<'a>(IsolationContext<'a>);
 
 impl<'a> IsolatedContext<'a> {
-    #[deny(unused_variables)]
     pub fn command<S: AsRef<OsStr>>(&self, program: S) -> Result<Command> {
-        let IsolationContext {
-            layer,
-            working_directory,
-            setenv,
-            platform,
-            inputs,
-            outputs,
-            invocation_type,
-            register,
-            user,
-            ephemeral,
-            tmpfs,
-            devtmpfs,
-            tmpfs_overlay,
-            hostname,
-            readonly,
-            enable_network,
-        } = &self.0;
-
         // TODO: remove these settings entirely when we get rid of
         // systemd-nspawn / move the things that require this (like image_test)
         // to *only* use systemd-nspawn
-        if *invocation_type != InvocationType::Pid2Pipe {
+        if self.0.invocation_type != InvocationType::Pid2Pipe {
             return Err(Error::UnsupportedSetting("invocation_type"));
         }
-        if *register {
+        if self.0.register {
             return Err(Error::UnsupportedSetting("register"));
         }
-        if *enable_network {
+        if self.0.enable_network {
             return Err(Error::UnsupportedSetting("enable_network"));
         }
-
-        let mut dir_binds = Vec::new();
-        let mut file_binds = Vec::new();
-        for (dst, src, ro) in inputs
-            .iter()
-            .chain(platform.iter())
-            .map(|(dst, src)| (dst, src, true))
-            .chain(outputs.iter().map(|(dst, src)| (dst, src, false)))
-        {
-            let ft = src.metadata()?.file_type();
-            let dst = if let Ok(target) =
-                std::fs::read_link(layer.join(dst.strip_prefix("/").unwrap_or(dst)))
-            {
-                dst.parent().unwrap_or(dst).join(target)
-            } else {
-                dst.clone().into_owned()
-            };
-            let dst = Path::new(isolate_unshare_preexec::NEWROOT)
-                .join(dst.strip_prefix("/").unwrap_or(&dst));
-            if ft.is_dir() {
-                dir_binds.push(isolate_unshare_preexec::Bind {
-                    src: src.clone().into(),
-                    dst,
-                    ro,
-                });
-            } else {
-                file_binds.push(isolate_unshare_preexec::Bind {
-                    src: src.clone().into(),
-                    dst,
-                    ro,
-                });
-            }
-        }
-        for devtmpfs in devtmpfs {
-            for dev in ["null", "random", "urandom"] {
-                file_binds.push(isolate_unshare_preexec::Bind {
-                    src: Path::new("/dev").join(dev),
-                    dst: Path::new(isolate_unshare_preexec::NEWROOT)
-                        .join(devtmpfs.strip_prefix("/").unwrap_or(devtmpfs))
-                        .join(dev),
-                    ro: false,
-                });
-            }
-        }
-
-        let (uid, gid) = if user == "root" {
-            (0, 0)
-        } else {
-            match std::fs::read_to_string(layer.join("etc/passwd")) {
-                Ok(contents) => {
-                    let user_db = EtcPasswd::parse(&contents).map_err(Error::UserDb)?;
-                    user_db
-                        .get_user_by_name(user)
-                        .ok_or_else(|| Error::MissingUser(user.clone().into()))
-                        .map(|u| (u.uid.into(), u.gid.into()))
-                }
-                Err(e) => match e.kind() {
-                    ErrorKind::NotFound => Err(Error::MissingUser(user.clone().into())),
-                    _ => Err(Error::IO(e)),
-                },
-            }?
-        };
-
-        let args = isolate_unshare_preexec::Args {
-            root: layer.clone().into(),
-            root_ro: *readonly,
-            dir_binds,
-            file_binds,
-            tmpfs: tmpfs
-                .iter()
-                .map(|t| {
-                    Path::new(isolate_unshare_preexec::NEWROOT)
-                        .join(t.strip_prefix("/").unwrap_or(t))
-                        .to_owned()
-                })
-                .collect(),
-            devtmpfs: devtmpfs
-                .iter()
-                .map(|t| {
-                    Path::new(isolate_unshare_preexec::NEWROOT)
-                        .join(t.strip_prefix("/").unwrap_or(t))
-                        .to_owned()
-                })
-                .collect(),
-            tmpfs_overlay: tmpfs_overlay
-                .iter()
-                .enumerate()
-                .map(|(idx, t)| {
-                    let dst = Path::new(isolate_unshare_preexec::NEWROOT)
-                        .join(t.strip_prefix("/").unwrap_or(t))
-                        .to_owned();
-                    let upper =
-                        Path::new("/tmp/__antlir2__/tmpfs_overlay").join(format!("upper_{idx}"));
-                    let work =
-                        Path::new("/tmp/__antlir2__/tmpfs_overlay").join(format!("work_{idx}"));
-                    let mut data: OsString = "lowerdir=".into();
-                    data.push(&dst);
-                    data.push(",upperdir=");
-                    data.push(&upper);
-                    data.push(",workdir=");
-                    data.push(&work);
-                    isolate_unshare_preexec::TmpfsOverlay {
-                        dst,
-                        upper,
-                        work,
-                        data,
-                    }
-                })
-                .collect(),
-            working_dir: working_directory
-                .as_ref()
-                .map(|wd| wd.clone().into())
-                .or_else(|| std::env::current_dir().ok())
-                .expect("no working dir set"),
-            hostname: hostname.clone().map(|h| h.clone().into()),
-            uid,
-            gid,
-            ephemeral: *ephemeral,
-        };
 
         let mut cmd = Command::new(
             buck_resources::get("antlir/antlir2/antlir2_isolate/isolate_unshare/preexec")
                 .expect("isolate_unshare_preexec is always present"),
         );
 
-        cmd.env_clear();
-        // reasonable default PATH (same as systemd-nspawn uses)
-        cmd.env(
-            "PATH",
-            "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+        cmd.arg(
+            serde_json::to_string(&self.0)
+                .map_err(|e| Error::Serialize(e, format!("{:#?}", self.0)))?,
         );
-        cmd.env("container", "antlir2");
-        cmd.env("USER", &**user);
-        if let Some(term) = std::env::var_os("TERM") {
-            cmd.env("TERM", term);
-        }
-        cmd.envs(setenv);
-        cmd.arg(serde_json::to_string(&args).expect("args are always serializable"));
         cmd.arg(program);
         cmd.arg("--");
 
