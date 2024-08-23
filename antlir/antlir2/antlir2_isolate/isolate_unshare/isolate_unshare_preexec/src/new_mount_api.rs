df177f855a91aae208a8bec234092e60aec938fd
Vinnie Magro <vmagro@meta.com>
2024-08-23 12:26:48 -0700

    [antlir2][isolate_unshare] ensure recursive input bind mounts are readonly
    Summary:
    This diff ensures that all input directories are readonly, even if they are a
    recursive bind mount.
    
    In unprivileged user namespaces, bind mounts must always be created with
    `MS_REC` to prevent access to overshadowed directories.
    Recursive bind mounts cannot be made readonly in one go, the children must be
    made readonly first.
    
    Test Plan:
    Unit tests
    
    ```
    ❯ buck2 test fbcode//antlir/antlir2/antlir2_isolate/isolate_unshare/... -- readonly
    Buck UI: https://www.internalfb.com/buck2/78bfb166-8fee-46e7-a277-e2defa55da65
    Test UI: https://www.internalfb.com/intern/testinfra/testrun/5066549823168723
    Tests finished: Pass 3. Fail 0. Fatal 0. Skip 0. Build failure 0
    ```
    
    As expected, before the code change, `recursive_inputs_are_readonly` failed
    ```counterexample
    ❯ buck2 test fbcode//antlir/antlir2/antlir2_isolate/isolate_unshare:tests -- readonly
    ✗ Fail: antlir/antlir2/antlir2_isolate/isolate_unshare:tests - recursive_inputs_are_readonly (0.2s)
    thread 'recursive_inputs_are_readonly' panicked at fbcode/antlir/antlir2/antlir2_isolate/isolate_unshare/tests/tests.rs:128:5:
    assertion failed: !out.status.success()
    note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
    Buck UI: https://www.internalfb.com/buck2/6182d3a5-fb71-429d-a661-d653f8590061
    Test UI: https://www.internalfb.com/intern/testinfra/testrun/281475356868794
    Network: Up: 3.1KiB  Down: 0B  (reSessionID-0e95c9f0-a5d0-477a-9700-443d812cde56)
    Jobs completed: 10. Time elapsed: 3.6s.
    Cache hits: 0%. Commands: 1 (cached: 0, remote: 0, local: 1)
    Tests finished: Pass 1. Fail 1. Fatal 0. Skip 0. Build failure 0
    1 TESTS FAILED
      ✗ antlir/antlir2/antlir2_isolate/isolate_unshare:tests - recursive_inputs_are_readonly
    ```
    
    Reviewed By: naveedgol
    
    Differential Revision: D61575776
    
    fbshipit-source-id: 926edd0ff59a72b4cd46ad1a615a3da2fe8291a8

---
+++ antlir/antlir2/antlir2_isolate/isolate_unshare/isolate_unshare_preexec/src/new_mount_api.rs
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+//! (Very thin) wrappers around the new Linux mount api
+
+use std::ffi::CString;
+use std::os::unix::ffi::OsStrExt;
+use std::path::Path;
+
+use anyhow::Context;
+use anyhow::Result;
+use libc::c_char;
+use libc::c_uint;
+use libc::AT_FDCWD;
+use libc::AT_RECURSIVE;
+use libc::AT_SYMLINK_NOFOLLOW;
+use rustix::mount::MountAttrFlags;
+
+#[repr(C)]
+#[allow(non_camel_case_types)]
+struct mount_attr {
+    attr_set: u64,
+    attr_clr: u64,
+    propagation: u64,
+    userns_fd: u64,
+}
+
+unsafe fn mount_setattr(
+    dirfd: std::os::fd::RawFd,
+    path: *const c_char,
+    flags: c_uint,
+    attr: &mount_attr,
+) -> Result<(), std::io::Error> {
+    if libc::syscall(
+        libc::SYS_mount_setattr,
+        dirfd,
+        path,
+        flags,
+        attr as *const _ as usize,
+        std::mem::size_of::<mount_attr>(),
+    ) == -1
+    {
+        Err(std::io::Error::last_os_error())
+    } else {
+        Ok(())
+    }
+}
+
+pub(crate) fn make_mount_readonly(path: &Path) -> Result<()> {
+    let path_c = CString::new(path.as_os_str().as_bytes()).context("while making CString path")?;
+    unsafe {
+        mount_setattr(
+            AT_FDCWD,
+            path_c.as_ptr(),
+            (AT_SYMLINK_NOFOLLOW | AT_RECURSIVE) as u32,
+            &mount_attr {
+                attr_set: MountAttrFlags::MOUNT_ATTR_RDONLY.bits() as u64,
+                attr_clr: 0,
+                propagation: 0,
+                userns_fd: 0,
+            },
+        )
+    }
+    .context("while making mount readonly")
+}
