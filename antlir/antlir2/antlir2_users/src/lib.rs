d62a4c9244aec7a21e73d9b13b3d0e34bec442d8
generatedunixname89002005232357 <generatedunixname89002005232357@meta.com>
2024-12-18 01:34:14 -0800

    Revert D66719226
    Summary:
    This diff reverts D66719226
    (The context such as a Sandcastle job, Task, SEV, etc. was not provided.)
    
    Test Plan:
    ```
    buck2 build mode/opt fbcode//tupperware/image/emulation:emulation.centos9.sendstream.v2 --show-full-output
    ```
    
    Before and after revert
    
    P1699485763
    
    Reviewed By: jamiecahill
    
    Differential Revision: D67380579
    
    fbshipit-source-id: 194a1e1f99e5cb8c040932c7110c92529bfca62b

--- antlir/antlir2/antlir2_users/src/lib.rs
+++ antlir/antlir2/antlir2_users/src/lib.rs
@@ -5,6 +5,9 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+use std::fmt::Display;
+use std::fmt::Formatter;
+
 pub mod group;
 pub mod passwd;
 pub mod shadow;
@@ -16,11 +19,9 @@ pub enum Error {
     Io(String),
     #[error("parse error: {0}")]
     Parse(String),
-    #[error("{0} defined twice, first as {1} and then as {2}")]
-    Duplicate(String, String, String),
 }
 
-pub type Result<T, E = Error> = std::result::Result<T, E>;
+pub type Result<T> = std::result::Result<T, Error>;
 
 pub trait Id: Copy + std::fmt::Debug {
     fn from_raw(id: u32) -> Self
@@ -83,3 +84,64 @@ macro_rules! id_type {
 
 id_type!(UserId, nix::unistd::Uid);
 id_type!(GroupId, nix::unistd::Gid);
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Password {
+    Shadow,
+    Locked,
+    /// Empty string, login is allowed without a password at all
+    Empty,
+}
+
+impl Password {
+    pub(crate) fn parse<'a, E>(input: &'a str) -> nom::IResult<&'a str, Self, E>
+    where
+        E: nom::error::ParseError<&'a str> + nom::error::ContextError<&'a str>,
+    {
+        let (input, txt) = nom::error::context(
+            "password",
+            nom::branch::alt((
+                nom::bytes::complete::tag("x"),
+                nom::bytes::complete::tag("!"),
+                nom::bytes::complete::tag(""),
+            )),
+        )(input)?;
+        Ok((
+            input,
+            match txt {
+                "x" => Self::Shadow,
+                "!" => Self::Locked,
+                "" => Self::Empty,
+                _ => unreachable!("parser would have failed"),
+            },
+        ))
+    }
+}
+
+impl Display for Password {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        match self {
+            Self::Shadow => write!(f, "x"),
+            Self::Locked => write!(f, "!"),
+            Self::Empty => write!(f, ""),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use nom::error::VerboseError;
+    use rstest::rstest;
+
+    use super::*;
+
+    #[rstest]
+    #[case::shadow("x", Password::Shadow)]
+    #[case::shadow("!", Password::Locked)]
+    #[case::shadow("", Password::Empty)]
+    fn test_parse_password(#[case] input: &str, #[case] expected: Password) {
+        let (rest, pw) = Password::parse::<VerboseError<&str>>(input).expect("failed to parse");
+        assert_eq!(pw, expected);
+        assert_eq!(rest, "", "all input should have been consumed");
+    }
+}
