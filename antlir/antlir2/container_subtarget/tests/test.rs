4cea9107cb41669a634e506dd6a4e42b72812355
Vinnie Magro <vmagro@meta.com>
2025-03-24 17:24:02 -0700

    [antlir2][container] unit tests for interactive console subtarget
    Summary:
    `[container]` subtargets are a critical debugging tool for users. Over the
    years we have regressed the functionality of this many times (even back to the
    early days of antlir1). This diff finally adds long-needed testing for this
    functionality so we can't silently regress again (at least to a state where
    it's not functional *at all*).
    
    Test Plan:
    ```
    ❯ buck2 test fbcode//antlir/antlir2/container_subtarget/...
    Buck UI: https://www.internalfb.com/buck2/8dc30691-6ff5-4e6e-8d5a-6388b4e90588
    Test UI: https://www.internalfb.com/intern/testinfra/testrun/3659174969331017
    Network: Up: 0B  Down: 0B  (reSessionID-df2b4420-64e8-41f4-bef7-e254643c03fb)
    Tests finished: Pass 7. Fail 0. Fatal 0. Skip 0. Build failure 0
    ```
    
    Reviewed By: naveedgol
    
    Differential Revision: D71657972
    
    fbshipit-source-id: 57a63242ccc30d8fe3aa44534730406ea977937b

---
+++ antlir/antlir2/container_subtarget/tests/test.rs
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+use rexpect::process::wait::WaitStatus;
+use rexpect::session::PtySession;
+
+fn test_base(exe: &str) -> PtySession {
+    let mut p = rexpect::spawn(exe, Some(5_000)).expect("failed to spawn");
+    // it would be better to set an explicit bash prompt, but that's hard to do
+    // without an rcfile, so we just rely on it being the default 'bash-$version#'
+    p.exp_regex("bash(.*?)#").expect("didn't get bash prompt");
+    p.send_line("echo 'testing that the shell works'")
+        .expect("failed to write shell command line");
+    p.exp_string("testing that the shell works")
+        .expect("didn't get echo output");
+    p
+}
+
+fn test_exit_code(exe: &str, code: i32) {
+    let mut p = test_base(exe);
+    p.send_line(&format!("exit {code}"))
+        .expect("failed to send exit command");
+    p.exp_eof().expect("didn't get EOF");
+    let status = p.process.wait().expect("failed to wait for process");
+    match status {
+        WaitStatus::Exited(_, real_code) => assert_eq!(code, real_code),
+        status => panic!("unexpected exit status: {status:?}"),
+    }
+}
+
+#[test]
+fn test_rooted_simple() {
+    let exe = std::env::var("ROOTED").expect("missing env var");
+    test_exit_code(&exe, 0);
+}
+
+#[test]
+fn test_rooted_exit_code() {
+    let exe = std::env::var("ROOTED").expect("missing env var");
+    test_exit_code(&exe, 42);
+}
+
+#[test]
+fn test_rootless_simple() {
+    let exe = std::env::var("ROOTLESS").expect("missing env var");
+    test_exit_code(&exe, 0);
+}
+
+#[test]
+fn test_rootless_exit_code() {
+    let exe = std::env::var("ROOTLESS").expect("missing env var");
+    test_exit_code(&exe, 42);
+}
+
+// TODO: test --boot variants once that always gives a shell
