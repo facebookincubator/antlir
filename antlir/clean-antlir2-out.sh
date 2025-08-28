fcb280a7453f33f5e29e972ac67802d4ca2aa1a5
Vinnie Magro <vmagro@meta.com>
2025-08-28 10:46:52 -0700

    [devserver][clean_disk] make antlir2-out cleaner more resilient
    Summary:
    The top level `antlir2-out` dir has not held subvolumes since D76380320.
    But there might still be devservers that have subvols in the old location. This
    just cleans up this script to handle
    
    Test Plan:
    Created a convoluted setup with subvols in both `antlir2-out/` and
    `antlir2-out/subvols`, including nested subvolumes in read-only parents. This
    new script still cleaned it up.
    ```
    vmagro@devvm21292.nha0 in fbsource/antlir2-out
    ❯ btrfs subvolume create foo
    Create subvolume './foo'
    
    vmagro@devvm21292.nha0 in fbsource/antlir2-out
    ❯ btrfs subvolume create subvols/bar
    Create subvolume 'subvols/bar'
    
    vmagro@devvm21292.nha0 in fbsource/antlir2-out
    ❯ btrfs subvolume create subvols/bar/baz
    Create subvolume 'subvols/bar/baz'
    
    vmagro@devvm21292.nha0 in fbsource/antlir2-out
    ❯ sudo btrfs property set subvols/bar ro true
    
    vmagro@devvm21292.nha0 in fbsource/antlir2-out
    ❯ tree
    .
    ├── foo
    └── subvols
        └── bar
            └── baz
    
    4 directories, 0 files
    
    vmagro@devvm21292.nha0 in fbsource/antlir2-out
    ❯ ~fbcode/antlir/clean-antlir2-out.sh
    + '[' '!' -d antlir2-out ']'
    + '[' '!' -d antlir2-out ']'
    + pushd antlir2-out
    /data/users/vmagro/fbsource/antlir2-out /data/users/vmagro/fbsource
    + find . -maxdepth 2 -depth -type d -inum 256 -exec sudo btrfs property set '{}' ro false ';'
    + sudo find . -depth -type d -inum 256 -exec btrfs subvolume delete '{}' ';'
    Delete subvolume 3169 (no-commit): '/data/users/vmagro/fbsource/antlir2-out/subvols/bar/baz'
    Delete subvolume 3168 (no-commit): '/data/users/vmagro/fbsource/antlir2-out/subvols/bar'
    Delete subvolume 3167 (no-commit): '/data/users/vmagro/fbsource/antlir2-out/foo'
    
    vmagro@devvm21292.nha0 in fbsource/antlir2-out
    ❯ tree
    .
    └── subvols
    
    1 directory, 0 files
    ```
    
    Rollback Plan:
    
    Reviewed By: sergeyfd
    
    Differential Revision: D81242825
    
    fbshipit-source-id: abf6737aa74418c8339a08f526941ad6d4b0c536

---
+++ antlir/clean-antlir2-out.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+set -ex
+
+if [ ! -d "antlir2-out" ]; then
+    cd "$(hg root)"
+fi
+
+if [ ! -d "antlir2-out" ]; then
+    echo "no antlir2-out found in repo root, exiting..."
+    exit
+fi
+
+pushd "antlir2-out"
+
+# all top-level subvolumes must be made read-write in case any nested
+# subvolume needs to be deleted (the compiler will now loudly fail if these are
+# found, but we still want to clean them up)
+# subvolumes may be found in either antlir2-out (old style) or
+# antlir2-out/subvols (new style) hence the -maxdepth 2
+find . -maxdepth 2 -depth -type d -inum 256 -exec sudo btrfs property set {} ro false \;
+
+# delete any nested subvolumes (sudo because we might not have permissions to
+# read the directories that they're found in)
+sudo find . -depth -type d -inum 256 -exec btrfs subvolume delete {} \;
